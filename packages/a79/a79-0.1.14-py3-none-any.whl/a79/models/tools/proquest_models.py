728887b6b4d728f3e74dcd4c856a40978817801f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:06:19 +0000

    Add package a79/0.1.14/a79-0.1.14-py3-none-any.whl

---
+++ packages/a79/a79-0.1.14-py3-none-any.whl/a79/models/tools/proquest_models.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+from common_py.connector.tap_proquest.models import ArticleRecord
+from external.a79.src.models.tools import ToolOutput
+
+
+class GetArticlesInput(BaseModel):
+    feed_url: str
+    limit: int = 15
+
+
+class GetArticlesOutput(ToolOutput):
+    articles: list[ArticleRecord]
