fbe98869b972a2fc1c4c5c430eb8c0843fbd32e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:04:57 +0000

    Add package pymrm/2.1.16/pymrm-2.1.16.tar.gz

---
+++ packages/pymrm/pymrm-2.1.16.tar.gz/pymrm-2.1.16/scripts/build_documentation.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# The commands provided here will be executed for building the documentation in the ci
+# but may also be used locally
+
+pip install myst-parser # workaround. Should be included in Docker image?
+cd docs/sphinx
+make html 2>documentation_errors.txt
+num_warnings=$(wc -l < documentation_errors.txt)
+echo "Found $num_warnings problem(s) during sphinx-build"
+if [[ $num_warnings -gt 1 ]]; then
+    echo Sphinx documentation needs to be improved!
+    exit 1
+fi
+echo Sphinx documentation was build successfully
---
+++ packages/pymrm/pymrm-2.1.16.tar.gz/pymrm-2.1.16/test/test_dummy.py
@@ -0,0 +1,6 @@
+from pymrm import grid
+
+
+def test_dummy():
+    mygrid = grid.generate_grid(100)
+    print(mygrid)
