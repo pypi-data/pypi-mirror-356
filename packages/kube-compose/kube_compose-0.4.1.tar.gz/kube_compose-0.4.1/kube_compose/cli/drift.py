55425bdcc3a1bc0b1a4d6a5551eb1f132d6f276b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:14 +0000

    Add package kube-compose/0.4.1/kube_compose-0.4.1.tar.gz

---
+++ packages/kube-compose/kube_compose-0.4.1.tar.gz/kube_compose-0.4.1/kube_compose/cli/drift.py
@@ -0,0 +1,13 @@
+import yaml
+from kube_compose.cli import cli
+from kube_compose import utils
+
+@cli.command()
+@utils.require_binaries(kubectl='kubectl')
+def drift(*, kubectl, namespace, **_):
+  ''' Compare the expected next deployment with the current deployment in kubernetes
+  '''
+  from kube_compose.cli.template import template
+  utils.run([
+    *kubectl, 'diff', *(('-n', namespace) if namespace else tuple()), '-f', '-'
+  ], input=yaml.dump_all(template()).encode())
