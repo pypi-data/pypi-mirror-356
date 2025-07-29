9b954f47cad07463444e5a8d27aaf3aea694a8af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:29 +0000

    Add package invaro/0.0.3/invaro-0.0.3-py3-none-any.whl

---
+++ packages/invaro/invaro-0.0.3-py3-none-any.whl/invaro/parser/__init__.py
@@ -0,0 +1,10 @@
+"""
+Invaro Parser Module
+
+Document parsing capabilities for invoices, statements, and general documents.
+
+This module is under development and will be available in a future release.
+"""
+
+# Module under development
+__all__ = []
