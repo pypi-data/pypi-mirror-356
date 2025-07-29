381e80a0488255fc985acd694aee5d1e48c53159
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:39 +0000

    Add package testmatrix/0.0.8/testmatrix-0.0.8-py3-none-any.whl

---
+++ packages/testmatrix/testmatrix-0.0.8-py3-none-any.whl/src/testmatrix/__init__.py
@@ -0,0 +1,10 @@
+# Matrix server sanity checker
+# ©Sebastian Spaeth & contributors
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+from .matrixserver import MatrixServer
+from .jwtauth import JWTAuth
+
+# User Agent and possibly other required headers that we need for all requests
+req_headers = {"User-Agent": "Mozilla/5.0 (Linux; x64)"}
+__all__ = ["MatrixServer", "JWTAuth"]
