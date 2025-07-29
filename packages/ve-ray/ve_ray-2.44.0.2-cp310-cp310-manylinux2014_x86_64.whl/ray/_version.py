30da8a768650c38eb13da5b33893c136ce1020c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:45 +0000

    Add package ve-ray/2.44.0.2/ve_ray-2.44.0.2-cp310-cp310-manylinux2014_x86_64.whl

---
+++ packages/ve-ray/ve_ray-2.44.0.2-cp310-cp310-manylinux2014_x86_64.whl/ray/_version.py
@@ -0,0 +1,6 @@
+# Replaced with the current commit when building the wheels.
+commit = "7abe4df53cb97174936766416d55a99960304f47"
+version = "2.44.0.2"
+
+if __name__ == "__main__":
+    print("%s %s" % (version, commit))
