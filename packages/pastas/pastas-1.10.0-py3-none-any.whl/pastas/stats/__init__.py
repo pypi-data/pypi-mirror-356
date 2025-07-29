a865b1b7488d5779242f34a15e0de854f3648a56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:04:57 +0000

    Add package pastas/1.10.0/pastas-1.10.0-py3-none-any.whl

---
+++ packages/pastas/pastas-1.10.0-py3-none-any.whl/pastas/stats/__init__.py
@@ -0,0 +1,41 @@
+"""The stats sub-package contains statistical methods for Pastas.
+
+.. currentmodule:: pastas.stats
+
+.. autosummary::
+    :toctree: ./generated
+    :nosignatures:
+
+    core
+    metrics
+    dutch
+    sgi
+    signatures
+    tests
+
+"""
+
+# flake8: noqa
+
+import pastas.stats.metrics as metrics
+import pastas.stats.signatures as signatures
+
+from .core import acf, ccf, mean, std, var
+from .dutch import ghg, glg, gvg, q_ghg, q_glg, q_gvg, gg
+from .metrics import (
+    aic,
+    bic,
+    evp,
+    kge,
+    kge_2012,
+    mae,
+    nse,
+    nnse,
+    pearsonr,
+    rmse,
+    rsq,
+    sse,
+    picp,
+)
+from .sgi import sgi
+from .tests import diagnostics, durbin_watson, ljung_box, runs_test, stoffer_toloi
---
+++ packages/pastas/pastas-1.10.0-py3-none-any.whl/pastas/typing/__init__.py
@@ -0,0 +1,18 @@
+# flake8: noqa
+from .types import (
+    ArrayLike,
+    Axes,
+    CallBack,
+    Figure,
+    Function,
+    Model,
+    NoiseModel,
+    OseriesSettingsDict,
+    Recharge,
+    RFunc,
+    Solver,
+    StressModel,
+    StressSettingsDict,
+    TimeSeries,
+    TimestampType,
+)
