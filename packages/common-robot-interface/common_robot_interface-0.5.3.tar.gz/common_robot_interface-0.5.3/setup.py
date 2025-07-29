1fff2f252042e6c96dda6492c9ea07de0e236a8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:08:38 +0000

    Add package common-robot-interface/0.5.3/common_robot_interface-0.5.3.tar.gz

---
+++ packages/common-robot-interface/common_robot_interface-0.5.3.tar.gz/common_robot_interface-0.5.3/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""Setup file for Common Robot Interface.
+"""
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="common_robot_interface",
+    version="0.5.3",
+    description="Common Robot Interface",
+    license="GPLv3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="John Lloyd, Nathan Lepora",
+    author_email="j.lloyd@bristol.ac.uk, n.lepora@bristol.ac.uk",
+    url="https://github.com/robot-dexterity/common-robot-interface",
+    packages=find_packages(),
+	package_data={'cri.ur': ['rtde_config.xml'],
+            "cri.dobot.magician": ['DobotDll.dll',"msvcp120.dll","msvcr120.dll","Qt5Core.dll","Qt5Network.dll","Qt5SerialPort.dll"]},
+    install_requires=["numpy", "transforms3d"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
