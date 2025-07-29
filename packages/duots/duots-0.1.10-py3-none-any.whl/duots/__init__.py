fcb04393061d31b7203d68485b1f6a624741b2a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:17 +0000

    Add package duots/0.1.10/duots-0.1.10-py3-none-any.whl

---
+++ packages/duots/duots-0.1.10-py3-none-any.whl/duots/__init__.py
@@ -0,0 +1,12 @@
+"""
+duots: Composable timeseries feature extraction from paired signals.
+"""
+__version__ = "0.1.0"
+
+import operator as op
+import itertools as its
+import functools as fts
+
+
+# Expose the imported modules (optional, depending on your design)
+__all__ = ['op', 'its', 'fts']
