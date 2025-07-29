a212701ac40597060440f97f5c4799df5cf3b860
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:58 +0000

    Add package datagrid-ai/0.1.0a11/datagrid_ai-0.1.0a11.tar.gz

---
+++ packages/datagrid-ai/datagrid_ai-0.1.0a11.tar.gz/datagrid_ai-0.1.0a11/src/datagrid_ai/types/agent_tools.py
@@ -0,0 +1,33 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing_extensions import Literal, TypeAlias
+
+__all__ = ["AgentTools"]
+
+AgentTools: TypeAlias = Literal[
+    "data_analysis",
+    "semantic_search",
+    "agent_memory",
+    "schema_info",
+    "table_info",
+    "create_dataset",
+    "calendar",
+    "schedule_recurring_message_tool",
+    "procore",
+    "egnyte",
+    "notion",
+    "google_sheets",
+    "slack",
+    "microsoft_teams",
+    "data_classification",
+    "data_extraction",
+    "image_detection",
+    "attachment_extraction",
+    "pdf_extraction",
+    "connect_data",
+    "download_data",
+    "web_search",
+    "fetch_url",
+    "company_prospect_researcher",
+    "people_prospect_researcher",
+]
