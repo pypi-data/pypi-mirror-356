6ec24d59c6736f2bc875eee927dc1c61fb22ecde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:34 +0000

    Add package invaro/0.0.4/invaro-0.0.4-py3-none-any.whl

---
+++ packages/invaro/invaro-0.0.4-py3-none-any.whl/invaro/accounting/__init__.py
@@ -0,0 +1,10 @@
+"""
+Invaro Accounting Module
+
+Unified accounting API integrations for seamless financial data management.
+
+This module is under development and will be available in a future release.
+"""
+
+# Module under development
+__all__ = []
