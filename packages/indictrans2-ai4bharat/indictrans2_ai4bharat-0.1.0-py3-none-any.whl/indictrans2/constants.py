469f13c255531bed7ddf9c4d5c4b7badb6396e7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:56 +0000

    Add package indictrans2-ai4bharat/0.1.0/indictrans2_ai4bharat-0.1.0-py3-none-any.whl

---
+++ packages/indictrans2-ai4bharat/indictrans2_ai4bharat-0.1.0-py3-none-any.whl/indictrans2/constants.py
@@ -0,0 +1,30 @@
+"""Constants for IndicTrans2 translation"""
+
+SUPPORTED_LANGUAGES = {
+    "mr": "Marathi",
+    "gu": "Gujarati", 
+    "or": "Odia",
+    "brx": "Bodo",
+    "ml": "Malayalam",
+    "ks": "Kashmiri",
+    "en": "English",
+    "sd": "Sindhi",
+    "bn": "Bengali",
+    "sat": "Santali", 
+    "ta": "Tamil",
+    "mai": "Maithili",
+    "ne": "Nepali",
+    "pa": "Punjabi",
+    "te": "Telugu",
+    "mni": "Meitei (Manipuri)",
+    "ur": "Urdu",
+    "kn": "Kannada",
+    "as": "Assamese",
+    "sa": "Sanskrit",
+    "hi": "Hindi",
+    "kok": "Konkani",
+    "doi": "Dogri"
+}
+
+API_ENDPOINT = "https://admin.models.ai4bharat.org/inference/translate"
+SERVICE_ID = "ai4bharat/indictrans--gpu-t4"
