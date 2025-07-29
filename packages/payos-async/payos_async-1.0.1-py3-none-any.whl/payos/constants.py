6201ab481a6253208e23ed0a6105944d69786fc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:13 +0000

    Add package payos-async/1.0.1/payos_async-1.0.1-py3-none-any.whl

---
+++ packages/payos-async/payos_async-1.0.1-py3-none-any.whl/payos/constants.py
@@ -0,0 +1,18 @@
+ERROR_MESSAGE_VI = {
+    'NO_SIGNATURE': 'Không có chữ ký.',
+    'NO_DATA': 'Không có dữ liệu.',
+    'INVALID_SIGNATURE': 'Chữ ký không hợp lệ.',
+    'DATA_NOT_INTEGRITY': 'Dữ liệu không đáng tin cậy vì chữ ký của phản hồi không khớp với chữ ký của dữ liệu.',
+    'WEBHOOK_URL_INVALID': 'URL Webhook không hợp lệ.',
+    'UNAUTHORIZED': 'Không được phép.',
+    'INTERNAL_SERVER_ERROR': 'Lỗi máy chủ nội bộ.',
+    'INVALID_PARAMETER': 'Tham số không hợp lệ.'
+}
+
+ERROR_CODE = {
+    'INTERNAL_SERVER_ERROR': '20',
+    'UNAUTHORIZED': '401'
+}
+
+PAYOS_BASE_URL = "https://api-merchant.payos.vn"
+
