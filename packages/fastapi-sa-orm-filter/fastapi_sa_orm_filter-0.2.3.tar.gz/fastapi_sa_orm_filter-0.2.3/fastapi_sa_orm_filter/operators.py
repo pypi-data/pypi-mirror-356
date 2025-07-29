1d1e895be02b65e8879fcae49cbe905a2af78001
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:13 +0000

    Add package fastapi-sa-orm-filter/0.2.3/fastapi_sa_orm_filter-0.2.3.tar.gz

---
+++ packages/fastapi-sa-orm-filter/fastapi_sa_orm_filter-0.2.3.tar.gz/fastapi_sa_orm_filter-0.2.3/fastapi_sa_orm_filter/operators.py
@@ -0,0 +1,26 @@
+from enum import Enum
+
+
+class Operators(str, Enum):
+    eq = "__eq__"
+    gt = "__gt__"
+    lt = "__lt__"
+    gte = "__ge__"
+    lte = "__le__"
+    in_ = "in_"
+    startswith = "startswith"
+    endswith = "endswith"
+    between = "between"
+    like = "like"
+    ilike = "ilike"
+    contains = "contains"
+    icontains = "icontains"
+    not_eq = "__ne__"
+    not_in = "not_in"
+    not_like = "not_like"
+    not_between = "not_between"
+
+
+class OrderSequence(str, Enum):
+    desc = "desc"
+    asc = "asc"
---
+++ packages/fastapi-sa-orm-filter/fastapi_sa_orm_filter-0.2.3.tar.gz/fastapi_sa_orm_filter-0.2.3/tests/utils.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class JobCategory(Enum):
+    finance = "Finance"
+    marketing = "Marketing"
+    agro = "Agriculture"
+    it = "IT"
+    metallurgy = "Metallurgy"
+    medicine = "Medicine"
+    construction = "Construction"
+    building = "Building"
+    services = "Services"
+    miscellaneous = "Miscellaneous"
