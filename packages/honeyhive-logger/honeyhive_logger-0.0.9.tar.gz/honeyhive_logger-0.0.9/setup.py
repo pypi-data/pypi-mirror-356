114ea5cbf91c58e745b31f2b6116e4c4a87b1243
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:26 +0000

    Add package honeyhive-logger/0.0.9/honeyhive_logger-0.0.9.tar.gz

---
+++ packages/honeyhive-logger/honeyhive_logger-0.0.9.tar.gz/honeyhive_logger-0.0.9/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="honeyhive-logger",
+    version="0.0.9",
+    packages=find_packages(),
+    install_requires=[],
+    extras_require={
+        "dev": [
+            "pytest>=7.0.0",
+            "twine>=4.0.0",
+        ],
+    },
+    author="HoneyHive",
+    author_email="support@honeyhive.ai",
+    description="A Python logger for HoneyHive",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/honeyhive-ai/honeyhive-logger",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+) 
