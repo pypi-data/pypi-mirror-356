0f9134af6eaf86604fc97c73554f22185367c6a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:24 +0000

    Add package TA-Box/0.0.1a1.dev4/ta_box-0.0.1a1.dev4-cp312-cp312-win_amd64.whl

---
+++ packages/TA-Box/ta_box-0.0.1a1.dev4-cp312-cp312-win_amd64.whl/tabox/__init__.py
@@ -0,0 +1,82 @@
+# Math Transform
+from .ta_func.ta_ACOS import *
+from .ta_func.ta_ASIN import *
+from .ta_func.ta_ATAN import *
+from .ta_func.ta_SQRT import *
+from .ta_func.ta_TAN import *
+from .ta_func.ta_CEIL import *
+from .ta_func.ta_COS import *
+from .ta_func.ta_COSH import *
+from .ta_func.ta_EXP import *
+from .ta_func.ta_FLOOR import *
+from .ta_func.ta_LN import *
+from .ta_func.ta_LOG10 import *
+from .ta_func.ta_SINH import *
+from .ta_func.ta_TANH import *
+from .ta_func.ta_MAXINDEX import *
+from .ta_func.ta_MININDEX import *
+from .ta_func.ta_MINMAX import *
+from .ta_func.ta_MINMAXINDEX import *
+
+# Math Operators
+from .ta_func.ta_ADD import *
+from .ta_func.ta_SUB import *
+from .ta_func.ta_MULT import *
+from .ta_func.ta_DIV import *
+from .ta_func.ta_MAX import *
+from .ta_func.ta_MIN import *
+
+# Momentum Indicators
+from .ta_func.ta_ADX import *
+
+# Overlap Studies
+from .ta_func.ta_MA import *
+from .ta_func.ta_DEMA import *
+from .ta_func.ta_TEMA import *
+from .ta_func.ta_TRIMA import *
+from .ta_func.ta_KAMA import *
+from .ta_func.ta_MAMA import *
+from .ta_func.ta_T3 import *
+from .ta_func.ta_SMA import *
+from .ta_func.ta_EMA import *
+from .ta_func.ta_WMA import *
+from .ta_func.ta_BBANDS import *
+
+
+# Momentum Indicators
+from .ta_func.ta_MACD import *
+from .ta_func.ta_MACDEXT import *
+
+# Price Transform
+from .ta_func.ta_AVGPRICE import *
+from .ta_func.ta_MEDPRICE import *
+from .ta_func.ta_TYPPRICE import *
+from .ta_func.ta_WCLPRICE import *
+
+# Statistic Functions
+from .ta_func.ta_STDDEV import *
+from .ta_func.ta_VAR import *
+
+# Volatility Indicators
+from .ta_func.ta_TRANGE import *
+from .ta_func.ta_ATR import *
+
+# Volume Indicators
+from .ta_func.ta_AD import *
+from .ta_func.ta_ADOSC import *
+from .ta_func.ta_ADX import *
+
+
+# Other Studies
+from .ta_func.ta_RSI import *
+
+
+
+
+
+
+
+
+
+
+
