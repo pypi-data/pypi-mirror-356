79f7d50c1ccf01f3ad601ab942db5efbd9253db0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:06:01 +0000

    Add package contextual-privacy-guard/0.1.4/contextual_privacy_guard-0.1.4.tar.gz

---
+++ packages/contextual-privacy-guard/contextual_privacy_guard-0.1.4.tar.gz/contextual_privacy_guard-0.1.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+name='contextual-privacy-guard',
+version='0.1.4',
+description='A toolkit for safeguarding contextual privacy in LLM prompts',
+author='IBM Research',
+author_email='ivoline@ibm.com',
+url='https://github.com/IBM/contextual-privacy-LLM',
+packages=find_packages(exclude=['tests']),
+include_package_data=True,
+package_data={
+'contextual_privacy_guard': ['prompts/*/*.txt'],
+},
+install_requires=[
+'requests>=2.0'
+],
+entry_points={
+'console_scripts': [
+'contextual-privacy-guard=contextual_privacy_guard.runner:main',
+],
+},
+python_requires='>=3.8',
+)
