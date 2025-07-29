7d8c91f6bf891c6f19c16c8656104f1cb1942c1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:41 +0000

    Add package nervos/0.0.3/nervos-0.0.3.tar.gz

---
+++ packages/nervos/nervos-0.0.3.tar.gz/nervos-0.0.3/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = "0.0.3"
+DESCRIPTION = "Tool to simulate Spiking Neural Networks"
+
+
+setup(
+    name="nervos",
+    version=VERSION,
+    author="Jaskirat Singh Maskeen",
+    author_email="<jsmaskeen@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[
+        "keras",
+        "matplotlib",
+        "numpy",
+        "pandas",
+        "rich",
+        "scikit-learn",
+        "scipy",
+        "tensorflow"
+    ],
+    keywords=[
+        "python",
+        "spiking neural network",
+        "simulation",
+        "stdp",
+        "snn",
+        "neuromorphic computing",
+        "mnist",
+        "iris",
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
