5800b10b387a593b493116b41e2ee289da424c41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:01 +0000

    Add package scipyconf/0.0.dev0/scipyconf-0.0.dev0.tar.gz

---
+++ packages/scipyconf/scipyconf-0.0.dev0.tar.gz/scipyconf-0.0.dev0/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='scipyconf',
+      version='0.0dev',
+      description='SciPy Conference',
+      author='SciPy Conference Organizers',
+      )
