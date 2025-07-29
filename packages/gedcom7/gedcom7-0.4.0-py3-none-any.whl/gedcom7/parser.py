f72b29ec0e46abc4d41564ecb9fda4761563822c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:28 +0000

    Add package gedcom7/0.4.0/gedcom7-0.4.0-py3-none-any.whl

---
+++ packages/gedcom7/gedcom7-0.4.0-py3-none-any.whl/gedcom7/parser.py
@@ -0,0 +1,42 @@
+"""GEDCOM 7 parser."""
+
+import re
+
+from . import cast, const, grammar
+from .types import GedcomStructure
+
+
+def loads(string: str) -> list[GedcomStructure]:
+    """Load from a string."""
+    context: dict[int, GedcomStructure] = {}
+    records: list[GedcomStructure] = []
+    ext: dict[str, str] = {}
+    for match in re.finditer(grammar.line, string):
+        data = match.groupdict()
+        level = int(data["level"])
+        # handle continuation lines
+        if data["tag"] == const.CONT:
+            context[level - 1].text += "\n" + data["linestr"]
+            continue
+        structure = GedcomStructure(
+            tag=ext.get(data["tag"]) or data["tag"],
+            pointer=data["pointer"],
+            xref=data["xref"],
+            text=data["linestr"],
+        )
+        # handle extension tags
+        if (
+            structure.tag == const.TAG
+            and context[0].tag == const.HEAD
+            and context[1].tag == const.SCHMA
+        ):
+            tag_name, tag_uri = structure.text.split(" ")
+            ext[tag_name] = tag_uri
+        context[level] = structure
+        # append structure to output
+        if level > 0:
+            parent = context[level - 1]
+            parent.append_child(structure)
+        else:
+            records.append(structure)
+    return records
