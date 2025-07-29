7554a6c07d64eb18d4842f7dd55c567ebf7c9272
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:07:15 +0000

    Add package a2wsgi/1.10.10/a2wsgi-1.10.10.tar.gz

---
+++ packages/a2wsgi/a2wsgi-1.10.10.tar.gz/a2wsgi-1.10.10/tests/test_asgi.py
@@ -0,0 +1,242 @@
+import asyncio
+import concurrent.futures
+from collections import Counter
+
+import httpx
+import pytest
+
+from a2wsgi.asgi import ASGIMiddleware, build_scope
+
+
+async def hello_world(scope, receive, send):
+    assert scope["type"] == "http"
+    await send(
+        {
+            "type": "http.response.start",
+            "status": 200,
+            "headers": [
+                [b"content-type", b"text/plain"],
+                [b"content-length", b"13"],
+            ],
+        }
+    )
+    await send({"type": "http.response.body", "body": b"Hello, world!"})
+
+
+async def echo_body(scope, receive, send):
+    assert scope["type"] == "http"
+    body = b""
+    more_body = True
+    while more_body:
+        msg = await receive()
+        body += msg.get("body", b"")
+        more_body = msg.get("more_body", False)
+    await send(
+        {
+            "type": "http.response.start",
+            "status": 200,
+            "headers": [
+                (b"content-type", b"text/plain"),
+                (b"Content-Length", str(len(body)).encode("latin1")),
+            ],
+        }
+    )
+    await send({"type": "http.response.body", "body": body})
+
+
+async def raise_exception(scope, receive, send):
+    raise RuntimeError("Something went wrong")
+
+
+async def background_tasks(scope, receive, send):
+    await hello_world(scope, receive, send)
+    await asyncio.sleep(10)
+
+
+async def concurrent_rw(scope, receive, send):
+    async def listen_for_disconnect() -> None:
+        while True:
+            message = await receive()
+            if message["type"] == "http.disconnect":
+                break
+
+    async def stream_response() -> None:
+        await send(
+            {
+                "type": "http.response.start",
+                "status": 200,
+                "headers": [],
+            }
+        )
+        for chunk in range(10):
+            await send(
+                {
+                    "type": "http.response.body",
+                    "body": chunk.to_bytes(4, "big"),
+                    "more_body": True,
+                }
+            )
+
+        await send({"type": "http.response.body", "body": b"", "more_body": False})
+
+    done, pending = await asyncio.wait(
+        [
+            asyncio.create_task(listen_for_disconnect()),
+            asyncio.create_task(stream_response()),
+        ],
+        return_when=asyncio.ALL_COMPLETED,
+    )
+    [task.cancel() for task in pending]
+    [task.result() for task in done]
+
+
+def test_asgi_get():
+    app = ASGIMiddleware(hello_world)
+    with httpx.Client(
+        transport=httpx.WSGITransport(app=app), base_url="http://testserver:80"
+    ) as client:
+        response = client.get("/")
+        assert response.status_code == 200
+        assert response.text == "Hello, world!"
+
+
+def test_asgi_post():
+    app = ASGIMiddleware(echo_body)
+    with httpx.Client(
+        transport=httpx.WSGITransport(app=app), base_url="http://testserver:80"
+    ) as client:
+        response = client.post("/", content="hi boy")
+        assert response.status_code == 200
+        assert response.text == "hi boy"
+
+
+def test_asgi_exception():
+    app = ASGIMiddleware(raise_exception)
+    with httpx.Client(
+        transport=httpx.WSGITransport(app=app), base_url="http://testserver:80"
+    ) as client:
+        with pytest.raises(RuntimeError):
+            client.get("/")
+
+
+def test_asgi_exception_info():
+    app = ASGIMiddleware(raise_exception)
+    with httpx.Client(
+        transport=httpx.WSGITransport(app, raise_app_exceptions=False),
+        base_url="http://testserver:80",
+    ) as client:
+        response = client.get("/")
+        assert response.status_code == 500
+        assert response.text == "Server got itself in trouble"
+
+
+def test_background_app():
+    executor = concurrent.futures.ThreadPoolExecutor()
+
+    def _():
+        app = ASGIMiddleware(background_tasks)
+        with httpx.Client(
+            transport=httpx.WSGITransport(app=app), base_url="http://testserver:80"
+        ) as client:
+            response = client.get("/")
+            assert response.status_code == 200
+            assert response.text == "Hello, world!"
+
+    future = executor.submit(_)
+    with pytest.raises(concurrent.futures.TimeoutError):
+        future.result(1)
+    future.cancel()
+
+
+def test_background_app_wait_time():
+    executor = concurrent.futures.ThreadPoolExecutor()
+
+    def _():
+        app = ASGIMiddleware(background_tasks, wait_time=1)
+        with httpx.Client(
+            transport=httpx.WSGITransport(app=app), base_url="http://testserver:80"
+        ) as client:
+            response = client.get("/")
+            assert response.status_code == 200
+            assert response.text == "Hello, world!"
+
+    future = executor.submit(_)
+    future.result(2)
+
+
+def test_concurrent_rw():
+    app = ASGIMiddleware(concurrent_rw)
+
+    with httpx.Client(
+        transport=httpx.WSGITransport(app=app), base_url="http://testserver:80"
+    ) as client:
+        response = client.get("/")
+        assert response.status_code == 200
+
+
+def test_http_content_headers():
+    content_type = "application/json"
+    content_length = "5"
+    environ = {
+        "REQUEST_METHOD": "POST",
+        "QUERY_STRING": "",
+        "PATH_INFO": "/foo",
+        "SERVER_NAME": "foo.invalid",
+        "SERVER_PORT": "80",
+        "CONTENT_TYPE": content_type,
+        "HTTP_CONTENT_TYPE": content_type,
+        "CONTENT_LENGTH": content_length,
+        "HTTP_CONTENT_LENGTH": content_length,
+    }
+    scope = build_scope(environ)
+    counter = Counter(scope["headers"])
+    assert counter[(b"content-type", content_type.encode())] == 1
+    assert counter[(b"content-length", content_length.encode())] == 1
+
+
+def test_starlette_stream_response():
+    from starlette.responses import StreamingResponse
+
+    app = ASGIMiddleware(StreamingResponse(content=map(str, range(10))))
+    with httpx.Client(
+        transport=httpx.WSGITransport(app=app), base_url="http://testserver:80"
+    ) as client:
+        response = client.get("/")
+        assert response.status_code == 200
+        assert response.text == "0123456789"
+
+
+def test_starlette_base_http_middleware():
+    from starlette.responses import JSONResponse
+    from starlette.middleware.base import BaseHTTPMiddleware
+
+    class Middleware(BaseHTTPMiddleware):
+        async def dispatch(self, request, call_next):
+            response = await call_next(request)
+            response.headers["x-middleware"] = "true"
+            return response
+
+    app = ASGIMiddleware(Middleware(JSONResponse({"hello": "world"})))
+    with httpx.Client(
+        transport=httpx.WSGITransport(app=app), base_url="http://testserver:80"
+    ) as client:
+        response = client.get("/")
+        assert response.status_code == 200
+        assert response.text == '{"hello":"world"}'
+        assert response.headers["x-middleware"] == "true"
+
+
+def test_baize_stream_response():
+    from baize.asgi import StreamResponse
+
+    async def stream():
+        for i in range(10):
+            yield str(i).encode()
+
+    app = ASGIMiddleware(StreamResponse(stream()))
+    with httpx.Client(
+        transport=httpx.WSGITransport(app=app), base_url="http://testserver:80"
+    ) as client:
+        response = client.get("/")
+        assert response.status_code == 200
+        assert response.text == "0123456789"
---
+++ packages/a2wsgi/a2wsgi-1.10.10.tar.gz/a2wsgi-1.10.10/tests/test_wsgi.py
@@ -0,0 +1,248 @@
+import asyncio
+import os
+import sys
+import threading
+
+import httpx
+import pytest
+
+from a2wsgi.wsgi import Body, WSGIMiddleware, build_environ
+
+
+def test_body():
+    event_loop = asyncio.new_event_loop()
+    threading.Thread(target=event_loop.run_forever, daemon=True).start()
+
+    async def receive():
+        return {
+            "type": "http.request.body",
+            "body": b"""This is a body test.
+Why do this?
+To prevent memory leaks.
+And cancel pre-reading.
+Newline.0
+Newline.1
+Newline.2
+Newline.3
+""",
+        }
+
+    body = Body(event_loop, receive)
+    assert body.readline() == b"This is a body test.\n"
+    assert body.read(4) == b"Why "
+    assert body.readline(2) == b"do"
+    assert body.readline(20) == b" this?\n"
+
+    assert body.readlines(2) == [
+        b"To prevent memory leaks.\n",
+        b"And cancel pre-reading.\n",
+    ]
+    for index, line in enumerate(body):
+        assert line == b"Newline." + str(index).encode("utf8") + b"\n"
+        if index == 1:
+            break
+    assert body.readlines() == [
+        b"Newline.2\n",
+        b"Newline.3\n",
+    ]
+    assert body.readlines() == []
+    assert body.readline() == b""
+    assert body.read() == b""
+
+    for line in body:
+        assert False
+
+
+def hello_world(environ, start_response):
+    status = "200 OK"
+    output = b"Hello World!\n"
+    headers = [
+        ("Content-Type", "text/plain; charset=utf-8"),
+        ("Content-Length", str(len(output))),
+    ]
+    start_response(status, headers)
+    return [output]
+
+
+def echo_body(environ, start_response):
+    status = "200 OK"
+    output = environ["wsgi.input"].read()
+    headers = [
+        ("Content-Type", "text/plain; charset=utf-8"),
+        ("Content-Length", str(len(output))),
+    ]
+    start_response(status, headers)
+    return [output]
+
+
+def raise_exception(environ, start_response):
+    raise RuntimeError("Something went wrong")
+
+
+def return_exc_info(environ, start_response):
+    try:
+        raise RuntimeError("Something went wrong")
+    except RuntimeError:
+        status = "500 Internal Server Error"
+        output = b"Internal Server Error"
+        headers = [
+            ("Content-Type", "text/plain; charset=utf-8"),
+            ("Content-Length", str(len(output))),
+        ]
+        start_response(status, headers, exc_info=sys.exc_info())
+        return [output]
+
+
+@pytest.mark.asyncio
+async def test_wsgi_get():
+    app = WSGIMiddleware(hello_world)
+    async with httpx.AsyncClient(
+        transport=httpx.ASGITransport(app=app), base_url="http://testserver"
+    ) as client:
+        response = await client.get("/")
+        assert response.status_code == 200
+        assert response.text == "Hello World!\n"
+
+
+@pytest.mark.asyncio
+async def test_wsgi_post():
+    app = WSGIMiddleware(echo_body)
+    async with httpx.AsyncClient(
+        transport=httpx.ASGITransport(app=app), base_url="http://testserver"
+    ) as client:
+        response = await client.post("/", json={"example": 123})
+        assert response.status_code == 200
+        assert response.json() == {"example": 123}
+
+
+@pytest.mark.asyncio
+async def test_wsgi_exception():
+    # Note that we're testing the WSGI app directly here.
+    # The HTTP protocol implementations would catch this error and return 500.
+    app = WSGIMiddleware(raise_exception)
+    async with httpx.AsyncClient(
+        transport=httpx.ASGITransport(app=app), base_url="http://testserver"
+    ) as client:
+        with pytest.raises(RuntimeError):
+            await client.get("/")
+
+
+@pytest.mark.asyncio
+async def test_wsgi_exc_info():
+    # Note that we're testing the WSGI app directly here.
+    # The HTTP protocol implementations would catch this error and return 500.
+    app = WSGIMiddleware(return_exc_info)
+    async with httpx.AsyncClient(
+        transport=httpx.ASGITransport(app=app), base_url="http://testserver"
+    ) as client:
+        with pytest.raises(RuntimeError):
+            response = await client.get("/")
+
+    app = WSGIMiddleware(return_exc_info)
+    async with httpx.AsyncClient(
+        transport=httpx.ASGITransport(app, raise_app_exceptions=False),
+        base_url="http://testserver",
+    ) as client:
+        response = await client.get("/")
+        assert response.status_code == 500
+        assert response.text == "Internal Server Error"
+
+
+@pytest.mark.asyncio
+async def test_build_environ():
+    scope = {
+        "type": "http",
+        "http_version": "1.1",
+        "method": "GET",
+        "scheme": "https",
+        "path": "/中文",
+        "query_string": b"a=123&b=456",
+        "headers": [
+            (b"host", b"www.example.org"),
+            (b"content-type", b"application/json"),
+            (b"content-length", b"18"),
+            (b"accept", b"application/json"),
+            (b"accept", b"text/plain"),
+        ],
+        "client": ("134.56.78.4", 1453),
+        "server": ("www.example.org", 443),
+    }
+
+    async def receive():
+        raise NotImplementedError
+
+    environ = build_environ(scope, Body(asyncio.get_event_loop(), receive))
+    environ.pop("wsgi.input")
+    environ.pop("asgi.scope")
+    assert environ == {
+        "CONTENT_LENGTH": "18",
+        "CONTENT_TYPE": "application/json",
+        "HTTP_ACCEPT": "application/json,text/plain",
+        "HTTP_HOST": "www.example.org",
+        "PATH_INFO": "/中文".encode("utf8").decode("latin-1"),
+        "QUERY_STRING": "a=123&b=456",
+        "REMOTE_ADDR": "134.56.78.4",
+        "REMOTE_PORT": "1453",
+        "REQUEST_METHOD": "GET",
+        "SCRIPT_NAME": "",
+        "SERVER_NAME": "www.example.org",
+        "SERVER_PORT": "443",
+        "SERVER_PROTOCOL": "HTTP/1.1",
+        "wsgi.errors": sys.stdout,
+        "wsgi.multiprocess": True,
+        "wsgi.multithread": True,
+        "wsgi.run_once": False,
+        "wsgi.url_scheme": "https",
+        "wsgi.version": (1, 0),
+    }
+
+
+@pytest.mark.asyncio
+async def test_build_environ_with_env():
+    os.environ["SCRIPT_NAME"] = "/urlprefix"
+
+    scope = {
+        "type": "http",
+        "http_version": "1.1",
+        "method": "GET",
+        "scheme": "https",
+        "path": "/中文",
+        "query_string": b"a=123&b=456",
+        "headers": [
+            (b"host", b"www.example.org"),
+            (b"content-type", b"application/json"),
+            (b"content-length", b"18"),
+            (b"accept", b"application/json"),
+            (b"accept", b"text/plain"),
+        ],
+        "client": ("134.56.78.4", 1453),
+        "server": ("www.example.org", 443),
+    }
+
+    async def receive():
+        raise NotImplementedError
+
+    environ = build_environ(scope, Body(asyncio.get_event_loop(), receive))
+    environ.pop("wsgi.input")
+    environ.pop("asgi.scope")
+    assert environ == {
+        "CONTENT_LENGTH": "18",
+        "CONTENT_TYPE": "application/json",
+        "HTTP_ACCEPT": "application/json,text/plain",
+        "HTTP_HOST": "www.example.org",
+        "PATH_INFO": "/中文".encode("utf8").decode("latin-1"),
+        "QUERY_STRING": "a=123&b=456",
+        "REMOTE_ADDR": "134.56.78.4",
+        "REMOTE_PORT": "1453",
+        "REQUEST_METHOD": "GET",
+        "SCRIPT_NAME": "/urlprefix",
+        "SERVER_NAME": "www.example.org",
+        "SERVER_PORT": "443",
+        "SERVER_PROTOCOL": "HTTP/1.1",
+        "wsgi.errors": sys.stdout,
+        "wsgi.multiprocess": True,
+        "wsgi.multithread": True,
+        "wsgi.run_once": False,
+        "wsgi.url_scheme": "https",
+        "wsgi.version": (1, 0),
+    }
