057f0d3917c69fbc3c0b8423d6e5aa4129011b65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:03 +0000

    Add package NLEParser/0.2.0/nleparser-0.2.0.tar.gz

---
+++ packages/NLEParser/nleparser-0.2.0.tar.gz/nleparser-0.2.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="NLEParser",
+    version="0.2.0",
+    author="NateLang",
+    author_email="natancorreiatr@gmail.com",
+    description="A Natural language processing parser for Zork-style games",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(where="NLEParser"),
+    package_dir={"": "NLEParser"},
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.13",
+)
