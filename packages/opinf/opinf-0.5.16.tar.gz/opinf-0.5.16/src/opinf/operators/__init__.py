e87cb27a16486487ee295385f7d98a5d71d7219f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:33 +0000

    Add package opinf/0.5.16/opinf-0.5.16.tar.gz

---
+++ packages/opinf/opinf-0.5.16.tar.gz/opinf-0.5.16/src/opinf/operators/__init__.py
@@ -0,0 +1,7 @@
+# operators/__init__.py
+"""Operator classes for the individual terms of a model."""
+
+from ._base import *
+from ._nonparametric import *
+from ._affine import *
+from ._interpolate import *
