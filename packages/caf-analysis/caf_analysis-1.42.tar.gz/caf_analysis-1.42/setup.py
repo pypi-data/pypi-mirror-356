fdf1dcbae748af13f05fff2cd2daedd10ac12f1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:07 +0000

    Add package caf-analysis/1.42/caf_analysis-1.42.tar.gz

---
+++ packages/caf-analysis/caf_analysis-1.42.tar.gz/caf_analysis-1.42/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='caf-analysis',
+    version='1.42',
+    author='Archie Baldock',
+    description='TOF peak finding for CaF data analysis',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'matplotlib',
+        'scipy'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
