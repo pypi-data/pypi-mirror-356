3dc4f8874bc06c927de520e9386441d4f124a6da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:54 +0000

    Add package antelope-reports/0.2.1/antelope_reports-0.2.1.tar.gz

---
+++ packages/antelope-reports/antelope_reports-0.2.1.tar.gz/antelope_reports-0.2.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup()
+
+"""
+Version History
+
+0.2.1 - catch-up release for ISSST workshop
+
+0.2.0 - catch-up release for ACLCA workshop
+
+0.1.1post99 - 2024-05-15 get ready for PyPI release
+
+0.1.1 - First split from lca-tools
+
+"""
