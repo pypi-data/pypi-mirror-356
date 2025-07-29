d241a863aad13694f31fe56420633aec80879572
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:08:25 +0000

    Add package scisuit/1.8.0/scisuit-1.8.0-cp310-cp310-win_amd64.whl

---
+++ packages/scisuit/scisuit-1.8.0-cp310-cp310-win_amd64.whl/scisuit/settings.py
@@ -0,0 +1,8 @@
+
+"""
+Apps embedding Python have their own mainloop.
+When using scisuit plotting functions there is no need to start another mainloop.
+
+Tested on apps built with wxWidgets.
+"""
+START_APP_MAINLOOP = True
