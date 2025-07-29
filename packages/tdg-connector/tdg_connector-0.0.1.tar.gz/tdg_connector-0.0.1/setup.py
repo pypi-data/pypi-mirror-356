1e3da7afec6c26ecb3e912ed2cb96b71111e8127
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:55 +0000

    Add package tdg-connector/0.0.1/tdg_connector-0.0.1.tar.gz

---
+++ packages/tdg-connector/tdg_connector-0.0.1.tar.gz/tdg_connector-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'TdgAPI Python Client'
+
+setup(
+    name="tdg-connector",
+    version=VERSION,
+    author="Waqar-tdg",
+    author_email="pankaj.gawade@truthics.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['requests'],
+    keywords=['python']
+)
