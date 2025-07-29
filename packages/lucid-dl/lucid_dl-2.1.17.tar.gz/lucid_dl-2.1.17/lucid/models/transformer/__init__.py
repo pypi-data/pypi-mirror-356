8ae0f79438b0782b9dc7b9cdd0883dfd7c2f0f6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:09:04 +0000

    Add package lucid-dl/2.1.17/lucid_dl-2.1.17.tar.gz

---
+++ packages/lucid-dl/lucid_dl-2.1.17.tar.gz/lucid_dl-2.1.17/lucid/models/transformer/__init__.py
@@ -0,0 +1,10 @@
+# Sequence-to-Sequence
+from .base import *
+
+# Image Classification
+from .vit import *
+from .swin import *
+from .cvt import *
+from .pvt import *
+from .crossvit import *
+from .maxvit import *
