4e33e487c8845638474486101bbab410817164b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:08:18 +0000

    Add package seliseblocks-genesis/0.1.2/seliseblocks_genesis-0.1.2-py3-none-any.whl

---
+++ packages/seliseblocks-genesis/seliseblocks_genesis-0.1.2-py3-none-any.whl/blocks_genesis/core/blocks_secret.py
@@ -0,0 +1,17 @@
+from typing import Optional
+from pydantic import BaseModel
+
+
+class BlocksSecret(BaseModel):
+    CacheConnectionString: str = ""
+    MessageConnectionString: str = ""
+    LogConnectionString: str = ""
+    MetricConnectionString: str = ""
+    TraceConnectionString: str = ""
+    LogDatabaseName: str = ""
+    MetricDatabaseName: str = ""
+    TraceDatabaseName: str = ""
+    ServiceName: str = ""
+    DatabaseConnectionString: str = ""
+    RootDatabaseName: str = ""
+
