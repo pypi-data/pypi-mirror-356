3508b018ce75df57716e4b22cd49d08d65118084
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:05 +0000

    Add package grokcore.error/5.0/grokcore_error-5.0-py3-none-any.whl

---
+++ packages/grokcore.error/grokcore_error-5.0-py3-none-any.whl/grokcore/error/errorreport.py
@@ -0,0 +1,47 @@
+import logging
+
+import grokcore.component as grok
+import zope.error.interfaces
+
+
+@grok.implementer(zope.error.interfaces.IErrorReportingUtility)
+class LoggingErrorReporting:
+    grok.provides(zope.error.interfaces.IErrorReportingUtility)
+
+    def __init__(
+            self,
+            info_level_errors=None,
+            warning_level_errors=None,
+            always_exc_info=False):
+        self.logger = logging.getLogger('grokcore.error')
+        self.info_level_errors = info_level_errors
+        self.warning_level_errors = warning_level_errors
+        self.always_exc_info = always_exc_info
+
+    def make_extra(self, request=None):
+        return None
+
+    def raising(self, exc_info, request=None):
+        exc_class = exc_info[0]
+        msg = f'{exc_info[0].__name__} {str(exc_info[1])}'
+        if request is not None:
+            url = getattr(request, 'URL', None)
+            if url is not None:
+                msg += (' (URL: %s)' % url)
+
+        level = self.logger.error
+
+        if self.info_level_errors and \
+                issubclass(exc_class, self.info_level_errors):
+            level = self.logger.info
+            exc_info = exc_info if self.always_exc_info else None
+
+        elif self.warning_level_errors and \
+                issubclass(exc_class, self.warning_level_errors):
+            level = self.logger.warning
+            exc_info = exc_info if self.always_exc_info else None
+
+        try:
+            level(msg, exc_info=exc_info, extra=self.make_extra(request))
+        finally:
+            exc_info = None  # gc cleanup.
---
+++ packages/grokcore.error/grokcore_error-5.0-py3-none-any.whl/grokcore/error/tests/test_sentry.py
@@ -0,0 +1,54 @@
+import unittest
+from unittest import mock
+
+from zope.publisher.browser import TestRequest
+
+from grokcore.error.sentry import SentryAwareLoggingErrorReporting
+from grokcore.error.zcml import errorreportingutility
+
+
+class ZcmlTestCase(unittest.TestCase):
+
+    def test_errorreportingutility(self):
+        _context = mock.Mock()
+        errorreportingutility(
+            _context, factory=SentryAwareLoggingErrorReporting)
+        self.assertTrue(_context.action.called)
+
+    def test_make_extra(self):
+        request = TestRequest()
+        reporter = SentryAwareLoggingErrorReporting()
+        result = reporter.make_extra(request)
+        expect = {'sentry.interfaces.Http': {
+            'host': None,
+            'headers': {'Host': '127.0.0.1'},
+            'cookies': None,
+            'env': {'CONTENT_LENGTH': '0',
+                    'SERVER_URL': 'http://127.0.0.1',
+                    'GATEWAY_INTERFACE': 'TestFooInterface/1.0'},
+            'url': 'http://127.0.0.1',
+            'data': bytes('', encoding='utf-8'),
+            'method': 'GET',
+            'query_string': None}}
+        self.assertEqual(expect, result)
+
+    def test_make_extra_principal(self):
+        class MockPrincipal:
+            id = 'foo'
+
+        request = TestRequest()
+        request.setPrincipal(MockPrincipal())
+        reporter = SentryAwareLoggingErrorReporting()
+        result = reporter.make_extra(request)
+        self.assertIn('sentry.interfaces.User', result)
+        self.assertEqual('foo', result['sentry.interfaces.User']['id'])
+
+    def test_make_extra_request_none(self):
+        reporter = SentryAwareLoggingErrorReporting()
+        result = reporter.make_extra(None)
+        self.assertEqual(result, None)
+
+    def test_make_extra_request_no_httprequest(self):
+        reporter = SentryAwareLoggingErrorReporting()
+        result = reporter.make_extra(object())
+        self.assertEqual(result, None)
