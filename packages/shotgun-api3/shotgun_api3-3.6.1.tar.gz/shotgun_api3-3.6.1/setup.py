59c4562dacec002439c6abb0e8ebde377a684519
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:06:41 +0000

    Add package shotgun-api3/3.6.1/shotgun_api3-3.6.1.tar.gz

---
+++ packages/shotgun-api3/shotgun_api3-3.6.1.tar.gz/shotgun_api3-3.6.1/setup.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019 Shotgun Software Inc.
+#
+# CONFIDENTIAL AND PROPRIETARY
+#
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
+# Source Code License included in this distribution package. See LICENSE.
+# By accessing, using, copying or modifying this work you indicate your
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
+# not expressly granted therein are reserved by Shotgun Software Inc.
+
+import sys
+from setuptools import setup, find_packages
+
+f = open('README.md')
+readme = f.read().strip()
+
+f = open('LICENSE')
+license = f.read().strip()
+
+# For python 2.4 support
+script_args = sys.argv[1:]
+if (sys.version_info[0] <= 2) or (sys.version_info[0] == 2 and sys.version_info[1] <= 5):
+    if 'install' in script_args and '--no-compile' not in script_args:
+        script_args.append('--no-compile')
+
+
+setup(
+    name='shotgun_api3',
+    version='3.6.1',
+    description='Flow Production Tracking Python API',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='Autodesk',
+    url='https://github.com/shotgunsoftware/python-api',
+    license=license,
+    packages=find_packages(exclude=('tests',)),
+    script_args=script_args,
+    include_package_data=True,
+    package_data={'': ['cacerts.txt', 'cacert.pem']},
+    zip_safe=False,
+    python_requires=">=3.7.0",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: OS Independent",
+    ],
+)
