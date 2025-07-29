19dcd587646a16fee9304d35f45ed85f387a98af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:32 +0000

    Add package django-fast/0.1.0/django_fast-0.1.0.tar.gz

---
+++ packages/django-fast/django_fast-0.1.0.tar.gz/django_fast-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="django_fast",
+    version="0.1.0",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "django>=4.2,<5.0",
+        "uvicorn>=0.22,<1.0",
+    ],
+    entry_points={
+        "console_scripts": [
+            "django-fast = django_fast.cli:main",  # adjust name/module path
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Framework :: Django",
+    ],
+    python_requires=">=3.8",
+)
