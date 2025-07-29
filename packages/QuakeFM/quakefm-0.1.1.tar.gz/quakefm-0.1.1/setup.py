19220aaeddadfd3772cbe5c61ac11e103901c3b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:34 +0000

    Add package QuakeFM/0.1.1/quakefm-0.1.1.tar.gz

---
+++ packages/QuakeFM/quakefm-0.1.1.tar.gz/quakefm-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="QuakeFM",
+    version="0.1.1",
+    long_description="",
+    long_description_content_type="text/markdown",
+    packages=["quakefm"],
+    install_requires=["numpy",  "h5py", "matplotlib", "pandas"],
+)
