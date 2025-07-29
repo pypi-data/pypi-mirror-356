743ff7131d081fa758542e636d4d4aa81c450423
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:06:19 +0000

    Add package thsdk/1.4.8/thsdk-1.4.8.tar.gz

---
+++ packages/thsdk/thsdk-1.4.8.tar.gz/thsdk-1.4.8/thsdk/examples/tick_level1.py
@@ -0,0 +1,18 @@
+from thsdk import THS
+import pandas as pd
+import time
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+with THS() as ths:
+    response = ths.tick_level1("USZA300033")
+    print("tick历史数据:")
+    if not response.is_success():
+        print(f"错误信息: {response.err_info}")
+
+    df = pd.DataFrame(response.get_result())
+    df["时间"] = pd.to_datetime(df["时间"], unit="s").dt.tz_localize("UTC").dt.tz_convert("Asia/Shanghai")
+    # pd.set_option('display.max_columns', None)
+    # pd.set_option('display.max_rows', None)
+    print(df)
+    time.sleep(1)
