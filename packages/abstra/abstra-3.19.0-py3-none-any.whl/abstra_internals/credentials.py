093193717d82fa582126eec4625f195436a49974
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:12 +0000

    Add package abstra/3.19.0/abstra-3.19.0-py3-none-any.whl

---
+++ packages/abstra/abstra-3.19.0-py3-none-any.whl/abstra_internals/credentials.py
@@ -0,0 +1,53 @@
+import os
+from threading import Lock
+
+from abstra_internals.settings import Settings
+from abstra_internals.utils.dot_abstra import CREDENTIALS_FILE
+
+credentials_lock = Lock()
+
+
+def get_credentials():
+    if os.getenv("ABSTRA_API_TOKEN"):
+        return os.getenv("ABSTRA_API_TOKEN")
+
+    with credentials_lock:
+        credentials_path = Settings.root_path.joinpath(CREDENTIALS_FILE)
+
+        if not credentials_path.exists():
+            return None
+
+        with open(credentials_path, "r", encoding="utf-8") as f:
+            return f.read().strip()
+
+
+def delete_credentials():
+    with credentials_lock:
+        credentials_path = Settings.root_path.joinpath(CREDENTIALS_FILE)
+
+        if credentials_path.exists():
+            credentials_path.unlink()
+
+
+def set_credentials(token: str):
+    with credentials_lock:
+        credentials_path = Settings.root_path.joinpath(CREDENTIALS_FILE)
+        credentials_path.parent.mkdir(exist_ok=True)
+
+        credentials_path.write_text(token, encoding="utf-8")
+
+
+def resolve_headers():
+    credentials = get_credentials()
+
+    if not credentials:
+        return None
+
+    return {"Api-Authorization": f"Bearer {credentials}"}
+
+
+def resolve_headers_raise():
+    headers = resolve_headers()
+    if headers is None:
+        raise Exception("You must be logged in to execute this operation")
+    return headers
