a587655fd0ee3ea62fc1072716be5dca60b7f3c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:08 +0000

    Add package MeUtils/2025.6.18.14.28.4/MeUtils-2025.6.18.14.28.4-py3-none-any.whl

---
+++ packages/MeUtils/MeUtils-2025.6.18.14.28.4-py3-none-any.whl/examples/_openaisdk/openai_doubao.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : openai_siliconflow
+# @Time         : 2024/6/26 10:42
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  :
+import os
+
+from meutils.pipe import *
+from openai import OpenAI
+from openai import OpenAI, APIStatusError
+
+# 404 403 429
+client = OpenAI(
+    # api_key=os.getenv("OPENAI_API_KEY_GUOCHAN"),
+    # api_key="sk-acnBrFLJo3E732FfHN0kf0tcHyjfAiCEomyjKr56AUtPIWso",
+    api_key="a4fa52c2-f1cc-435a-a143-62403922488c",
+    base_url="https://ark.cn-beijing.volces.com/api/v3", # /chat/completions
+)
+
+try:
+    completion = client.chat.completions.create(
+        # model="ep-20241225184145-7nf5n",
+        # model="deepseek-r1-250528",
+        model="doubao-1-5-pro-32k-250115",
+
+        # model="doubao-lite-32k-character",
+        # model="doubao-pro-32k-character",
+
+        # model="doubao-pro-32k-search",
+
+        messages=[
+            {"role": "user", "content": "你是谁"}
+        ],
+        # top_p=0.7,
+        top_p=None,
+        temperature=None,
+        stream=True,
+        stream_options={"include_usage": True},
+        max_tokens=1000
+    )
+except APIStatusError as e:
+    print(e.status_code)
+
+    print(e.response)
+    print(e.message)
+    print(e.code)
+
+print(completion)
+for chunk in completion:
+    print(chunk)
+    print(chunk.choices[0].delta.content)
