32555592d4eb645debab42e2cecfc32da9433b18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:08:36 +0000

    Add package canonmap/0.1.63/canonmap-0.1.63.tar.gz

---
+++ packages/canonmap/canonmap-0.1.63.tar.gz/canonmap-0.1.63/setup.py
@@ -0,0 +1,33 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="canonmap",
+    version="0.1.63",
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "spacy>=3.7.2",
+        "rapidfuzz",
+        "Metaphone",
+        "scikit-learn",
+        "chardet",
+        "torch",
+        "transformers>=4.0.0",
+    ],
+    author="Vince Berry",
+    author_email="vince.berry@gmail.com",
+    description="CanonMap - A Python library for entity canonicalization and mapping",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/vinceberry/canonmap",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires=">=3.8",
+)
