974a07b0b3a7ef539e09f17b2f8a0e40573f9880
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:47 +0000

    Add package trustcaptcha-python/1.2.1b0/trustcaptcha_python-1.2.1b0-py3-none-any.whl

---
+++ packages/trustcaptcha-python/trustcaptcha_python-1.2.1b0-py3-none-any.whl/trustcaptcha/__init__.py
@@ -0,0 +1,8 @@
+import warnings
+
+warnings.warn(
+    "The `trustcaptcha-python` package is DEPRECATED in favor of "
+    "`trustcaptcha>=2.0.0`. Please update your requirements.",
+    DeprecationWarning,
+    stacklevel=2,
+)
