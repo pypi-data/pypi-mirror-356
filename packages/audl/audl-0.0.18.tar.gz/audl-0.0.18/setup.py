2ef37cebd68ac4e9716b07c4f35f79f52303be5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:08:35 +0000

    Add package audl/0.0.18/audl-0.0.18.tar.gz

---
+++ packages/audl/audl-0.0.18.tar.gz/audl-0.0.18/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+import pathlib
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="audl",
+    version="0.0.18",
+    license="MIT",
+    author="yukikongju",
+    author_email="yukikongju@outlook.com",
+    description="Unofficial audl api",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yukikongju/audl",
+    install_requires=["pandas", "requests", "lxml", "html5lib", "pdoc3"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.7",
+    keywords=[
+        "audl",
+        "audl.com",
+        "ultimate frisbee",
+    ],
+)
