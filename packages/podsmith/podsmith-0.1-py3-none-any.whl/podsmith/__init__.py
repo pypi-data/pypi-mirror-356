b5cf93351b4c9fbf166378d6b1665e40a9d9d44b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:07:15 +0000

    Add package podsmith/0.1/podsmith-0.1-py3-none-any.whl

---
+++ packages/podsmith/podsmith-0.1-py3-none-any.whl/podsmith/__init__.py
@@ -0,0 +1,26 @@
+# Copyright (c) 2025 Andreas Stenius
+# This software is licensed under the MIT License.
+# See the LICENSE file for details.
+"""**Podsmith** is a Python toolkit for managing Kubernetes-based test dependencies, enabling dynamic or pre-provisioned environments for integration testing."""
+
+__version__ = "0.1"
+
+from .config_map import ConfigMap
+from .manifest import random_text
+from .pod import Pod
+from .role import ClusterRole, Role
+from .role_binding import ClusterRoleBinding, RoleBinding
+from .service import Service
+from .service_account import ServiceAccount
+
+__all__ = [
+    "ClusterRole",
+    "ClusterRoleBinding",
+    "ConfigMap",
+    "Pod",
+    "Role",
+    "RoleBinding",
+    "Service",
+    "ServiceAccount",
+    "random_text",
+]
