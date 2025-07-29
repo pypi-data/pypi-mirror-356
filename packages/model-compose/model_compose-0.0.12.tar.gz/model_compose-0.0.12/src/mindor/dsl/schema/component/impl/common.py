a1e5f87187ec1b6398daedbd37551b332b2223d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:08:04 +0000

    Add package model-compose/0.0.12/model_compose-0.0.12.tar.gz

---
+++ packages/model-compose/model_compose-0.0.12.tar.gz/model_compose-0.0.12/src/mindor/dsl/schema/component/impl/common.py
@@ -0,0 +1,9 @@
+from typing import Type, Union, Literal, Optional, Dict, List, Tuple, Set, Annotated, Any
+from pydantic import BaseModel, Field
+from .types import ComponentType
+
+class CommonComponentConfig(BaseModel):
+    type: ComponentType
+    runtime: Literal[ "docker", "native" ] = "native"
+    max_concurrent_count: int = 1
+    default: bool = False
