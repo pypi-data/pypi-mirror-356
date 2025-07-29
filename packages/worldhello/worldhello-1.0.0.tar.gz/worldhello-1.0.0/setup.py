023be5732b0ad65ca956d6f615e5b30b177c6e72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:15 +0000

    Add package worldhello/1.0.0/worldhello-1.0.0.tar.gz

---
+++ packages/worldhello/worldhello-1.0.0.tar.gz/worldhello-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="worldhello",
+    version="1.0.0",
+    description="PoC for dependency confusion",
+    author="sl4x0",
+    author_email="attacker@oob.sl4x0.xyz",
+    packages=["worldhello"],
+    install_requires=[],
+)
