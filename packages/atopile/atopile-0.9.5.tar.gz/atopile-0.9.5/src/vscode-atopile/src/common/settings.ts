e57a3c7cb1efaf5597b4f7753df0cc681cf90a5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:07:01 +0000

    Add package atopile/0.9.5/atopile-0.9.5.tar.gz

---
+++ packages/atopile/atopile-0.9.5.tar.gz/atopile-0.9.5/src/vscode-atopile/src/common/settings.ts
@@ -0,0 +1,30 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import { WorkspaceFolder } from 'vscode';
+import { getConfiguration, getWorkspaceFolders, resolvePath } from './vscodeapi';
+import { SERVER_ID } from './constants';
+
+export interface ISettings {
+    cwd: string;
+    workspace: string;
+    ato: string | undefined;
+    from: string | undefined;
+}
+
+export function getExtensionSettings(): Promise<ISettings[]> {
+    return Promise.all(getWorkspaceFolders().map((w) => getWorkspaceSettings(w)));
+}
+
+export async function getWorkspaceSettings(workspace: WorkspaceFolder): Promise<ISettings> {
+    const config = getConfiguration(SERVER_ID, workspace.uri);
+
+    const ato_config = config.get<string>(`ato`);
+    const workspaceSetting = {
+        cwd: workspace.uri.fsPath,
+        workspace: workspace.uri.toString(),
+        ato: ato_config ? resolvePath(ato_config, workspace) : undefined,
+        from: config.get<string>(`from`),
+    };
+    return workspaceSetting;
+}
