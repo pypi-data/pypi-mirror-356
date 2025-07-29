10818af146abc82cfd16dbf7451a39aebe125218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:50 +0000

    Add package cachetto/0.0.1/cachetto-0.0.1-py3-none-any.whl

---
+++ packages/cachetto/cachetto-0.0.1-py3-none-any.whl/cachetto/__init__.py
@@ -0,0 +1,4 @@
+from cachetto.config import disable_caching, enable_caching, get_config, set_config
+from cachetto.core import cached
+
+__all__ = ["cached", "disable_caching", "enable_caching", "get_config", "set_config"]
