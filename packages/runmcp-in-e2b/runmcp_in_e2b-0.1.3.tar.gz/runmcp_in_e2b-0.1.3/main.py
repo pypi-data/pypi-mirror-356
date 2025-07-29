524cff2d62adffa4b508a77fed6338caf353aa68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:21 +0000

    Add package runmcp-in-e2b/0.1.3/runmcp_in_e2b-0.1.3.tar.gz

---
+++ packages/runmcp-in-e2b/runmcp_in_e2b-0.1.3.tar.gz/runmcp_in_e2b-0.1.3/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from runmcp-in-e2b!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/runmcp-in-e2b/runmcp_in_e2b-0.1.3.tar.gz/runmcp_in_e2b-0.1.3/src/runmcp_in_e2b/__main__.py
@@ -0,0 +1,5 @@
+import asyncio
+from .runmcp import main
+
+print("__main__ start main")
+asyncio.run(main())
