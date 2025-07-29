588b07b6a48c062b8acdc066c83eb56f17d58bd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:34 +0000

    Add package voiceflow-module/0.5.0/voiceflow_module-0.5.0.tar.gz

---
+++ packages/voiceflow-module/voiceflow_module-0.5.0.tar.gz/voiceflow_module-0.5.0/README.md
@@ -0,0 +1,7 @@
+## voiceflow-module
+
+Module to help speed up Voiceflow text agents' development
+
+# Installation
+
+pip install voiceflow-module
---
+++ packages/voiceflow-module/voiceflow_module-0.5.0.tar.gz/voiceflow_module-0.5.0/voiceflow_module/__init__.py
@@ -0,0 +1,3 @@
+from .kb_manager import try_upload_table, try_upload_text, was_document_uploaded
+from .utils import split_text_into_chunks, get_structered_xml
+from .ai_service import OpenaAIService
