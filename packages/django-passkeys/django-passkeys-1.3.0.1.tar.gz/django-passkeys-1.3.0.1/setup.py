b4c9b708e5d7da662e68db9210108094941e2f05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:57 +0000

    Add package django-passkeys/1.3.0.1/django-passkeys-1.3.0.1.tar.gz

---
+++ packages/django-passkeys/django-passkeys-1.3.0.1.tar.gz/django-passkeys-1.3.0.1/setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(
+    name='django-passkeys',
+    version='1.3.0.1',
+    description='A Django Authentication Backend for Passkeys',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author='Mohamed El-Kalioby',
+    author_email = 'mkalioby@mkalioby.com',
+    url = 'https://github.com/mkalioby/django-passkeys',
+    download_url='https://github.com/mkalioby/django-passkeys',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'django >= 2.0',
+        'ua-parser',
+        'user-agents',
+        'fido2>=1.1.1,<2.1.0',
+      ],
+    python_requires=">=3.7",
+    include_package_data=True,
+    zip_safe=False, # because we're including static files
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        #"Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 2.0",
+        "Framework :: Django :: 2.1",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
+        "Framework :: Django :: 3.1",
+        "Framework :: Django :: 3.2",
+        "Framework :: Django :: 4.0",
+        "Framework :: Django :: 4.1",
+        "Framework :: Django :: 4.2",
+        "Framework :: Django :: 5.0",
+        "Framework :: Django :: 5.1",
+        "Framework :: Django :: 5.2",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+]
+)
