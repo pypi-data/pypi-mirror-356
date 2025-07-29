0116be7805dfc61f5390999bd3caff28b77593a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:08:30 +0000

    Add package pydantic-rest-client/1.1.0/pydantic_rest_client-1.1.0-py3-none-any.whl

---
+++ packages/pydantic-rest-client/pydantic_rest_client-1.1.0-py3-none-any.whl/pydantic_rest_client/__init__.py
@@ -0,0 +1,28 @@
+"""Pydantic REST Client - library for working with REST APIs with Pydantic validation"""
+
+from .base_rest_client import RestClient, validate_response
+from .aiohttp_rest_client import AioHttpRestClient
+from .exceptions import (
+    RestClientError,
+    NetworkError,
+    ValidationError,
+    ConfigurationError,
+    ResponseError,
+    TimeoutError,
+)
+
+__version__ = "1.1.0"
+__author__ = "Damian Sop"
+__email__ = "damian.sop.official@gmail.com"
+
+__all__ = [
+    "RestClient",
+    "AioHttpRestClient",
+    "validate_response",
+    "RestClientError",
+    "NetworkError",
+    "ValidationError",
+    "ConfigurationError",
+    "ResponseError",
+    "TimeoutError",
+]
