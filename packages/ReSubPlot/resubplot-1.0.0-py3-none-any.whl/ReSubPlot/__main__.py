c13a4c88a5367a216936dc172f64f97c26995b74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:24 +0000

    Add package ReSubPlot/1.0.0/resubplot-1.0.0-py3-none-any.whl

---
+++ packages/ReSubPlot/resubplot-1.0.0-py3-none-any.whl/ReSubPlot/__main__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""
+ReSubPlot.__main__: executed when ReSubPlot directory is called as script.
+"""
+
+from ReSubPlot.resubplot_cli import main
+
+if __name__ == "__main__":
+    main()
