4df837ea600d1cd4aa31cbb97d9b43bc84ae9a0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:08 +0000

    Add package atla-insights/0.0.1a13/atla_insights-0.0.1a13-py3-none-any.whl

---
+++ packages/atla-insights/atla_insights-0.0.1a13-py3-none-any.whl/atla_insights/__init__.py
@@ -0,0 +1,52 @@
+"""Atla package for PyPI distribution."""
+
+from logfire import instrument
+
+from ._main import (
+    configure,
+    instrument_agno,
+    instrument_anthropic,
+    instrument_google_genai,
+    instrument_langchain,
+    instrument_litellm,
+    instrument_mcp,
+    instrument_openai,
+    instrument_openai_agents,
+    instrument_smolagents,
+    mark_failure,
+    mark_success,
+    uninstrument_agno,
+    uninstrument_anthropic,
+    uninstrument_google_genai,
+    uninstrument_langchain,
+    uninstrument_litellm,
+    uninstrument_mcp,
+    uninstrument_openai,
+    uninstrument_openai_agents,
+    uninstrument_smolagents,
+)
+
+__all__ = [
+    "configure",
+    "instrument",
+    "instrument_agno",
+    "instrument_anthropic",
+    "instrument_google_genai",
+    "instrument_langchain",
+    "instrument_litellm",
+    "instrument_mcp",
+    "instrument_openai",
+    "instrument_openai_agents",
+    "instrument_smolagents",
+    "mark_failure",
+    "mark_success",
+    "uninstrument_agno",
+    "uninstrument_anthropic",
+    "uninstrument_google_genai",
+    "uninstrument_langchain",
+    "uninstrument_litellm",
+    "uninstrument_mcp",
+    "uninstrument_openai",
+    "uninstrument_openai_agents",
+    "uninstrument_smolagents",
+]
