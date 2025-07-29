04ac2ba097a3d54dd0330e55736c387fd1210b5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:46 +0000

    Add package codeforlife/0.27.10/codeforlife-0.27.10-py3-none-any.whl

---
+++ packages/codeforlife/codeforlife-0.27.10-py3-none-any.whl/codeforlife/permissions/__init__.py
@@ -0,0 +1,12 @@
+"""
+© Ocado Group
+Created on 23/01/2024 at 16:38:07(+00:00).
+
+Reusable DRF permissions.
+"""
+
+from .allow_any import AllowAny
+from .allow_none import AllowNone
+from .base import BasePermission
+from .is_authenticated import IsAuthenticated
+from .operators import AND, NOT, OR, Permission
