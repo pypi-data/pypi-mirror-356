953adea752cd6325c3a613bf2626aae66d2a0c66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:12 +0000

    Add package victron-mqtt/2025.6.10/victron_mqtt-2025.6.10.tar.gz

---
+++ packages/victron-mqtt/victron_mqtt-2025.6.10.tar.gz/victron_mqtt-2025.6.10/tests/deviceandmetrics_test.py
@@ -0,0 +1,23 @@
+"""Tests that devices and metrics can be enumerated."""
+
+import pytest
+import victron_mqtt  # pylint: disable=import-error
+
+
+@pytest.mark.asyncio
+async def test_devices_and_metrics(config_host, config_port, config_username, config_password, config_use_ssl):
+    hub = victron_mqtt.Hub(config_host, config_port, config_username, config_password, config_use_ssl)
+    await hub.connect()
+
+    assert len(hub.devices) > 0
+
+    for device in hub.devices:
+        assert len(device.metrics) > 0
+        assert device.device_type is not None
+        assert device.device_type != victron_mqtt.DeviceType.ANY
+
+        for metric in device.metrics:
+            assert metric.short_id is not None
+            assert len(metric.short_id) > 0
+
+    await hub.disconnect()
