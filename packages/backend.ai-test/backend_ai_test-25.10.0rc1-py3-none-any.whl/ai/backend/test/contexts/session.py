edcb85f3ccd83610a395fe92ea0e6cfef8b01fff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:08:48 +0000

    Add package backend.ai-test/25.10.0rc1/backend_ai_test-25.10.0rc1-py3-none-any.whl

---
+++ packages/backend.ai-test/backend_ai_test-25.10.0rc1-py3-none-any.whl/ai/backend/test/contexts/session.py
@@ -0,0 +1,59 @@
+from typing import override
+
+from ai.backend.test.contexts.context import BaseTestContext, ContextName
+from ai.backend.test.data.session import CreatedSessionMeta, SessionDependency
+from ai.backend.test.tester.dependency import (
+    BatchSessionDep,
+    ClusterDep,
+    CodeExecutionDep,
+    SessionDep,
+)
+
+
+class SessionContext(BaseTestContext[SessionDep]):
+    @override
+    @classmethod
+    def name(cls) -> ContextName:
+        return ContextName.SESSION
+
+
+class BatchSessionContext(BaseTestContext[BatchSessionDep]):
+    @override
+    @classmethod
+    def name(cls) -> ContextName:
+        return ContextName.BATCH_SESSION
+
+
+class ClusterContext(BaseTestContext[ClusterDep]):
+    @override
+    @classmethod
+    def name(cls) -> ContextName:
+        return ContextName.CLUSTER_CONFIG
+
+
+class CodeExecutionContext(BaseTestContext[CodeExecutionDep]):
+    @override
+    @classmethod
+    def name(cls) -> ContextName:
+        return ContextName.CODE_EXECUTION
+
+
+class CreatedSessionTemplateIDContext(BaseTestContext[str]):
+    @override
+    @classmethod
+    def name(cls) -> ContextName:
+        return ContextName.CREATED_SESSION_TEMPLATE_ID
+
+
+class CreatedSessionMetaContext(BaseTestContext[CreatedSessionMeta]):
+    @override
+    @classmethod
+    def name(cls) -> ContextName:
+        return ContextName.CREATED_SESSION_META
+
+
+class SessionDependencyContext(BaseTestContext[SessionDependency]):
+    @override
+    @classmethod
+    def name(cls) -> ContextName:
+        return ContextName.SESSION_DEPENDENCY
