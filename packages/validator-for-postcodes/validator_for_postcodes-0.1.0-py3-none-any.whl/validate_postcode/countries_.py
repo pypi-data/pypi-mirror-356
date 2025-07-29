1e7a5effffa2f68a797fcf9308b74ce154e4ed99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:41 +0000

    Add package validator-for-postcodes/0.1.0/validator_for_postcodes-0.1.0-py3-none-any.whl

---
+++ packages/validator-for-postcodes/validator_for_postcodes-0.1.0-py3-none-any.whl/validate_postcode/countries_.py
@@ -0,0 +1,15 @@
+from validate_postcode.countries.uk.postcode import UKPostcode
+
+POSTCODES = {
+    "uk": UKPostcode,
+}
+
+
+class Countries:
+    UK = "UK"
+
+
+def postcode_factory(country):
+    if (postcode := POSTCODES.get(country)) is not None:
+        return postcode
+    raise KeyError(f"Unsupported country: {country}")
