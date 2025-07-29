f31bc2d1829adaf29f3f16f05cf235c95968473e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:53 +0000

    Add package piworld/0.2.0/piworld-0.2.0.tar.gz

---
+++ packages/piworld/piworld-0.2.0.tar.gz/piworld-0.2.0/piworld/__init__.py
@@ -0,0 +1,12 @@
+from ._core import pi10, pi20, pi50, pi100, pi200, pi500, pidigit, pislice
+
+__all__ = (
+    "pi10",
+    "pi20",
+    "pi50",
+    "pi100",
+    "pi200",
+    "pi500",
+    "pidigit",
+    "pislice"
+)
