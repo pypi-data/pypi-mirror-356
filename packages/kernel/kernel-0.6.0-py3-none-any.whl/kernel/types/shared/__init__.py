b41dd366a04a8602400671e77491d08d48ac30ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:20 +0000

    Add package kernel/0.6.0/kernel-0.6.0-py3-none-any.whl

---
+++ packages/kernel/kernel-0.6.0-py3-none-any.whl/kernel/types/shared/__init__.py
@@ -0,0 +1,6 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .log_event import LogEvent as LogEvent
+from .error_event import ErrorEvent as ErrorEvent
+from .error_model import ErrorModel as ErrorModel
+from .error_detail import ErrorDetail as ErrorDetail
