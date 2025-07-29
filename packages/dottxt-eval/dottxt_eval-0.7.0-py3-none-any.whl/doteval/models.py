0ec7d430233949fb859d77a042b0a0e6de910884
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:37 +0000

    Add package dottxt-eval/0.7.0/dottxt_eval-0.7.0-py3-none-any.whl

---
+++ packages/dottxt-eval/dottxt_eval-0.7.0-py3-none-any.whl/doteval/models.py
@@ -0,0 +1,62 @@
+import time
+from collections import defaultdict
+from dataclasses import dataclass, field
+from typing import Any, Optional, Union
+
+from doteval.metrics import Metric
+
+
+@dataclass
+class Sample:
+    prompt: str
+    scores: list["Score"]
+    
+    def __init__(self, prompt: str, scores: Union["Score", list["Score"]]):
+        self.prompt = prompt
+        if isinstance(scores, list):
+            self.scores = scores
+        else:
+            self.scores = [scores]
+
+
+@dataclass
+class Score:
+    name: str
+    value: Any
+    metrics: list[Metric]
+    metadata: dict[str, Any] = field(default_factory=dict)
+
+
+@dataclass
+class EvaluationResult:
+    """Results of a single evaluation step"""
+
+    sample: Sample
+    item_id: int
+    dataset_row: dict[str, Any] = field(default_factory=dict)
+    error: Optional[str] = None
+    timestamp: float = field(default_factory=time.time)
+
+
+class EvaluationSummary:
+    """Aggregated results of a full evaluation"""
+
+    def __init__(self, results: list[EvaluationResult]):
+        self.results = results
+        self.summary = self.compute_summary()
+
+    def compute_summary(self):
+        summary = defaultdict(dict)
+
+        # Regorganize the results by evaluator and metric
+        aggregated_results = defaultdict(lambda: defaultdict(list))
+        for results in self.results:
+            for score in results.sample.scores:
+                for metric in score.metrics:
+                    aggregated_results[score.name][metric].append(score.value)
+
+        for evaluator_name, metrics_values in aggregated_results.items():
+            for metrics, values in metrics_values.items():
+                summary[evaluator_name][metrics.__name__] = metrics(values)
+
+        return summary
