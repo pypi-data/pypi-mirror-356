28a126873a39c739f35e2a117b287da295c24e6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:20 +0000

    Add package singlestoredb/1.14.2/singlestoredb-1.14.2-cp38-abi3-win32.whl

---
+++ packages/singlestoredb/singlestoredb-1.14.2-cp38-abi3-win32.whl/singlestoredb/management/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from .cluster import manage_cluster
+from .files import manage_files
+from .manager import get_token
+from .workspace import get_organization
+from .workspace import get_secret
+from .workspace import get_stage
+from .workspace import manage_workspaces
