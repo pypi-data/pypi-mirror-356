8f00c1927b790efeecbe443322b82b4c9026cd52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:57:41 +0000

    Add package hubble-shuttle/0.6.2/hubble_shuttle-0.6.2-py3-none-any.whl

---
+++ packages/hubble-shuttle/hubble_shuttle-0.6.2-py3-none-any.whl/hubble_shuttle/api.py
@@ -0,0 +1,55 @@
+
+from .http import RequestsShuttleTransport
+
+class ShuttleAPI:
+
+    transport = RequestsShuttleTransport
+
+    api_endpoint = None
+    headers = {}
+    query = {}
+    request_content_type = "application/x-www-form-urlencoded"
+    locale = None
+
+    def __init__(self, **kwargs):
+        if "api_endpoint" in kwargs:
+            self.api_endpoint = kwargs["api_endpoint"]
+        if "headers" in kwargs:
+            self.headers = kwargs["headers"]
+        if "query" in kwargs:
+            self.query = kwargs["query"]
+        if "request_content_type" in kwargs:
+            self.request_content_type = kwargs["request_content_type"]
+        if "locale" in kwargs:
+            self.locale = kwargs["locale"]
+
+        # Create a copy of the headers to avoid modifying the original
+        headers = {
+            **self.headers,
+        }
+        if self.locale is not None:
+            headers["Accept-Language"] = self.locale
+
+        self.http = self.transport(
+            api_endpoint = self.api_endpoint,
+            headers = headers,
+            query = self.query,
+            request_content_type = self.request_content_type,
+            service_name = type(self).__name__,
+        )
+
+    def http_get(self, url, **kwargs):
+        return self.http.get(url, **kwargs)
+
+    def http_post(self, url, **kwargs):
+        return self.http.post(url, **kwargs)
+
+    def http_put(self, url, **kwargs):
+        return self.http.put(url, **kwargs)
+
+    def http_patch(self, url, **kwargs):
+        return self.http.patch(url, **kwargs)
+
+    def http_delete(self, url, **kwargs):
+        return self.http.delete(url, **kwargs)
+
