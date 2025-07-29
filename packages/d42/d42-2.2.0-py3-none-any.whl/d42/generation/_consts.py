629986d810c23dc36cf73d64b80016d9cf2ebca1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:39 +0000

    Add package d42/2.2.0/d42-2.2.0-py3-none-any.whl

---
+++ packages/d42/d42-2.2.0-py3-none-any.whl/d42/generation/_consts.py
@@ -0,0 +1,16 @@
+import string
+import sys
+
+INT_MIN = -(2 ** 63)
+INT_MAX = 2 ** 63 - 1
+FLOAT_MIN = float(INT_MIN)
+FLOAT_MAX = float(INT_MAX)
+STR_ALPHABET = string.digits + string.ascii_letters + " -_"
+STR_LEN_MIN = 0
+STR_LEN_MAX = 32
+LIST_LEN_MIN = 0
+LIST_LEN_MAX = 16
+BYTES_LEN_MIN = 0
+BYTES_LEN_MAX = 32
+
+MAX_UNIQUE_GENERATION_ATTEMPTS = sys.getrecursionlimit()
