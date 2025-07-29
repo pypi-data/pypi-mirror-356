ad24dd5b5354a565b4b982ec4d5eba72c6286d48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:26 +0000

    Add package Package-Mehdi/0.0.4/package_mehdi-0.0.4.tar.gz

---
+++ packages/Package-Mehdi/package_mehdi-0.0.4.tar.gz/package_mehdi-0.0.4/src/Package_Mehdi/Mehdi.py
@@ -0,0 +1,62 @@
+def Mehdi_print():
+    """
+    Affiche un message simple.
+
+    Usage:
+        Mehdi_print()
+
+    Sortie:
+        Affiche "hello I am Mehdi" dans la console.
+    """
+    print("hello I am Mehdi")
+
+def Mehdi_add(a, b):
+    """
+    Calcule la somme de deux nombres.
+
+    Args:
+        a (int ou float): Premier nombre.
+        b (int ou float): Deuxième nombre.
+
+    Returns:
+        int ou float: La somme de a et b.
+
+    Exemple:
+        >>> Mehdi_add(2, 3)
+        5
+    """
+    return a + b
+
+def Mehdi_subtract(a, b):
+    """
+    Calcule la différence entre deux nombres.
+
+    Args:
+        a (int ou float): Premier nombre.
+        b (int ou float): Deuxième nombre.
+
+    Returns:
+        int ou float: La différence a - b.
+
+    Exemple:
+        >>> Mehdi_subtract(5, 2)
+        3
+    """
+    return a - b
+
+def Mehdi_multiply(a, b):
+    """
+    Calcule le produit de deux nombres.
+
+    Args:
+        a (int ou float): Premier nombre.
+        b (int ou float): Deuxième nombre.
+
+    Returns:
+        int ou float: Le produit a * b.
+
+    Exemple:
+        >>> Mehdi_multiply(4, 3)
+        12
+    """
+    return a * b
