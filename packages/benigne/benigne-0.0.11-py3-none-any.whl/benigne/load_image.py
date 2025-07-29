3f1fa42926ac00dd139debcbce30d10707042a12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:34 +0000

    Add package benigne/0.0.11/benigne-0.0.11-py3-none-any.whl

---
+++ packages/benigne/benigne-0.0.11-py3-none-any.whl/benigne/load_image.py
@@ -0,0 +1,18 @@
+from numpy import array
+
+def ft_load(path: str) -> array:
+    """
+    Load an image file and return its content.
+    
+    :return: The content of the image file.
+    """
+    import os
+    from PIL import Image    
+    
+    if not os.path.exists(path):
+        print("File not found.")
+        return None
+    with Image.open(path) as img:
+        img_array = array(img)
+    print(f"The shape of image is: {img_array.shape}")
+    return img_array
