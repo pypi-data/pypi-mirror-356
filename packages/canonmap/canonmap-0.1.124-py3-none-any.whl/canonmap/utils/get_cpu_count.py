ab19e98b5d8102d8be0d42771c2b9d2d511c5069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:07:00 +0000

    Add package canonmap/0.1.124/canonmap-0.1.124-py3-none-any.whl

---
+++ packages/canonmap/canonmap-0.1.124-py3-none-any.whl/canonmap/utils/get_cpu_count.py
@@ -0,0 +1,10 @@
+import multiprocessing
+import logging
+
+from canonmap.utils.logger import get_logger
+
+logger = get_logger("get_cpu_count")
+
+def get_cpu_count():
+    count = multiprocessing.cpu_count()
+    return count
