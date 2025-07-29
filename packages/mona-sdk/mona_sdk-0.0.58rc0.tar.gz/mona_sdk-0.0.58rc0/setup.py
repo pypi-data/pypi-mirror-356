903b61e582ff339715a6dc57d77f5186d21e9285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:09:04 +0000

    Add package mona-sdk/0.0.58rc0/mona_sdk-0.0.58rc0.tar.gz

---
+++ packages/mona-sdk/mona_sdk-0.0.58rc0.tar.gz/mona_sdk-0.0.58rc0/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mona_sdk",
+    version="0.0.58rc",
+    author="MonaLabs",
+    author_email="sdk@monalabs.io",
+    description="SDK for communicating with Mona's servers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/monalabs/mona-sdk",
+    download_url="https://pypi.python.org/pypi/mona-sdk/",
+    install_requires=[
+        "pyjwt",
+        "python-jose>=3.2.0",
+        "requests-mock>=1.8.0",
+        "dataclasses==0.8; python_version<'3.7'",
+        "cachetools",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+)
