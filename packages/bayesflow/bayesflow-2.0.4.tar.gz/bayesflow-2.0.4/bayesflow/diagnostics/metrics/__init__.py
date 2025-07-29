3c80e44649f0c4d2f20f8a9a78e1756794107bcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:08:30 +0000

    Add package bayesflow/2.0.4/bayesflow-2.0.4.tar.gz

---
+++ packages/bayesflow/bayesflow-2.0.4.tar.gz/bayesflow-2.0.4/bayesflow/diagnostics/metrics/__init__.py
@@ -0,0 +1,6 @@
+from .calibration_error import calibration_error
+from .posterior_contraction import posterior_contraction
+from .root_mean_squared_error import root_mean_squared_error
+from .expected_calibration_error import expected_calibration_error
+from .classifier_two_sample_test import classifier_two_sample_test
+from .model_misspecification import bootstrap_comparison, summary_space_comparison
