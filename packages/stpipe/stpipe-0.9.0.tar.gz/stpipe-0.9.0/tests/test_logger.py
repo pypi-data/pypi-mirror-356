ad6706293a5ca5952d1ae344bed643e28a3faa00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:59 +0000

    Add package stpipe/0.9.0/stpipe-0.9.0.tar.gz

---
+++ packages/stpipe/stpipe-0.9.0.tar.gz/stpipe-0.9.0/tests/test_logger.py
@@ -0,0 +1,73 @@
+import io
+import logging
+
+import pytest
+
+from stpipe import log as stpipe_log
+
+
+@pytest.fixture(autouse=True)
+def _clean_up_logging():
+    yield
+    logging.shutdown()
+    stpipe_log.load_configuration(io.BytesIO(stpipe_log.DEFAULT_CONFIGURATION))
+
+
+def test_configuration(tmp_path):
+    logfilename = tmp_path / "output.log"
+
+    configuration = f"""
+[.]
+handler = file:{logfilename}
+break_level = ERROR
+level = WARNING
+format = '%(message)s'
+"""
+
+    with io.StringIO() as fd:
+        fd.write(configuration)
+        fd.seek(0)
+        stpipe_log.load_configuration(fd)
+
+    log = stpipe_log.getLogger(stpipe_log.STPIPE_ROOT_LOGGER)
+
+    log.info("Hidden")
+    log.warning("Shown")
+
+    with pytest.raises(stpipe_log.LoggedException):
+        log.critical("Breaking")
+
+    logging.shutdown()
+
+    with open(logfilename) as fd:
+        lines = [x.strip() for x in fd.readlines()]
+
+    assert lines == ["Shown", "Breaking"]
+
+
+def test_record_logs():
+    stpipe_logger = stpipe_log.getLogger(stpipe_log.STPIPE_ROOT_LOGGER)
+    root_logger = stpipe_log.getLogger()
+
+    assert not any(
+        isinstance(h, stpipe_log.RecordingHandler) for h in root_logger.handlers
+    )
+
+    with stpipe_log.record_logs(
+        level=logging.ERROR, formatter=logging.Formatter("%(message)s")
+    ) as log_records:
+        stpipe_logger.warning("Warning from stpipe")
+        stpipe_logger.error("Error from stpipe")
+        root_logger.warning("Warning from root")
+        root_logger.error("Error from root")
+
+    assert not any(
+        isinstance(h, stpipe_log.RecordingHandler) for h in root_logger.handlers
+    )
+
+    stpipe_logger.error("Additional error from stpipe")
+    root_logger.error("Additional error from root")
+
+    assert len(log_records) == 2
+    assert log_records[0] == "Error from stpipe"
+    assert log_records[1] == "Error from root"
