025656650cb034793f60bf01b5261fb6efaf1f15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:08:18 +0000

    Add package shuke-ai/0.1.0/shuke_ai-0.1.0.tar.gz

---
+++ packages/shuke-ai/shuke_ai-0.1.0.tar.gz/shuke_ai-0.1.0/shuke_ai/main.py
@@ -0,0 +1,15 @@
+from PyQt6.QtWidgets import QApplication
+from .view.main_window import MainWindow
+from .utils.logger import logger
+import sys
+import argparse
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()
+
