01b02595b6e9e14f0762ff754441f4322187c73c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:57 +0000

    Add package omnirgress/1.1.0/omnirgress-1.1.0-py3-none-any.whl

---
+++ packages/omnirgress/omnirgress-1.1.0-py3-none-any.whl/omniregress/__init__.py
@@ -0,0 +1,5 @@
+from .linear_regression import LinearRegression
+from .polynomial_regression import PolynomialRegression
+
+__version__ = "1.0.0"
+__all__ = ['LinearRegression', 'PolynomialRegression']
