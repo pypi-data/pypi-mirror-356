bcd448df2c2b464a6229c5976a9e404cc576c297
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:50 +0000

    Add package matplot2tikz/0.3.3/matplot2tikz-0.3.3.tar.gz

---
+++ packages/matplot2tikz/matplot2tikz-0.3.3.tar.gz/matplot2tikz-0.3.3/tests/test_viridis.py
@@ -0,0 +1,28 @@
+"""Test _quadmesh.py."""
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.figure import Figure
+
+mpl.use("Agg")
+
+
+def plot() -> Figure:
+    x, y = np.meshgrid(np.linspace(0, 1), np.linspace(0, 1))
+    z = x**2 - y**2
+
+    fig = plt.figure()
+    plt.pcolormesh(x, y, z, cmap=plt.get_cmap("viridis"), shading="gouraud")
+
+    return fig
+
+
+def test() -> None:
+    from .helpers import assert_equality
+
+    # test relative data path
+    assert_equality(
+        plot,
+        __file__[:-3] + "_reference.tex",
+    )
