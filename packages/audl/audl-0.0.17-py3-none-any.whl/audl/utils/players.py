eaf49693ea546b3e39af601af11e854c1ab32776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:28 +0000

    Add package audl/0.0.17/audl-0.0.17-py3-none-any.whl

---
+++ packages/audl/audl-0.0.17-py3-none-any.whl/audl/utils/players.py
@@ -0,0 +1,11 @@
+import requests
+
+from typing import List
+
+def get_all_players_ext_ids():
+    """
+    Function that fetch all players external ids
+    """
+    base_url = "https://www.watchufa.com/league/players"
+    pass
+
