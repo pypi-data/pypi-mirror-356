ddddc157dd930b0620520d8241b479b82b8473c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:58 +0000

    Add package kurra/1.5.3/kurra-1.5.3.tar.gz

---
+++ packages/kurra/kurra-1.5.3.tar.gz/kurra-1.5.3/tests/test_cli/test_cli_db_delete.py
@@ -0,0 +1,54 @@
+from typer.testing import CliRunner
+
+from kurra.cli import app
+
+runner = CliRunner()
+
+
+def test_cli_db_delete(fuseki_container):
+    port = fuseki_container.get_exposed_port(3030)
+    result = runner.invoke(
+        app,
+        [
+            "db",
+            "list",
+            f"http://localhost:{port}",
+            "--username",
+            "admin",
+            "--password",
+            "admin",
+        ],
+    )
+    assert result.exit_code == 0
+    assert "'ds.name': '/ds'" in result.output
+
+    result = runner.invoke(
+        app,
+        [
+            "db",
+            "delete",
+            f"http://localhost:{port}",
+            "ds",
+            "--username",
+            "admin",
+            "--password",
+            "admin",
+        ],
+    )
+    assert result.exit_code == 0
+    assert "Dataset ds deleted." in result.output
+
+    result = runner.invoke(
+        app,
+        [
+            "db",
+            "list",
+            f"http://localhost:{port}",
+            "--username",
+            "admin",
+            "--password",
+            "admin",
+        ],
+    )
+    assert result.exit_code == 0
+    assert "'ds.name': '/ds'" not in result.output
