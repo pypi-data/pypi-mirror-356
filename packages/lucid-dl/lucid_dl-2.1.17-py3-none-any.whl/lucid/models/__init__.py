b96044da836853e58277a58fcaf138fb40c97514
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:09:04 +0000

    Add package lucid-dl/2.1.17/lucid_dl-2.1.17-py3-none-any.whl

---
+++ packages/lucid-dl/lucid_dl-2.1.17-py3-none-any.whl/lucid/models/__init__.py
@@ -0,0 +1,4 @@
+from lucid.models.conv import *
+from lucid.models.transformer import *
+
+from ._util import summarize, get_model_names
---
+++ packages/lucid-dl/lucid_dl-2.1.17-py3-none-any.whl/lucid/models/conv/__init__.py
@@ -0,0 +1,22 @@
+# Image Classification
+from .lenet import *
+from .alex import *
+from .zfnet import *
+from .vgg import *
+from .inception import *
+from .inception_res import *
+from .resnet import *
+from .resnext import *
+from .resnest import *
+from .senet import *
+from .sknet import *
+from .dense import *
+from .xception import *
+from .mobile import *
+from .efficient import *
+from .convnext import *
+from .inception_next import *
+from .coatnet import *
+
+# Object Detection
+from .rcnn import *
