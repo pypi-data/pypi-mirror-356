6fe36b54cba54791c321c25e56d6bfedd406b8d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:57 +0000

    Add package airflow-common-operators/0.5.2/airflow_common_operators-0.5.2.tar.gz

---
+++ packages/airflow-common-operators/airflow_common_operators-0.5.2.tar.gz/airflow_common_operators-0.5.2/airflow_common_operators/__init__.py
@@ -0,0 +1,6 @@
+from .airflow import *
+from .library import *
+from .models import *
+from .utility import *
+
+__version__ = "0.5.2"
---
+++ packages/airflow-common-operators/airflow_common_operators-0.5.2.tar.gz/airflow_common_operators-0.5.2/airflow_common_operators/models/__init__.py
@@ -0,0 +1,6 @@
+from pkn import Dict, List  # noqa: F401
+
+from .base import *
+from .bash import *
+from .clean import *
+from .operators import *
