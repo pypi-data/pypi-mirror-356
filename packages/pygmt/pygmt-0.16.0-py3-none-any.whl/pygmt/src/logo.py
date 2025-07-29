362bfe18c5d11d6091cab84d9b0707a915255c21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:01 +0000

    Add package pygmt/0.16.0/pygmt-0.16.0-py3-none-any.whl

---
+++ packages/pygmt/pygmt-0.16.0-py3-none-any.whl/pygmt/src/logo.py
@@ -0,0 +1,59 @@
+"""
+logo - Plot the GMT logo.
+"""
+
+from pygmt.clib import Session
+from pygmt.helpers import build_arg_list, fmt_docstring, kwargs_to_strings, use_alias
+
+
+@fmt_docstring
+@use_alias(
+    R="region",
+    J="projection",
+    D="position",
+    F="box",
+    S="style",
+    V="verbose",
+    c="panel",
+    t="transparency",
+)
+@kwargs_to_strings(R="sequence", c="sequence_comma", p="sequence")
+def logo(self, **kwargs):
+    r"""
+    Plot the GMT logo.
+
+    By default, the GMT logo is 2 inches wide and 1 inch high and
+    will be positioned relative to the current plot origin.
+    Use various options to change this and to place a transparent or
+    opaque rectangular map panel behind the GMT logo.
+
+    Full GMT docs at :gmt-docs:`gmtlogo.html`.
+
+    {aliases}
+
+    Parameters
+    ----------
+    {projection}
+    {region}
+    position : str
+        [**g**\|\ **j**\|\ **J**\|\ **n**\|\ **x**]\ *refpoint*\
+        **+w**\ *width*\ [**+j**\ *justify*]\ [**+o**\ *dx*\ [/*dy*]].
+        Set reference point on the map for the image.
+    box : bool or str
+        If set to ``True``, draw a rectangular border around the
+        GMT logo.
+    style : str
+        [**l**\|\ **n**\|\ **u**].
+        Control what is written beneath the map portion of the logo.
+
+        - **l** to plot the text label "The Generic Mapping Tools"
+          [Default]
+        - **n** to skip the label placement
+        - **u** to place the URL to the GMT site
+    {verbose}
+    {panel}
+    {transparency}
+    """
+    self._activate_figure()
+    with Session() as lib:
+        lib.call_module(module="logo", args=build_arg_list(kwargs))
---
+++ packages/pygmt/pygmt-0.16.0-py3-none-any.whl/pygmt/src/solar.py
@@ -0,0 +1,122 @@
+"""
+solar - Plot day-night terminators and other sunlight parameters.
+"""
+
+from typing import Literal
+
+import pandas as pd
+from pygmt.clib import Session
+from pygmt.exceptions import GMTInvalidInput
+from pygmt.helpers import build_arg_list, fmt_docstring, kwargs_to_strings, use_alias
+
+__doctest_skip__ = ["solar"]
+
+
+@fmt_docstring
+@use_alias(
+    B="frame",
+    G="fill",
+    J="projection",
+    R="region",
+    T="terminator/terminator_datetime-",
+    V="verbose",
+    W="pen",
+    c="panel",
+    p="perspective",
+    t="transparency",
+)
+@kwargs_to_strings(R="sequence", c="sequence_comma", p="sequence")
+def solar(
+    self,
+    terminator: Literal["astronomical", "civil", "day_night", "nautical"] = "day_night",
+    terminator_datetime=None,
+    **kwargs,
+):
+    r"""
+    Plot day-night terminators and other sunlight parameters.
+
+    This function plots the day-night terminator. Alternatively, it can plot the
+    terminators for civil twilight, nautical twilight, or astronomical twilight.
+
+    Full GMT docs at :gmt-docs:`solar.html`.
+
+    {aliases}
+
+    Parameters
+    ----------
+    terminator
+        Set the type of terminator displayed, which can be set with either the full name
+        or the first letter of the name. Available options are:
+
+        - ``"astronomical"``: Astronomical twilight
+        - ``"civil"``: Civil twilight
+        - ``"day_night"``: Day-night terminator
+        - ``"nautical"``: Nautical twilight
+
+        Refer to https://en.wikipedia.org/wiki/Twilight for the definitions of different
+        types of twilight.
+    terminator_datetime : str or datetime object
+        Set the UTC date and time of the displayed terminator [Default is the current
+        UTC date and time]. It can be passed as a string or Python datetime object.
+    {region}
+    {projection}
+    {frame}
+    fill : str
+        Set color or pattern for filling terminators [Default is no fill].
+    pen : str
+        Set pen attributes for lines [Default is ``"0.25p,black,solid"``].
+    {verbose}
+    {panel}
+    {perspective}
+    {transparency}
+
+    Example
+    -------
+    >>> # import the Python module "datetime"
+    >>> import datetime
+    >>> import pygmt
+    >>> # create a datetime object at 8:52:18 on June 24, 1997 (time in UTC)
+    >>> date = datetime.datetime(
+    ...     year=1997, month=6, day=24, hour=8, minute=52, second=18
+    ... )
+    >>> # create a new plot with pygmt.Figure()
+    >>> fig = pygmt.Figure()
+    >>> # create a map of the Earth with the coast method
+    >>> fig.coast(land="darkgreen", water="lightblue", projection="W10c", region="d")
+    >>> fig.solar(
+    ...     # set the terminator to "day_night"
+    ...     terminator="day_night",
+    ...     # pass the datetime object
+    ...     terminator_datetime=date,
+    ...     # fill the night-section with navyblue at 75% transparency
+    ...     fill="navyblue@75",
+    ...     # draw the terminator with a 1-point black line
+    ...     pen="1p,black",
+    ... )
+    >>> # show the plot
+    >>> fig.show()
+    """
+    self._activate_figure()
+    if kwargs.get("T") is not None:
+        msg = "Use 'terminator' and 'terminator_datetime' instead of 'T'."
+        raise GMTInvalidInput(msg)
+
+    valid_terminators = ["day_night", "civil", "nautical", "astronomical"]
+    if terminator not in valid_terminators and terminator not in "dcna":
+        msg = (
+            f"Unrecognized solar terminator type '{terminator}'. "
+            f"Valid values are {valid_terminators}."
+        )
+        raise GMTInvalidInput(msg)
+    kwargs["T"] = terminator[0]
+    if terminator_datetime:
+        try:
+            datetime_string = pd.to_datetime(terminator_datetime).strftime(
+                "%Y-%m-%dT%H:%M:%S.%f"
+            )
+        except ValueError as verr:
+            msg = "Unrecognized datetime format."
+            raise GMTInvalidInput(msg) from verr
+        kwargs["T"] += f"+d{datetime_string}"
+    with Session() as lib:
+        lib.call_module(module="solar", args=build_arg_list(kwargs))
