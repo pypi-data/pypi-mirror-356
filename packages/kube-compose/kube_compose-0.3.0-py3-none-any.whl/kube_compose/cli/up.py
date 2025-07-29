29dbe1491aa1aedd3c15d58ca0699e200238e0fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:08:25 +0000

    Add package kube-compose/0.3.0/kube_compose-0.3.0-py3-none-any.whl

---
+++ packages/kube-compose/kube_compose-0.3.0-py3-none-any.whl/kube_compose/cli/up.py
@@ -0,0 +1,36 @@
+import subprocess
+from kube_compose.cli import cli
+from kube_compose import utils
+
+@cli.command()
+@utils.require_binaries(helm='helm', kubectl='kubectl')
+@utils.require_kube_compose_release
+def up(*, helm, kubectl, name, namespace, docker_compose_config_raw, docker_compose_config, **_):
+  ''' Like `docker-compose up` but runs in the kubernetes cluster
+  '''
+  from kube_compose.cli.volume.create import create
+  create(volume=None)
+  if subprocess.call([
+    *helm, 'status', *(('-n', namespace) if namespace else tuple()), name
+  ], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) == 0:
+    utils.run([
+      *helm, 'upgrade',
+      *(('-n', namespace) if namespace else tuple()),
+      name,
+      utils.helm_chart,
+      '-f', '-'
+    ], input=docker_compose_config_raw)
+  else:
+    utils.run([
+      *helm, 'install',
+      *(('-n', namespace) if namespace else tuple()),
+      name,
+      utils.helm_chart,
+      '-f', '-',
+    ], input=docker_compose_config_raw)
+  #
+  utils.run([
+    *kubectl, 'rollout', 'status',
+    *(('-n', namespace) if namespace else tuple()),
+    *[f"deploy/{service}" for service in docker_compose_config.get('services', {})],
+  ])
