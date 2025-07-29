654cd8289726042ef72c4b2b1096d2f84e45af13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:31 +0000

    Add package upstoxalgo/0.3.7/upstoxalgo-0.3.7-py3-none-any.whl

---
+++ packages/upstoxalgo/upstoxalgo-0.3.7-py3-none-any.whl/upstoxalgo/__init__.py
@@ -0,0 +1,6 @@
+from upstoxalgo.imports_and_instrument_token import *
+from upstoxalgo.upstox import *
+from upstoxalgo.upstox_auto_auth_code import *
+from upstoxalgo.utils import *
+
+__version__ = "0.3.6"
