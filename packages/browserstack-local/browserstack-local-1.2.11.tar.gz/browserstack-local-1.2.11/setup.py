4d7518c3cc6409d6b060ad5aed70bbd510a7cc36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:37 +0000

    Add package browserstack-local/1.2.11/browserstack-local-1.2.11.tar.gz

---
+++ packages/browserstack-local/browserstack-local-1.2.11.tar.gz/browserstack-local-1.2.11/setup.py
@@ -0,0 +1,22 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+setup(
+  name = 'browserstack-local',
+  packages = ['browserstack'],
+  version = '1.2.11',
+  description = 'Python bindings for Browserstack Local',
+  long_description=open('README.md').read(),
+  long_description_content_type='text/markdown',
+  author = 'BrowserStack',
+  author_email = 'support@browserstack.com',
+  url = 'https://github.com/browserstack/browserstack-local-python',
+  download_url = 'https://github.com/browserstack/browserstack-local-python/archive/master.zip',
+  keywords = ['BrowserStack', 'Local', 'selenium', 'testing'],
+  classifiers = [],
+  install_requires=[
+        'psutil',
+  ],
+)
+
