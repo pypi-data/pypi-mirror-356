53cc15e41d0e0e30884e64059f16c91c914e9eb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:42 +0000

    Add package ob-metaflow-stubs/6.0.3.182rc1/ob_metaflow_stubs-6.0.3.182rc1-py2.py3-none-any.whl

---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.3.182rc1-py2.py3-none-any.whl/metaflow-stubs/ob_internal.pyi
@@ -0,0 +1,12 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.17.1+obcheckpoint(0.2.1);ob(v1)                                                   #
+# Generated on 2025-06-18T10:06:22.574940                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from .mf_extensions.outerbounds.plugins.kubernetes.pod_killer import PodKiller as PodKiller
+from .mf_extensions.outerbounds.plugins.fast_bakery.baker import bake_image as bake_image
+
