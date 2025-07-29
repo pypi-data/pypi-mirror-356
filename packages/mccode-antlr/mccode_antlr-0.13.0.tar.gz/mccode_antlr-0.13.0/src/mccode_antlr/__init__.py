ece5f9b5d4631f80eb2d66b484dbe845fe0f22ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:13 +0000

    Add package mccode-antlr/0.13.0/mccode_antlr-0.13.0.tar.gz

---
+++ packages/mccode-antlr/mccode_antlr-0.13.0.tar.gz/mccode_antlr-0.13.0/src/mccode_antlr/__init__.py
@@ -0,0 +1,31 @@
+"""Monte Carlo Particle Ray Tracing compiler, Volume 4"""
+import mccode_antlr.grammar
+__author__ = "Gregory Tucker"
+__affiliation__ = "European Spallation Source ERIC"
+
+from .version import version
+__version__ = version()
+
+from enum import IntEnum
+
+class Flavor(IntEnum):
+    """The specializations of the McCode language.
+
+    Enumerated _values_ follow the McCode _project_ number.
+    """
+    MCSTAS=1
+    MCXTRACE=2
+
+    def __str__(self):
+        options = {Flavor.MCSTAS: 'McStas', Flavor.MCXTRACE: 'McXTrace'}
+        return options[self]
+
+    def url(self) -> str:
+        options = {
+            Flavor.MCSTAS: 'https://www.mcstas.org',
+            Flavor.MCXTRACE: 'https://www.mcxtrace.org'
+        }
+        return options[self]
+
+
+__all__ = ["__author__", "__affiliation__", "__version__", "version", "Flavor"]
