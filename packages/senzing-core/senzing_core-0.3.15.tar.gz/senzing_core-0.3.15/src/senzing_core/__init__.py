afb5893a0d43d292160b52de03ef8f290877a6cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:22 +0000

    Add package senzing-core/0.3.15/senzing_core-0.3.15.tar.gz

---
+++ packages/senzing-core/senzing_core-0.3.15.tar.gz/senzing_core-0.3.15/src/senzing_core/__init__.py
@@ -0,0 +1,16 @@
+from .szabstractfactory import SzAbstractFactoryCore, SzAbstractFactoryParametersCore
+from .szconfig import SzConfigCore
+from .szconfigmanager import SzConfigManagerCore
+from .szdiagnostic import SzDiagnosticCore
+from .szengine import SzEngineCore
+from .szproduct import SzProductCore
+
+__all__ = [
+    "SzAbstractFactoryCore",
+    "SzAbstractFactoryParametersCore",
+    "SzConfigCore",
+    "SzConfigManagerCore",
+    "SzDiagnosticCore",
+    "SzEngineCore",
+    "SzProductCore",
+]
