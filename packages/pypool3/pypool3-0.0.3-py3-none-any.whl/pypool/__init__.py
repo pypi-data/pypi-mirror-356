73b365c5105ce7c4dacb6661498177e59b71bceb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:30 +0000

    Add package pypool3/0.0.3/pypool3-0.0.3-py3-none-any.whl

---
+++ packages/pypool3/pypool3-0.0.3-py3-none-any.whl/pypool/__init__.py
@@ -0,0 +1,16 @@
+"""
+Python Resource Pool Implementation
+"""
+
+#** Variables **#
+__all__ = [
+    'PoolError',
+    'Empty',
+    'Full',
+    'Timeout',
+
+    'Pool',
+]
+
+#** Imports **#
+from .pool import *
