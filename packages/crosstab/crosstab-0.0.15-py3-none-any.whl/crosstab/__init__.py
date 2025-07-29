989fbc6fc50b7700698f90275e6222d7e328197b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:56 +0000

    Add package crosstab/0.0.15/crosstab-0.0.15-py3-none-any.whl

---
+++ packages/crosstab/crosstab-0.0.15-py3-none-any.whl/crosstab/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from .crosstab import (
+    Crosstab,
+    __author__,
+    __author_email__,
+    __description__,
+    __license__,
+    __title__,
+    __url__,
+    __version__,
+)
+
+__all__ = [
+    "Crosstab",
+    "__author__",
+    "__author_email__",
+    "__description__",
+    "__license__",
+    "__title__",
+    "__url__",
+    "__version__",
+]
