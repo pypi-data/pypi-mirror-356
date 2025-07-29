8ef61b08f93392c05033e57dd4fd02e04da3b9b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:24 +0000

    Add package scc-firewall-manager-sdk/1.13.149/scc_firewall_manager_sdk-1.13.149-py3-none-any.whl

---
+++ packages/scc-firewall-manager-sdk/scc_firewall_manager_sdk-1.13.149-py3-none-any.whl/scc_firewall_manager_sdk/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from scc_firewall_manager_sdk.api.device_health_api import DeviceHealthApi
+from scc_firewall_manager_sdk.api.msp_api import MSPApi
+from scc_firewall_manager_sdk.api.transactions_api import TransactionsApi
+
