9d61c87f716bfe41f4d2a7f84e7486e9a3f430ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:10 +0000

    Add package COBY/1.0.3/coby-1.0.3-py3-none-any.whl

---
+++ packages/COBY/coby-1.0.3-py3-none-any.whl/COBY/version.py
@@ -0,0 +1,65 @@
+__version__="1.0.3"
+
+major_changes = [
+]
+
+minor_changes = [
+    "Added atomistic OPC water model in v1.0.2 (forgot to write in version changes).",
+    "Reorganized/renamed the directory-file structure for some membrane, protein and solvation related classes/methods.",
+    "Added 'gm_ldo'/'grid_maker_lipid_distribution_offset' subargument to the Membrane argument allowing offsetting/shifting the lipid order.",]
+
+bug_fixes = [
+    "The subargument 'optimize_run' for membrane arguments can now be used again. The setting was previously being given the wrong key in the membrane settings dictionary."
+]
+
+documentation_changes = [
+]
+
+tutorial_changes = [
+]
+
+other_changes = [
+]
+
+def version_change_writer(iterable, recursion_depth = 0):
+    list_of_strings = []
+    for i in iterable:
+        if type(i) == str:
+            ### Headers
+            if recursion_depth == 0:
+                list_of_strings.append(i)
+            ### Changes. -1 to have no spaces for first recursion. Two spaces "  " to fit with GitHub list formatting.
+            else:
+                list_of_strings.append("  " * (recursion_depth - 1) + "-" + " " + i)
+
+        elif type(i) in [list, tuple]:
+            list_of_strings.extend(version_change_writer(i, recursion_depth + 1))
+    return list_of_strings
+
+### Extra empty "" is to add a blank line between sections
+all_changes = []
+if len(major_changes) > 0:
+    all_changes += ["Major changes:", major_changes, ""]
+
+if len(minor_changes) > 0:
+    all_changes += ["Minor changes:", minor_changes, ""]
+
+if len(bug_fixes) > 0:
+    all_changes += ["Bug fixing:", bug_fixes, ""]
+
+if len(documentation_changes) > 0:
+    all_changes += ["Documentation changes:", documentation_changes, ""]
+
+if len(tutorial_changes) > 0:
+    all_changes += ["Tutorial changes:", tutorial_changes, ""]
+
+if len(other_changes) > 0:
+    all_changes += ["Other changes:", other_changes, ""]
+
+if len(all_changes) > 0:
+    all_changes = all_changes[:-1] # Removes the last ""
+
+version_changes_list = version_change_writer(all_changes)
+__version_changes__ = "\n".join(version_changes_list)
+__changes__         = __version_changes__
+__changelog__       = __version_changes__
