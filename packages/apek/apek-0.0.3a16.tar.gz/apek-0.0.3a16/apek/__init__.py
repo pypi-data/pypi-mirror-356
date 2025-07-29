a46080e8afa5e9dbfae6307673ddac2e1ed0f5bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:55 +0000

    Add package apek/0.0.3a16/apek-0.0.3a16.tar.gz

---
+++ packages/apek/apek-0.0.3a16.tar.gz/apek-0.0.3a16/apek/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+The __init__ module of the "apek" package, used to expose the package's classes and functions.
+
+largeNumber:
+    class LargeNumber:
+        Represent large numbers using the scientific notation XeY.
+
+meta:
+    const const_version:
+        The version number of this package.
+"""
+
+
+
+from . import largeNumber
+from . import helps
+from . import meta
