e5d0d3ab5854c3a7c9a8343163e537af2c33bd66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:15 +0000

    Add package grokcore.message/5.0/grokcore_message-5.0.tar.gz

---
+++ packages/grokcore.message/grokcore_message-5.0.tar.gz/grokcore_message-5.0/CHANGES.rst
@@ -0,0 +1,78 @@
+CHANGES
+*******
+
+5.0 (2025-06-18)
+================
+
+- Replace ``pkg_resources`` namespace with PEP 420 native namespace.
+
+
+4.1 (2025-06-04)
+================
+
+- Add support for Python 3.12, 3.13.
+
+- Drop support for Python 3.7, 3.8.
+
+
+4.0 (2023-08-28)
+================
+
+- Drop support for Python 2.7, 3.4, 3.5, 3.6.
+
+- Add support for Python 3.7, 3.8, 3.9, 3.10, 3.11.
+
+
+3.0.1 (2018-01-17)
+==================
+
+- Replace the use of `grok.implements()` with the `@grok.implementer()`
+  directive throughout.
+
+3.0.0 (2018-01-15)
+==================
+
+- Python 3 compatibility.
+
+0.4.3 (2016-02-15)
+==================
+
+- Update tests.
+
+0.4.2 (2010-10-25)
+==================
+
+- Tests fixed by explicitely registering the IClientIdManager and
+  ISessionDataContainer utilities. The ftesting.zcml was re-introduced for this.
+
+0.4.1 (2010-10-25)
+==================
+
+- Remove ftesting.zcml that was not necessary anymore.
+
+0.4 (2010-10-25)
+================
+
+* Make sure ``zope.session`` is configured, as this package claims to provide
+  for a session based flash message machinery.
+
+* Made package comply to zope.org repository policy.
+
+0.3 (2010-03-05)
+================
+
+* ``UniqueMessageSource`` now implements the ``IMessageSource``
+  interface completely, i.e. the ``type`` parameter is now optional
+  when using ``UniqueMessageSource.send()``.
+
+0.2 (2010-03-03)
+================
+
+* The utility function ``send`` now takes a ``name`` argument,
+  allowing the choice of the target message source.
+
+0.1 (2010-03-03)
+================
+
+* Factored out from former versions of ``grokui.admin``, ``grok`` and
+  ``megrok.layout`` respectively.
---
+++ packages/grokcore.message/grokcore_message-5.0.tar.gz/grokcore_message-5.0/setup.py
@@ -0,0 +1,62 @@
+import os
+
+from setuptools import setup
+
+
+version = '5.0'
+
+with open(os.path.join('src', 'grokcore', 'message', 'README.rst')) as f:
+    readme = f.read()
+with open("CHANGES.rst") as f:
+    changes = f.read()
+
+long_description = f"{readme}\n\n{changes}\n"
+
+install_requires = [
+    'setuptools',
+    'grokcore.component >= 2.5',
+    'z3c.flashmessage',
+    'zope.component',
+]
+
+tests_require = [
+    'zope.publisher',
+    'zope.security',
+    'zope.session >= 5.1',
+    'zope.testrunner >= 6.4',
+]
+
+setup(name='grokcore.message',
+      version=version,
+      description="Grok messaging machinery",
+      long_description=long_description,
+      keywords='Grok Messages',
+      author='Grok Team',
+      author_email='grok-dev@zope.dev',
+      url='https://github.com/zopefoundation/grokcore.message',
+      license='ZPL-2.1',
+      include_package_data=True,
+      zip_safe=False,
+      python_requires='>=3.9',
+      install_requires=install_requires,
+      extras_require=dict(test=tests_require),
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Framework :: Zope :: 3',
+          'Intended Audience :: Developers',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10',
+          'Programming Language :: Python :: 3.11',
+          'Programming Language :: Python :: 3.12',
+          'Programming Language :: Python :: 3.13',
+          'Programming Language :: Python :: Implementation',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Programming Language :: Python :: Implementation :: PyPy',
+          'Topic :: Internet :: WWW/HTTP',
+      ],
+      )
