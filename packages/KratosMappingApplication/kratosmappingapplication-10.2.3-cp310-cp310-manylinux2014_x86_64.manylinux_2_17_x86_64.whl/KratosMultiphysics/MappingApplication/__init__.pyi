df2d74824a62fd8b0a53f86c7907e0e91c822ebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:07:03 +0000

    Add package KratosMappingApplication/10.2.3/kratosmappingapplication-10.2.3-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.whl

---
+++ packages/KratosMappingApplication/kratosmappingapplication-10.2.3-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.whl/KratosMultiphysics/MappingApplication/__init__.pyi
@@ -0,0 +1,6 @@
+import Kratos
+from . import MapperUtilities as MapperUtilities, MappingIntersectionUtilities as MappingIntersectionUtilities
+
+class KratosMappingApplication(Kratos.KratosApplication):
+    def __init__(self) -> None:
+        """__init__(self: KratosMappingApplication.KratosMappingApplication) -> None"""
