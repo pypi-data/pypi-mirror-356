a1428e2d50b0aff651ed2b75801a83f7583a5d83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:59 +0000

    Add package grokcore.chameleon/5.0/grokcore_chameleon-5.0.tar.gz

---
+++ packages/grokcore.chameleon/grokcore_chameleon-5.0.tar.gz/grokcore_chameleon-5.0/CHANGES.rst
@@ -0,0 +1,88 @@
+CHANGES
+*******
+
+5.0 (2025-06-18)
+================
+
+- Replace ``pkg_resources`` namespace with PEP 420 native namespace.
+
+- Drop support for Python 3.8.
+
+
+4.1 (2024-12-04)
+================
+
+- Add support for Python 3.12, 3.13.
+
+- Drop support for Python 3.7.
+
+- The tests are not compatible with (yanked) ``Chameleon`` 4.3.0, thus not
+  allowing to use this version.
+
+- Update translation handling to be compatible with Chameleon 4.3+: do not
+  break on unhashable message ids, just return them.
+
+
+4.0 (2023-02-09)
+================
+
+- Drop support for Python 2.7, 3.4, 3.5, 3.6.
+
+- Add support for Python 3.7, 3.8, 3.9, 3.10, 3.11.
+
+
+3.0.1 (2018-01-12)
+==================
+
+- Rearrange tests such that Travis CI can pick up all functional tests too.
+
+3.0.0 (2018-01-11)
+==================
+
+- Python 3 compatibility.
+
+1.0.4 (2014-07-29)
+==================
+
+- Improve the performances of the translate mechanism with Chameleon
+  2.10 or more recent.
+
+1.0.3 (2012-10-12)
+==================
+
+- Fix broken translations when using Chameleon 2.9 or more recent.
+
+1.0.2 (2012-05-07)
+==================
+
+- With not using the z3c.pt PageTemplateFile baseclass, the behaviour of
+  finding the template file relative to the module was lost. This has been
+  fixed.
+
+1.0.1 (2012-05-03)
+==================
+
+- Make sure the minimal version requirements are defined.
+
+1.0 (2012-05-01)
+================
+
+- The ``target_language`` mangling was lost in version 1.0rc4.
+  Copied from z3c.pt.
+
+1.0rc4 (2012-01-03)
+===================
+
+- Update to newes Chameleon 2.7.1
+- Using some Components/Expressions directly from Chameleon instead of z3c.pt
+
+1.0rc3 (2011-07-14)
+===================
+
+- Rename megrok.chameleon into grokcore.chameleon to make it an official part
+  of Grok.
+
+Earlier versions
+================
+
+- Earlier versions of grokcore.chameleon came by the name megrok.chameleon.
---
+++ packages/grokcore.chameleon/grokcore_chameleon-5.0.tar.gz/grokcore_chameleon-5.0/setup.py
@@ -0,0 +1,69 @@
+import os
+
+from setuptools import setup
+
+
+version = '5.0'
+
+
+install_requires = [
+    'Chameleon != 4.3',
+    'grokcore.component',
+    'grokcore.view',
+    'setuptools',
+    'z3c.pt >= 2.2.2',
+    'zope.i18n',
+]
+
+
+tests_require = [
+    'grokcore.viewlet',
+    'zope.app.appsetup',
+    'zope.app.wsgi',
+    'zope.testbrowser',
+    'zope.testrunner >= 6.4',
+]
+
+
+long_description = (
+    open('README.rst').read() +
+    '\n\n' +
+    open(os.path.join('src', 'grokcore', 'chameleon', 'README.rst')).read() +
+    '\n\n' +
+    open('CHANGES.rst').read())
+
+
+setup(
+    name='grokcore.chameleon',
+    version=version,
+    description="Chameleon page template support for Grok",
+    long_description=long_description,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Framework :: Zope :: 3',
+    ],
+    keywords="grok chameleon template",
+    author="Uli Fouquet",
+    author_email="zope-dev@zope.dev",
+    url="https://github.com/zopefoundation/grokcore.chameleon/",
+    license="ZPL-2.1",
+    include_package_data=True,
+    zip_safe=False,
+    python_requires='>=3.9',
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require={'test': tests_require},
+)
