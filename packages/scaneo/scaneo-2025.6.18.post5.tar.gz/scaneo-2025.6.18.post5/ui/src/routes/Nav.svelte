305543f857e136a7f2d2e33585a0efcb95c3a8bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:39 +0000

    Add package scaneo/2025.6.18.post5/scaneo-2025.6.18.post5.tar.gz

---
+++ packages/scaneo/scaneo-2025.6.18.post5.tar.gz/scaneo-2025.6.18.post5/ui/src/routes/Nav.svelte
@@ -0,0 +1,37 @@
+<script>
+  import baseUrl from "$stores/baseUrl.svelte.js";
+</script>
+
+<nav class="flex flex-row justify-between bg-bg1 p-3">
+  <a
+    href={baseUrl.url}
+    class="text-lg font-bold hover:bg-bg2 px-2 py-1 rounded transition duration-300"
+    >SCANEO</a
+  >
+  <div class="flex flex-row gap-6">
+    <a
+      href="{baseUrl.url}/campaigns"
+      class="hover:bg-bg2 px-2 py-1 rounded transition duration-300"
+      >Campaigns</a
+    >
+    <a
+      href="{baseUrl.url}/models"
+      class="hover:bg-bg2 px-2 py-1 rounded transition duration-300">Models</a
+    >
+    <a
+      href="{baseUrl.url}/plugins"
+      class="hover:bg-bg2 px-2 py-1 rounded transition duration-300">Plugins</a
+    >
+  </div>
+  <div class="flex flex-row gap-3">
+    <!-- <a
+      href="/docs"
+      class="hover:bg-bg2 px-2 py-1 rounded transition duration-300">Docs</a
+    > -->
+    <a
+      href="https://github.com/earthpulse/scaneo"
+      target="_blank"
+      class="hover:bg-bg2 px-2 py-1 rounded transition duration-300">Github</a
+    >
+  </div>
+</nav>
