5784ee6ada49a08f819462cd19422e024c789702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:57 +0000

    Add package TiMBA-Charts/0.2.0/timba_charts-0.2.0.tar.gz

---
+++ packages/TiMBA-Charts/timba_charts-0.2.0.tar.gz/timba_charts-0.2.0/Toolbox/parameters/paths.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+PACKAGEDIR = Path(__file__).resolve().parent.parent
+SCINPUTPATH = PACKAGEDIR / Path("Input") / Path("Scenario_Files")
+AIINPUTPATH = PACKAGEDIR / Path("Input") / Path("Additional_Information")
+SCFOLDERPATH = Path("Input")
+ADDINFOPATH = Path("Input") / Path("Additional_Information")
+COUNTRYINFO = "country_info.csv"
+COMMODITYINFO = "commodity_info.csv"
+FORESTINFO = "Forest_world500.csv"
+HISTINFO = "FAO_Data.csv"
+FORMIP = "external_model_data.csv"
+
+FORMIPPATH = PACKAGEDIR / ADDINFOPATH / FORMIP
