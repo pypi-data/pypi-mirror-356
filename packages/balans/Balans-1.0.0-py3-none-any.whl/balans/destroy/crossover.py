c697c586a0e4488dc007a13366cf19537c1dbc60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:17 +0000

    Add package balans/1.0.0/Balans-1.0.0-py3-none-any.whl

---
+++ packages/balans/Balans-1.0.0-py3-none-any.whl/balans/destroy/crossover.py
@@ -0,0 +1,37 @@
+import copy
+import math
+
+from balans.base_state import _State
+from balans.utils import Constants
+
+
+def crossover(current: _State, rnd_state) -> _State:
+    #  Take one RANDOM solutions.
+    #  If a DISCRETE variable x_rand = x_inc, do not change it.
+    #  Otherwise, put it to the destroy set.
+    #  Send the destroy set to base_instance.
+    print("*** Operator: ", "CROSSOVER")
+    print("\t Destroy current objective:", current.obj_val)
+    next_state = copy.deepcopy(current)
+    next_state.reset_solve_settings()
+
+    # Static features from the instance
+    discrete_indexes = current.instance.discrete_indexes
+
+    r1_index_to_val, _ = current.instance.mip.solve_random_and_undo(Constants.timelimit_random_feasible)
+
+    # If we don't find a random feasible solution
+    if len(r1_index_to_val) == 0:
+        return next_state
+
+    #  If a discrete variable x_rand1 = x_inc, do not change it.
+    indexes_with_same_value = [i for i in discrete_indexes if
+                               math.isclose(r1_index_to_val[i], current.index_to_val[i])]
+
+    # Else potentially change it
+    next_state.destroy_set = set([i for i in discrete_indexes if i not in indexes_with_same_value])
+
+    return next_state
+
+
+
---
+++ packages/balans/Balans-1.0.0-py3-none-any.whl/balans/destroy/proximity.py
@@ -0,0 +1,104 @@
+import copy
+
+from balans.base_state import _State
+
+
+def proximity(current: _State, rnd_state, delta=0.005) -> _State:
+    # Objective function modification
+    # Change the objective coefficients of the original
+    # problem based on the current solution value.
+    # For binary variables,
+    # if x_inc =0, update its objective coefficient to 1.
+    # if x_inc =1, update its objective coefficient to -1.
+    # Drop all other variables by making their coefficient 0.
+    # Send the destroy set to base_instance.
+    # Note : the required objective operations for proximity search happens in base_instance file.
+
+    print("*** Operator: ", "PROXIMITY SEARCH")
+    print("\t Destroy current objective:", current.obj_val)
+    next_state = copy.deepcopy(current)
+    next_state.reset_solve_settings()
+
+    next_state.proximity_delta = delta
+
+    return next_state
+
+
+def proximity_005(current: _State, rnd_state) -> _State:
+    return proximity(current, rnd_state, delta=0.005)
+
+
+def proximity_010(current: _State, rnd_state) -> _State:
+    return proximity(current, rnd_state, delta=0.010)
+
+
+def proximity_015(current: _State, rnd_state) -> _State:
+    return proximity(current, rnd_state, delta=0.015)
+
+
+def proximity_020(current: _State, rnd_state) -> _State:
+    return proximity(current, rnd_state, delta=0.020)
+
+
+def proximity_025(current: _State, rnd_state) -> _State:
+    return proximity(current, rnd_state, delta=0.025)
+
+
+def proximity_030(current: _State, rnd_state) -> _State:
+    return proximity(current, rnd_state, delta=0.030)
+
+
+def proximity_035(current: _State, rnd_state) -> _State:
+    return proximity(current, rnd_state, delta=0.035)
+
+
+def proximity_040(current: _State, rnd_state) -> _State:
+    return proximity(current, rnd_state, delta=0.040)
+
+
+def proximity_045(current: _State, rnd_state) -> _State:
+    return proximity(current, rnd_state, delta=0.045)
+
+
+def proximity_05(current: _State, rnd_state) -> _State:
+    return proximity(current, rnd_state, delta=0.05)
+
+
+def proximity_055(current: _State, rnd_state) -> _State:
+    return proximity(current, rnd_state, delta=0.055)
+
+
+def proximity_060(current: _State, rnd_state) -> _State:
+    return proximity(current, rnd_state, delta=0.060)
+
+
+def proximity_065(current: _State, rnd_state) -> _State:
+    return proximity(current, rnd_state, delta=0.065)
+
+
+def proximity_070(current: _State, rnd_state) -> _State:
+    return proximity(current, rnd_state, delta=0.070)
+
+
+def proximity_075(current: _State, rnd_state) -> _State:
+    return proximity(current, rnd_state, delta=0.075)
+
+
+def proximity_080(current: _State, rnd_state) -> _State:
+    return proximity(current, rnd_state, delta=0.080)
+
+
+def proximity_085(current: _State, rnd_state) -> _State:
+    return proximity(current, rnd_state, delta=0.085)
+
+
+def proximity_090(current: _State, rnd_state) -> _State:
+    return proximity(current, rnd_state, delta=0.090)
+
+
+def proximity_095(current: _State, rnd_state) -> _State:
+    return proximity(current, rnd_state, delta=0.095)
+
+
+def proximity_10(current: _State, rnd_state) -> _State:
+    return proximity(current, rnd_state, delta=0.10)
