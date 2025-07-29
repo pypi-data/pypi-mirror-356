0e88e21eadb2d9d527aa748390327bd6d5e38f7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:01 +0000

    Add package bbperf/0.0.19/bbperf-0.0.19-py3-none-any.whl

---
+++ packages/bbperf/bbperf-0.0.19-py3-none-any.whl/bbperf/const.py
@@ -0,0 +1,48 @@
+# Copyright (c) 2024 Cloudflare, Inc.
+# Licensed under the Apache 2.0 license found in the LICENSE file or at https://www.apache.org/licenses/LICENSE-2.0
+
+SERVER_PORT = 5301
+
+DEFAULT_VALID_DATA_COLLECTION_TIME_SEC = 20
+
+# max duration for calibration phase
+MAX_DURATION_CALIBRATION_TIME_SEC = 20
+
+# cap the amount of time we will wait for valid data
+MAX_DATA_COLLECTION_TIME_WITHOUT_VALID_DATA = 60
+
+# never run longer than this under any circumstances
+MAX_RUN_TIME_FAILSAFE_SEC = 300
+
+# ignore incoming data for this amount of time after starting data collection phase
+DATA_SAMPLE_IGNORE_TIME_SEC = 1
+
+# for socket recv()
+BUFSZ = (128 * 1024)
+
+PAYLOAD_1K = b'a'*1024
+PAYLOAD_128K = b'a'*(128 * 1024)
+
+RUN_MODE_CALIBRATING = 1
+RUN_MODE_RUNNING = 2
+RUN_MODE_STOP = 3
+
+SAMPLE_INTERVAL_SEC = 0.1
+STDOUT_INTERVAL_SEC = 1
+
+RATE_LIMITED_BATCH_SIZE_PKTS_UDP_PKTS = 20
+RATE_LIMITED_BATCH_SIZE_PKTS_TCP_PKTS = 5
+
+SETUP_COMPLETE_MSG = "setup complete"
+START_MSG = " start "
+UDP_STOP_MSG = "stop"
+TCP_CONTROL_INITIAL_ACK = "control initial ack"
+TCP_CONTROL_ARGS_ACK = "control args ack"
+UDP_DATA_INITIAL_ACK = "data initial ack"
+
+SOCKET_TIMEOUT_SEC=30
+
+UDP_DEFAULT_INITIAL_RATE = 8000
+
+UDP_MIN_RATE = 100
+UDP_MAX_RATE = 800000
---
+++ packages/bbperf/bbperf-0.0.19-py3-none-any.whl/bbperf/tcp_helper.py
@@ -0,0 +1,67 @@
+# Copyright (c) 2024 Cloudflare, Inc.
+# Licensed under the Apache 2.0 license found in the LICENSE file or at https://www.apache.org/licenses/LICENSE-2.0
+
+import socket
+
+from . import const
+
+from .exceptions import PeerDisconnectedException
+
+
+def recv(args, tcp_sock, max_bytes_to_read):
+
+    # blocking
+    recv_bytes = tcp_sock.recv(max_bytes_to_read)
+
+    if len(recv_bytes) == 0:
+        raise PeerDisconnectedException()
+
+    if args and args.verbosity > 2:
+        print("tcp recv: {}".format(recv_bytes.decode()), flush=True)
+
+    return recv_bytes
+
+
+# data sock only
+def recv_exact_num_bytes(args, tcp_sock, total_num_bytes_to_read):
+    payload_bytes = bytearray()
+    num_bytes_read = 0
+
+    while num_bytes_read < total_num_bytes_to_read:
+
+        num_bytes_remaining = total_num_bytes_to_read - num_bytes_read
+
+        # blocking
+        recv_bytes = recv(args, tcp_sock, num_bytes_remaining)
+
+        num_bytes_received = len(recv_bytes)
+
+        if num_bytes_received == 0:
+            raise PeerDisconnectedException()
+
+        num_bytes_read += num_bytes_received
+
+        payload_bytes.extend(recv_bytes)
+
+    return payload_bytes
+
+
+def get_congestion_control(data_sock):
+    cc_algo_bytes = data_sock.getsockopt(socket.IPPROTO_TCP, socket.TCP_CONGESTION, 1024)
+    # cc_algo is null-terminated bytes
+    cc_algo_str = cc_algo_bytes.split(b'\x00')[0].decode()
+    return cc_algo_str
+
+def set_congestion_control(data_sock):
+    if get_congestion_control(data_sock) == "cubic":
+        # already set, nothing to do here
+        return
+
+    data_sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_CONGESTION, "cubic".encode())
+
+    cc_algo_str = get_congestion_control(data_sock)
+    if cc_algo_str != "cubic":
+        raise Exception("ERROR: unexpected congestion control in effect: {}".format(cc_algo_str))
+
+def set_tcp_notsent_lowat(data_sock):
+    data_sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NOTSENT_LOWAT, (1024 * 1024))
