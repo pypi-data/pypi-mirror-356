105735298bf99d50caada038d2bc32a789884d5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:19:05 +0000

    Add package chalkpy/2.81.37/chalkpy-2.81.37-py3-none-any.whl

---
+++ packages/chalkpy/chalkpy-2.81.37-py3-none-any.whl/chalk/sql/_internal/sql_settings.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, Mapping
+
+from chalk.sql._internal.incremental import IncrementalSettings
+
+if TYPE_CHECKING:
+    from chalk.sql.finalized_query import Finalizer
+
+
+@dataclass
+class SQLResolverSettings:
+    finalizer: Finalizer
+    incremental_settings: IncrementalSettings | None
+    fields_root_fqn: Mapping[str, str]  # column name -> root fqn of output feature
