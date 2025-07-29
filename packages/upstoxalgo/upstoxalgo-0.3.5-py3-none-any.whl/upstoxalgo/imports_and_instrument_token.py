1fc0d71d276798c88b558ea6f84db65604a321ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:09:06 +0000

    Add package upstoxalgo/0.3.5/upstoxalgo-0.3.5-py3-none-any.whl

---
+++ packages/upstoxalgo/upstoxalgo-0.3.5-py3-none-any.whl/upstoxalgo/imports_and_instrument_token.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import requests
+import datetime
+import logging
+import curlify
+import traceback
+import time
+import pyotp
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+url = "https://assets.upstox.com/market-quote/instruments/exchange/complete.csv.gz"
+
+instrument_list = pd.read_csv(url, compression = "gzip")
+
+def get_dataToken_upstox(tradesymbol, exchange = "NFO"):
+    if exchange == "NFO":
+        exchange = "NSE_FO"
+    if exchange == "NSE":
+        exchange = "NSE_EQ"
+    if exchange == "MCX":
+        exchange = "MCX_FO"
+    if exchange == "BFO":
+        exchange = "BSE_FO"
+    if exchange == "BSE":
+        exchange = "BSE_EQ"
+
+    dataToken = instrument_list[(instrument_list["tradingsymbol"] == tradesymbol) & (instrument_list["exchange"] == exchange)]
+
+    return dataToken.instrument_key.iloc[0]
