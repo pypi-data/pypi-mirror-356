ece61f3426d9d308cf6c4b29701df47dbff39cd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:08:28 +0000

    Add package python3-shodan/0.0.1/python3_shodan-0.0.1.tar.gz

---
+++ packages/python3-shodan/python3_shodan-0.0.1.tar.gz/python3_shodan-0.0.1/README.md
@@ -0,0 +1,1 @@
+# python3-shodan\n\nFake PoC Shodan client.
---
+++ packages/python3-shodan/python3_shodan-0.0.1.tar.gz/python3_shodan-0.0.1/setup.py
@@ -0,0 +1,1 @@
+from setuptools import setup;setup(name="python3-shodan",version="0.0.1",description="Shodan Python3 client",author="sl4x0",packages=["python3_shodan"],python_requires=">=3.6")
