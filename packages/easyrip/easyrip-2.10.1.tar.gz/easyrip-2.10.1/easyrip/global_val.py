be8f6dd20caf13ae5445131e34a33ef476227d05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:05 +0000

    Add package easyrip/2.10.1/easyrip-2.10.1.tar.gz

---
+++ packages/easyrip/easyrip-2.10.1.tar.gz/easyrip-2.10.1/easyrip/global_val.py
@@ -0,0 +1,7 @@
+class GlobalVal:
+    PROJECT_NAME = "Easy Rip"
+    PROJECT_VERSION = "2.10.1"
+    PROJECT_TITLE = f"{PROJECT_NAME} v{PROJECT_VERSION}"
+    PROJECT_URL = "https://github.com/op200/EasyRip"
+    PROJECT_RELEASE_URL = PROJECT_URL + "/releases/latest"
+    PROJECT_RELEASE_API = "https://api.github.com/repos/op200/EasyRip/releases/latest"
