67b9374c8c91bfc0821ea0a1957531537d92336c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:33 +0000

    Add package greethelper/0.1.0/greethelper-0.1.0.tar.gz

---
+++ packages/greethelper/greethelper-0.1.0.tar.gz/greethelper-0.1.0/greethelper/greetings.py
@@ -0,0 +1,4 @@
+def say_hello(name):
+    return f"Hello, {name}! Welcome to our application."
+def say_goodbye(name):
+    return f"Goodbye, {name}! Hope to see you again soon."
---
+++ packages/greethelper/greethelper-0.1.0.tar.gz/greethelper-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+setup(
+    name='greethelper',
+    version='0.1.0',
+    description='A simple greeting helper package',
+    author='Preethi',
+    author_email='preethiice@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    url='https://github.com/preethi777/Python-pip-1.git'
+)
