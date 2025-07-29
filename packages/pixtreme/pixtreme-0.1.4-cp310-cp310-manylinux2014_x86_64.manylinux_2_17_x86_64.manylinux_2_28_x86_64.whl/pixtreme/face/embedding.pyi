de2e95bbf980889d840cf16673d95926e674a579
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:47 +0000

    Add package pixtreme/0.1.4/pixtreme-0.1.4-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/pixtreme/pixtreme-0.1.4-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl/pixtreme/face/embedding.pyi
@@ -0,0 +1,23 @@
+from __future__ import annotations
+import builtins as __builtins__
+import numpy as np
+import onnx as onnx
+import onnxruntime as onnxruntime
+from pixtreme.color.bgr import bgr_to_rgb
+from pixtreme.face.schema import Face
+from pixtreme.utils.blob import to_blobs
+from pixtreme.utils.dlpack import to_cupy
+from pixtreme.utils.dlpack import to_numpy
+from pixtreme.utils.dtypes import to_float32
+import typing
+__all__ = ['Face', 'FaceEmbedding', 'bgr_to_rgb', 'np', 'onnx', 'onnxruntime', 'to_blobs', 'to_cupy', 'to_float32', 'to_numpy']
+class FaceEmbedding:
+    def __init__(self, model_path: str | None = None, model_bytes: bytes | None = None, device: str = 'cuda'):
+        ...
+    def forward(self, batch_data):
+        ...
+    def get(self, face: Face):
+        ...
+    def get_feat(self, imgs) -> np.ndarray:
+        ...
+__test__: dict = {}
