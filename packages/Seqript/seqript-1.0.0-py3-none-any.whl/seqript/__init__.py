ceb228370151b3bf289438f6027b6eb10248312c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:27 +0000

    Add package Seqript/1.0.0/seqript-1.0.0-py3-none-any.whl

---
+++ packages/Seqript/seqript-1.0.0-py3-none-any.whl/seqript/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: UTF-8 -*-
+
+
+__author__ = "Tarcadia"
+__url__ = "https://github.com/Tarcadia/Seqript"
+__copyright__ = "Copyright 2024-2025"
+__credits__ = ["Tarcadia"]
+__license__ = "MIT"
+__version__ = "1.0.0"
+
+from .seqript import Seqript
+
+
+
+__all__ = [
+    "Seqript",
+]
