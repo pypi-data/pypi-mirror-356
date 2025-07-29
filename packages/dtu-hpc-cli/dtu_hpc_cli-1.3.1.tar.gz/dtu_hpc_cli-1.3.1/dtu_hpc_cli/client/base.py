c5c5cf529411844377e65d0f1ea248c089a01c4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:07:16 +0000

    Add package dtu-hpc-cli/1.3.1/dtu_hpc_cli-1.3.1.tar.gz

---
+++ packages/dtu-hpc-cli/dtu_hpc_cli-1.3.1.tar.gz/dtu_hpc_cli-1.3.1/dtu_hpc_cli/client/base.py
@@ -0,0 +1,28 @@
+import abc
+
+
+class Client(abc.ABC):
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    def __del__(self):
+        self.close()
+
+    @abc.abstractmethod
+    def run(self, command: str, cwd: str | None = None) -> tuple[int, str]:
+        pass
+
+    @abc.abstractmethod
+    def close(self):
+        pass
+
+    @abc.abstractmethod
+    def remove(self, path: str):
+        pass
+
+    @abc.abstractmethod
+    def save(self, path: str, contents: str):
+        pass
