d87ba492d872a1cd0f075e1854264a220730bc80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:53 +0000

    Add package uber-compose/1.1.1/uber_compose-1.1.1-py3-none-any.whl

---
+++ packages/uber-compose/uber_compose-1.1.1-py3-none-any.whl/uber_compose/helpers/broken_services.py
@@ -0,0 +1,21 @@
+from uber_compose import Environment
+from uber_compose.core.docker_compose_shell.types import ServicesComposeState
+from uber_compose.core.utils.state_waiting import is_service_not_running_or_not_healthy
+
+
+def calc_broken_services(
+    services_state: ServicesComposeState, config_template: Environment, excluded_from_check: list[str] = None
+) -> list[str]:
+    if excluded_from_check is None:
+        excluded_from_check = []
+    non_ready_services = [
+        service
+        for service in services_state
+        if is_service_not_running_or_not_healthy(service)
+    ]
+    not_included_services = [
+        service
+        for service in config_template.get_services_names()
+        if service not in services_state.get_services_names()
+    ]
+    return list(set(non_ready_services) | set(not_included_services) - set(excluded_from_check))
