1efd5ff16b8f6603deed6fe82d8e40a4f274f51e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:18 +0000

    Add package probium/0.1.1/probium-0.1.1-py3-none-any.whl

---
+++ packages/probium/probium-0.1.1-py3-none-any.whl/probium/__init__.py
@@ -0,0 +1,35 @@
+from importlib.metadata import entry_points, version
+from typing import TYPE_CHECKING
+from .core import detect, scan_dir, list_engines
+from .exceptions import EngineFailure, FastbackError, UnsupportedType
+from .registry import register
+__all__ = [
+    "detect",
+    "scan_dir",
+    "list_engines",
+    "register",
+    "FastbackError",
+    "UnsupportedType",
+    "EngineFailure",
+]
+try:
+    from .core import detect_async
+except ImportError:
+    if TYPE_CHECKING:
+        from typing import Any, Coroutine
+        async def detect_async(*args: Any, **kw: Any) -> "Coroutine[Any, Any, None]": ...
+    else:
+        async def detect_async(*_a, **_kw):
+            raise RuntimeError("Install 'anyio' to use detect_async()")
+    __all__.append("detect_async")
+else:
+    __all__.append("detect_async")
+try:
+    from importlib.metadata import version
+    __version__ = version("fastback")
+except Exception:
+    __version__ = "0.0.0-dev"
+from . import engines
+
+for ep in entry_points(group="fastback.engines"):
+    ep.load()
