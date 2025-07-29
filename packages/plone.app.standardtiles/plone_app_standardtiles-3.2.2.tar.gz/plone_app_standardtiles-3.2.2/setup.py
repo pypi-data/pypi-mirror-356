a006d5f4b960ae8d90c243b57d65c53b924eeaef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:12 +0000

    Add package plone.app.standardtiles/3.2.2/plone_app_standardtiles-3.2.2.tar.gz

---
+++ packages/plone.app.standardtiles/plone_app_standardtiles-3.2.2.tar.gz/plone_app_standardtiles-3.2.2/setup.py
@@ -0,0 +1,66 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+version = "3.2.2"
+
+
+setup(
+    name="plone.app.standardtiles",
+    version=version,
+    description="Tiles for plone.app.blocks page composition",
+    long_description=(open("README.rst").read() + "\n" + open("CHANGES.rst").read()),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: Addon",
+        "Framework :: Plone :: 6.0",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords="tiles content plone",
+    author="Rob Gietema",
+    author_email="rob@fourdigits.nl",
+    url="https://github.com/plone/plone.app.standardtiles",
+    license="GPL",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    namespace_packages=["plone", "plone.app"],
+    include_package_data=True,
+    zip_safe=False,
+    python_requires=">=3.8",
+    install_requires=[
+        "plone.api>=2.3.0",
+        "plone.app.blocks",
+        "plone.app.tiles>=4.0.0",
+        "plone.base",
+        "plone.subrequest",
+        "plone.tiles>=1.8.0",
+        "requests",
+        "setuptools",
+        "Zope",
+    ],
+    extras_require={
+        "test": [
+            "plone.app.testing",
+            "plone.app.dexterity",
+            "plone.app.discussion",
+            "lxml",
+        ],
+        "attachment": [
+            "plone.formwidget.multifile>=2.0",
+        ],
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
