c1330717ad0c4e23e9d80cfe1572b31841b7a6a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:25 +0000

    Add package lambdakit-response/1.0.0/lambdakit_response-1.0.0.tar.gz

---
+++ packages/lambdakit-response/lambdakit_response-1.0.0.tar.gz/lambdakit_response-1.0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lambdakit-response",
+    version="1.0.0",
+    description="Lightweight response utility for AWS Lambda proxy integration",
+    author="mtrshuvo",
+    author_email="mtrshuvo@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "orjson>=3.9.0",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7"
+)
