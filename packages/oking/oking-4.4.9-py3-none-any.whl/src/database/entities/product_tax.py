78d4bba3c11b785430b88fb5f374780ac5f4dce5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:42 +0000

    Add package oking/4.4.9/oking-4.4.9-py3-none-any.whl

---
+++ packages/oking/oking-4.4.9-py3-none-any.whl/src/database/entities/product_tax.py
@@ -0,0 +1,17 @@
+class ProductTax:
+    def __init__(self, codigo_sku, ncm, grupo_tributacao, grupo_cliente,
+                 uf_origem, uf_destino, percentual_reducao_base_calcul,
+                 mva_iva, icms_intraestadual, icms_interestadual, identificador, filial, **kwargs):
+        self.sku_code: str = codigo_sku
+        self.ncm: str = ncm
+        self.taxation_group: str = grupo_tributacao
+        self.customer_group: str = grupo_cliente
+        self.origin_uf: str = uf_origem
+        self.destination_uf: str = uf_destino
+        self.percentage_reduction_base_calculation: float = percentual_reducao_base_calcul
+        self.mva_iva: float = mva_iva
+        self.intrastate_icms: float = icms_intraestadual
+        self.interstate_icms: str = icms_interestadual
+        self.identifier: str = identificador
+        self.branch: str = filial
+        self.__dict__.update(kwargs)
