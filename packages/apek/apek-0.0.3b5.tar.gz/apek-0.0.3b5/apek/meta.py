190973c09c1d2158fad6c727635b17e408ba3a04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:06:55 +0000

    Add package apek/0.0.3b5/apek-0.0.3b5.tar.gz

---
+++ packages/apek/apek-0.0.3b5.tar.gz/apek-0.0.3b5/apek/meta.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+This module defines the package's metadata.
+
+Constants:
+    cst_version (str): The package version number.
+"""
+
+
+
+# import xxx
+
+const_version = "0.0.3b5"
