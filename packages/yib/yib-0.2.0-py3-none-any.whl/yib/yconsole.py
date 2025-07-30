4ede9c4f4975e23641367f1dd16c2288b35bb1ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:55 +0000

    Add package yib/0.2.0/yib-0.2.0-py3-none-any.whl

---
+++ packages/yib/yib-0.2.0-py3-none-any.whl/yib/yconsole.py
@@ -0,0 +1,33 @@
+"""yconsole extends rich.console to provide extra functions."""
+
+import sys
+from typing import Any, overload
+
+import rich.console
+from rich.console import JustifyMethod, OverflowMethod
+from rich.style import Style
+
+
+class Console(rich.console.Console):
+    @overload
+    def fatal(
+        self,
+        *objects: Any,
+        sep: str = " ",
+        end: str = "\n",
+        style: str | Style | None = None,
+        justify: JustifyMethod | None = None,
+        overflow: OverflowMethod | None = None,
+        no_wrap: bool | None = None,
+        emoji: bool | None = None,
+        markup: bool | None = None,
+        highlight: bool | None = None,
+        width: int | None = None,
+        height: int | None = None,
+        crop: bool = True,
+        soft_wrap: bool | None = None,
+        new_line_start: bool = False,
+    ) -> None: ...
+    def fatal(self, *args, **kwargs) -> None:
+        self.print(*args, **kwargs)
+        sys.exit(1)
