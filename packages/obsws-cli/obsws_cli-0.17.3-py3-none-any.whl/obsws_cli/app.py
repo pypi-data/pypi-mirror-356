d2db5d72b485b79b722d5ec00164a8f2c0281f74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:52 +0000

    Add package obsws-cli/0.17.3/obsws_cli-0.17.3-py3-none-any.whl

---
+++ packages/obsws-cli/obsws_cli-0.17.3-py3-none-any.whl/obsws_cli/app.py
@@ -0,0 +1,131 @@
+"""Command line interface for the OBS WebSocket API."""
+
+import importlib
+import logging
+from typing import Annotated
+
+import obsws_python as obsws
+import typer
+
+from obsws_cli.__about__ import __version__ as obsws_cli_version
+
+from . import console, settings
+from .alias import AliasGroup
+
+app = typer.Typer(cls=AliasGroup)
+for sub_typer in (
+    'filter',
+    'group',
+    'hotkey',
+    'input',
+    'profile',
+    'projector',
+    'record',
+    'replaybuffer',
+    'scene',
+    'scenecollection',
+    'sceneitem',
+    'screenshot',
+    'stream',
+    'studiomode',
+    'virtualcam',
+):
+    module = importlib.import_module(f'.{sub_typer}', package=__package__)
+    app.add_typer(module.app, name=sub_typer)
+
+
+def version_callback(value: bool):
+    """Show the version of the CLI."""
+    if value:
+        console.out.print(f'obsws-cli version: {obsws_cli_version}')
+        raise typer.Exit()
+
+
+def setup_logging(debug: bool):
+    """Set up logging for the application."""
+    log_level = logging.DEBUG if debug else logging.CRITICAL
+    logging.basicConfig(
+        level=log_level,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    )
+
+
+@app.callback()
+def main(
+    ctx: typer.Context,
+    host: Annotated[
+        str,
+        typer.Option(
+            '--host',
+            '-H',
+            envvar='OBS_HOST',
+            help='WebSocket host',
+            show_default='localhost',
+        ),
+    ] = settings.get('host'),
+    port: Annotated[
+        int,
+        typer.Option(
+            '--port',
+            '-P',
+            envvar='OBS_PORT',
+            help='WebSocket port',
+            show_default=4455,
+        ),
+    ] = settings.get('port'),
+    password: Annotated[
+        str,
+        typer.Option(
+            '--password',
+            '-p',
+            envvar='OBS_PASSWORD',
+            help='WebSocket password',
+            show_default=False,
+        ),
+    ] = settings.get('password'),
+    timeout: Annotated[
+        int,
+        typer.Option(
+            '--timeout',
+            '-T',
+            envvar='OBS_TIMEOUT',
+            help='WebSocket timeout',
+            show_default=5,
+        ),
+    ] = settings.get('timeout'),
+    version: Annotated[
+        bool,
+        typer.Option(
+            '--version',
+            '-v',
+            is_eager=True,
+            help='Show the CLI version and exit',
+            show_default=False,
+            callback=version_callback,
+        ),
+    ] = False,
+    debug: Annotated[
+        bool,
+        typer.Option(
+            '--debug',
+            '-d',
+            envvar='OBS_DEBUG',
+            is_eager=True,
+            help='Enable debug logging',
+            show_default=False,
+            callback=setup_logging,
+            hidden=True,
+        ),
+    ] = settings.get('debug'),
+):
+    """obsws_cli is a command line interface for the OBS WebSocket API."""
+    ctx.obj = ctx.with_resource(obsws.ReqClient(**ctx.params))
+
+
+@app.command()
+def obs_version(ctx: typer.Context):
+    """Get the OBS Client and WebSocket versions."""
+    resp = ctx.obj.get_version()
+    console.out.print(
+        f'OBS Client version: {resp.obs_version} with WebSocket version: {resp.obs_web_socket_version}'
+    )
---
+++ packages/obsws-cli/obsws_cli-0.17.3-py3-none-any.whl/obsws_cli/scene.py
@@ -0,0 +1,115 @@
+"""module containing commands for controlling OBS scenes."""
+
+from typing import Annotated
+
+import typer
+from rich.table import Table
+
+from . import console, util, validate
+from .alias import AliasGroup
+
+app = typer.Typer(cls=AliasGroup)
+
+
+@app.callback()
+def main():
+    """Control OBS scenes."""
+
+
+@app.command('list | ls')
+def list_(
+    ctx: typer.Context,
+    uuid: Annotated[bool, typer.Option(help='Show UUIDs of scenes')] = False,
+):
+    """List all scenes."""
+    resp = ctx.obj.get_scene_list()
+    scenes = (
+        (scene.get('sceneName'), scene.get('sceneUuid'))
+        for scene in reversed(resp.scenes)
+    )
+
+    active_scene = ctx.obj.get_current_program_scene().scene_name
+
+    table = Table(title='Scenes', padding=(0, 2))
+    if uuid:
+        columns = [
+            ('Scene Name', 'left', 'cyan'),
+            ('Active', 'center', None),
+            ('UUID', 'left', 'cyan'),
+        ]
+    else:
+        table.title += ' (UUIDs hidden)'
+        columns = [
+            ('Scene Name', 'left', 'cyan'),
+            ('Active', 'center', None),
+        ]
+    for column, justify, style in columns:
+        table.add_column(column, justify=justify, style=style)
+
+    for scene_name, scene_uuid in scenes:
+        if scene_name == active_scene:
+            scene_output = f'[bold]{scene_name}[/bold]'
+        else:
+            scene_output = f'[dim]{scene_name}[/dim]'
+
+        if uuid:
+            table.add_row(
+                scene_output,
+                util.check_mark(scene_name == active_scene),
+                scene_uuid,
+            )
+        else:
+            table.add_row(
+                scene_output,
+                util.check_mark(scene_name == active_scene),
+            )
+
+    console.out.print(table)
+
+
+@app.command('current | get')
+def current(
+    ctx: typer.Context,
+    preview: Annotated[
+        bool, typer.Option(help='Get the preview scene instead of the program scene')
+    ] = False,
+):
+    """Get the current program scene or preview scene."""
+    if preview and not validate.studio_mode_enabled(ctx):
+        console.err.print('Studio mode is not enabled, cannot get preview scene.')
+        raise typer.Exit(1)
+
+    if preview:
+        resp = ctx.obj.get_current_preview_scene()
+        console.out.print(resp.current_preview_scene_name)
+    else:
+        resp = ctx.obj.get_current_program_scene()
+        console.out.print(resp.current_program_scene_name)
+
+
+@app.command('switch | set')
+def switch(
+    ctx: typer.Context,
+    scene_name: Annotated[
+        str, typer.Argument(..., help='Name of the scene to switch to')
+    ],
+    preview: Annotated[
+        bool,
+        typer.Option(help='Switch to the preview scene instead of the program scene'),
+    ] = False,
+):
+    """Switch to a scene."""
+    if preview and not validate.studio_mode_enabled(ctx):
+        console.err.print('Studio mode is not enabled, cannot set the preview scene.')
+        raise typer.Exit(1)
+
+    if not validate.scene_in_scenes(ctx, scene_name):
+        console.err.print(f'Scene [yellow]{scene_name}[/yellow] not found.')
+        raise typer.Exit(1)
+
+    if preview:
+        ctx.obj.set_current_preview_scene(scene_name)
+        console.out.print(f'Switched to preview scene: [green]{scene_name}[/green]')
+    else:
+        ctx.obj.set_current_program_scene(scene_name)
+        console.out.print(f'Switched to program scene: [green]{scene_name}[/green]')
