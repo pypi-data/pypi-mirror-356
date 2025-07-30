5db73f1b01b60e7fb91f1262f031b3e90fe37dc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:08:31 +0000

    Add package django-searchkit/1.1/django_searchkit-1.1-py3-none-any.whl

---
+++ packages/django-searchkit/django_searchkit-1.1-py3-none-any.whl/searchkit/filters.py
@@ -0,0 +1,27 @@
+from django.contrib.admin import SimpleListFilter
+from django.contrib.contenttypes.models import ContentType
+from .models import Search
+
+
+class SearchkitFilter(SimpleListFilter):
+    title = 'Searchkit Filter'
+    parameter_name = 'search'
+    template = 'searchkit/searchkit_filter.html'
+
+    def __init__(self, request, params, model, model_admin):
+        # We need the app_label and model as get parameter for the new search
+        # link.
+        self.searchkit_model = ContentType.objects.get_for_model(model)
+        super().__init__(request, params, model, model_admin)
+
+    def lookups(self, request, model_admin):
+        # Fetch the last three objects from SearchkitSearch and return them as
+        # choices.
+        searches = Search.objects.filter(contenttype=self.searchkit_model).order_by('-created_date')[:3]
+        return [(str(obj.id), obj.name) for obj in searches]
+
+    def queryset(self, request, queryset):
+        # Filter the queryset based on the selected SearchkitSearch object
+        if self.value():
+            search = Search.objects.get(id=int(self.value()))
+            return search.as_queryset()
---
+++ packages/django-searchkit/django_searchkit-1.1-py3-none-any.whl/searchkit/models.py
@@ -0,0 +1,27 @@
+from collections import OrderedDict
+from picklefield.fields import PickledObjectField
+from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.utils.translation import gettext_lazy as _
+
+
+class Search(models.Model):
+    name = models.CharField(_('Search name'), max_length=255)
+    contenttype = models.ForeignKey(ContentType, on_delete=models.CASCADE, verbose_name=_('Model'))
+    data = PickledObjectField(_('Serialized filter rule data'))
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('name', 'contenttype')
+
+    def as_lookups(self):
+        lookups = OrderedDict()
+        for data in self.data:
+            lookups[f'{data["field"]}__{data["operator"]}'] = data['value']
+        return lookups
+
+    def as_queryset(self):
+        """
+        Returns a filtered queryset for the model.
+        """
+        return self.contenttype.model_class().objects.filter(**self.as_lookups())
