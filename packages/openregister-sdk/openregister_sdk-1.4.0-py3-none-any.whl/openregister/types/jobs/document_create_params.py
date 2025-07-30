9aa1a0552da837b8ecba88cfa4863116c823c243
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:34 +0000

    Add package openregister-sdk/1.4.0/openregister_sdk-1.4.0-py3-none-any.whl

---
+++ packages/openregister-sdk/openregister_sdk-1.4.0-py3-none-any.whl/openregister/types/jobs/document_create_params.py
@@ -0,0 +1,22 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing_extensions import Literal, Required, TypedDict
+
+__all__ = ["DocumentCreateParams"]
+
+
+class DocumentCreateParams(TypedDict, total=False):
+    document_category: Required[
+        Literal[
+            "current_printout",
+            "chronological_printout",
+            "historical_printout",
+            "structured_information",
+            "shareholder_list",
+            "articles_of_association",
+        ]
+    ]
+
+    register_id: Required[str]
