c4b0dbf61b9aaa14bccc4888f0a5617dd073ccee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:07:22 +0000

    Add package operacionestestSebas/0.1.0/operacionestestsebas-0.1.0.tar.gz

---
+++ packages/operacionestestSebas/operacionestestsebas-0.1.0.tar.gz/operacionestestsebas-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+with open("README.md", "r") as fh:
+    description = fh.read()
+
+setup(
+    name='operacionestestSebas',
+    packages=find_packages(include=['operaciones']),
+    version='0.1.0',
+    description="libreria de operaciones básicas",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    author='Alexis Ruales',
+    license='MIT',
+    install_requires=[],
+    python_requires=">=3.12.4"
+)
