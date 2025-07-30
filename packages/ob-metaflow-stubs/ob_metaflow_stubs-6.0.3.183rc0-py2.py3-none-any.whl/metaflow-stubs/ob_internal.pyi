1e0fa75d5a8712c08dc776c088673ecd71313eaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:07:39 +0000

    Add package ob-metaflow-stubs/6.0.3.183rc0/ob_metaflow_stubs-6.0.3.183rc0-py2.py3-none-any.whl

---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.3.183rc0-py2.py3-none-any.whl/metaflow-stubs/ob_internal.pyi
@@ -0,0 +1,12 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.18.1+obcheckpoint(0.2.1);ob(v1)                                                   #
+# Generated on 2025-06-20T20:53:54.270733                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from .mf_extensions.outerbounds.plugins.kubernetes.pod_killer import PodKiller as PodKiller
+from .mf_extensions.outerbounds.plugins.fast_bakery.baker import bake_image as bake_image
+
---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.3.183rc0-py2.py3-none-any.whl/metaflow-stubs/plugins/aws/step_functions/__init__.pyi
@@ -0,0 +1,12 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.18.1+obcheckpoint(0.2.1);ob(v1)                                                   #
+# Generated on 2025-06-20T20:53:54.339036                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from . import schedule_decorator as schedule_decorator
+from . import step_functions_deployer as step_functions_deployer
+
