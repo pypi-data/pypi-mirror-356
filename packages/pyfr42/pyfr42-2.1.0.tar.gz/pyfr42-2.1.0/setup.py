acd7d81c2f8f93bff7f2948e9312bf4dbc4a44f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:45 +0000

    Add package pyfr42/2.1.0/pyfr42-2.1.0.tar.gz

---
+++ packages/pyfr42/pyfr42-2.1.0.tar.gz/pyfr42-2.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyfr42",
+    version="2.1.0",
+    author="asd dasd",
+    description="zxczxczxc",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    install_requires=["pyperclip", "openai"], 
+)
