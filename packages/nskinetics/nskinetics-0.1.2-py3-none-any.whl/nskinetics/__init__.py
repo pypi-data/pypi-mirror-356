39560c6def0fb1102745560fac38d50fbfc884a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:33 +0000

    Add package nskinetics/0.1.2/nskinetics-0.1.2-py3-none-any.whl

---
+++ packages/nskinetics/nskinetics-0.1.2-py3-none-any.whl/nskinetics/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 29 18:40:55 2025
+
+@author: sarangbhagwat
+"""
+__version__ = '0.1.2'
+
+# %% Initialize NSKinetics 
+
+from . import species
+from .species import *
+from . import reactions
+from .reactions import *
+from . import reaction_systems 
+from .reaction_systems import *
+
+__all__ = (
+     *species.__all__,
+     *reactions.__all__,
+     *reaction_systems.__all__,
+     )
