36d4dc63488e768ae8bd4bbca8fff833116ac9e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:54 +0000

    Add package Priya-dharshni-2002/0.1.0/priya_dharshni_2002-0.1.0.tar.gz

---
+++ packages/Priya-dharshni-2002/priya_dharshni_2002-0.1.0.tar.gz/priya_dharshni_2002-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Priya_dharshni_2002',  # Replace with your desired package name
+    version='0.1.0',
+    description='A simple Python package that says hello',
+    author='Your Name',
+    author_email='priyadharshniramesh@gmail.com',
+    packages=find_packages(),  # Automatically finds the my_package folder
+    install_requires=[],  # Add any dependencies here
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
