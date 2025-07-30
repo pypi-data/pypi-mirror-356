580287919b6d3e2060b46bf6e598a51c0c40f446
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:07:41 +0000

    Add package operaciones1/0.1.0/operaciones1-0.1.0-py3-none-any.whl

---
+++ packages/operaciones1/operaciones1-0.1.0-py3-none-any.whl/operaciones1/modulo.py
@@ -0,0 +1,12 @@
+class operaciones:
+    """
+    En esta  clase se calculan varias operacioens aritméticas.
+    a: número entero
+    b: npumero diferente de 0
+    """
+    def __init__(self, a, b):
+        self.a  =a
+        self.b = b
+    
+    def suma(self):
+        return self.a + self.b
