6a165186a90fc09298f814261f13550a24d1be78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:25 +0000

    Add package airflow-pydantic/1.2.10/airflow_pydantic-1.2.10.tar.gz

---
+++ packages/airflow-pydantic/airflow_pydantic-1.2.10.tar.gz/airflow_pydantic-1.2.10/airflow_pydantic/utils/__init__.py
@@ -0,0 +1,10 @@
+from pkn.pydantic import (
+    CallablePath,
+    ImportPath,
+    get_import_path,
+    serialize_path_as_string,
+)
+
+from .bash import *
+from .common import *
+from .ssh_hook import *
