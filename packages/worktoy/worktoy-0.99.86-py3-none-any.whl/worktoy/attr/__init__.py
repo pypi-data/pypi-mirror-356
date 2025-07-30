b39e851780824894990dba382452a452a59cb4f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:10:08 +0000

    Add package worktoy/0.99.86/worktoy-0.99.86-py3-none-any.whl

---
+++ packages/worktoy/worktoy-0.99.86-py3-none-any.whl/worktoy/attr/__init__.py
@@ -0,0 +1,12 @@
+"""The 'worktoy.attr' module implements the descriptor protocol."""
+#  AGPL-3.0 license
+#  Copyright (c) 2025 Asger Jon Vistisen
+from __future__ import annotations
+
+from ._field import Field
+from ._attri_box import AttriBox
+
+__all__ = [
+    'Field',
+    'AttriBox',
+]
