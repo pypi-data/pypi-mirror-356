e4290d260f82ba86b0399e5eacdebaf264d019c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:06:19 +0000

    Add package yta-image-advanced/0.0.1/yta_image_advanced-0.0.1.tar.gz

---
+++ packages/yta-image-advanced/yta_image_advanced-0.0.1.tar.gz/yta_image_advanced-0.0.1/src/yta_image_advanced/description/__init__.py
@@ -0,0 +1,7 @@
+"""
+Module about describingwhat is appearing and
+going on in an image.
+
+TODO: This module must be moved to an specific
+AI-related library.
+"""
---
+++ packages/yta-image-advanced/yta_image_advanced-0.0.1.tar.gz/yta_image_advanced-0.0.1/src/yta_image_advanced/generation/ai/__init__.py
@@ -0,0 +1,7 @@
+"""
+Artificial Intelligence models and engines able
+to create images from prompts.
+
+TODO: This module has to be moved to the AI-related
+library.
+"""
