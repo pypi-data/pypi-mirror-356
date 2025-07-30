48f68f6bcd0444b2c9ce15f4a3ac6fce22dda99b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:28 +0000

    Add package dj-mypypi2/0.2.0/dj_mypypi2-0.2.0.tar.gz

---
+++ packages/dj-mypypi2/dj_mypypi2-0.2.0.tar.gz/dj_mypypi2-0.2.0/tests/test_urls.py
@@ -0,0 +1,49 @@
+import cuid
+
+from django.urls import reverse, resolve
+from django.test import TestCase
+
+from djmypypi2 import models
+from djmypypi2 import factories
+
+
+class TestUrls(TestCase):
+    def test_package_list_reverse(self):
+        url = reverse('djmypypi2:package-list')
+        self.assertEqual(url, '/mypypi2/')
+
+    def test_package_list_resolve(self):
+        view_name = resolve('/mypypi2/').view_name
+        self.assertEqual(view_name, 'djmypypi2:package-list')
+
+    def test_package_detail_reverse(self):
+        package: models.Package = factories.PackageFactory()
+
+        url = reverse('djmypypi2:package-detail', kwargs={'package_name': package.name})
+        self.assertEqual(url, f'/mypypi2/{package.name}/')
+
+    def test_package_detail_resolve(self):
+        package: models.Package = factories.PackageFactory()
+
+        view_name = resolve(f'/mypypi2/{package.name}/').view_name
+        self.assertEqual(view_name, 'djmypypi2:package-detail')
+
+    def test_download_reverse(self):
+        version: models.Version = factories.VersionFactory()
+
+        url = reverse('djmypypi2:download-package', kwargs={'archive_name': version.archive_name})
+        self.assertEqual(url, f'/mypypi2/@download/{version.archive_name}')
+
+    def test_download_resolve(self):
+        version: models.Version = factories.VersionFactory()
+
+        view_name = resolve(f'/mypypi2/@download/{version.archive_name}').view_name
+        self.assertEqual(view_name, 'djmypypi2:download-package')
+
+    def test_upload_reverse(self):
+        url = reverse('djmypypi2:upload-package')
+        self.assertEqual(url, f'/mypypi2/@upload/')
+
+    def test_upload_resolve(self):
+        view_name = resolve(f'/mypypi2/@upload/').view_name
+        self.assertEqual(view_name, 'djmypypi2:upload-package')
