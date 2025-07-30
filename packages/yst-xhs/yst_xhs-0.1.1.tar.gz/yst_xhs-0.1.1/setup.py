638adf2ae79ff8f11085c411541f55b89f89964f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:42 +0000

    Add package yst-xhs/0.1.1/yst_xhs-0.1.1.tar.gz

---
+++ packages/yst-xhs/yst_xhs-0.1.1.tar.gz/yst_xhs-0.1.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="yst_xhs",
+    version="0.1.1",
+    author="yst",
+    description="小红书爬虫工具",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'yst_xhs.static': ['*.js'],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'yst_xhs=yst_xhs.main:main',
+        ],
+    },
+)
