c03897021672e6791c6c917b826529afd117283c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:57 +0000

    Add package stock-pandas/2.1.2/stock_pandas-2.1.2-cp313-cp313-macosx_11_0_arm64.whl

---
+++ packages/stock-pandas/stock_pandas-2.1.2-cp313-cp313-macosx_11_0_arm64.whl/stock_pandas/__init__.py
@@ -0,0 +1,23 @@
+from .dataframe import StockDataFrame
+from .directive import (
+    directive_stringify
+)
+
+from .exceptions import (
+    DirectiveSyntaxError,
+    DirectiveValueError
+)
+
+from .commands import (
+    COMMANDS,
+    CommandPreset
+)
+
+from .meta import (
+    TimeFrame,
+    TimeFrameArg,
+    Cumulator,
+    cumulators
+)
+
+__version__ = '2.1.2'
