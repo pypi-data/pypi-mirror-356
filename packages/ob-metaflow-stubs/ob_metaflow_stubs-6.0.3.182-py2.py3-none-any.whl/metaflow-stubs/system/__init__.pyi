96787979263edba90d747924f60949baae37de86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:36 +0000

    Add package ob-metaflow-stubs/6.0.3.182/ob_metaflow_stubs-6.0.3.182-py2.py3-none-any.whl

---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.3.182-py2.py3-none-any.whl/metaflow-stubs/system/__init__.pyi
@@ -0,0 +1,14 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.18.1+obcheckpoint(0.2.1);ob(v1)                                                   #
+# Generated on 2025-06-19T23:04:39.555497                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from . import system_monitor as system_monitor
+from .system_monitor import SystemMonitor as SystemMonitor
+from . import system_logger as system_logger
+from .system_logger import SystemLogger as SystemLogger
+
---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.3.182-py2.py3-none-any.whl/metaflow-stubs/tuple_util.pyi
@@ -0,0 +1,15 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.18.1+obcheckpoint(0.2.1);ob(v1)                                                   #
+# Generated on 2025-06-19T23:04:39.540424                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+
+def namedtuple_with_defaults(typename, field_descr, defaults = ()):
+    ...
+
+foreach_frame_field_list: list
+
