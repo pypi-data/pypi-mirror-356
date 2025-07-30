0a1b69bfe1da52aa2c1f8140d0cd2f123e86da38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:10:43 +0000

    Add package model-jacket/0.2.0/model_jacket-0.2.0.tar.gz

---
+++ packages/model-jacket/model_jacket-0.2.0.tar.gz/model_jacket-0.2.0/setup.py
@@ -0,0 +1,36 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="model_jacket",
+    version="0.2.0",
+    description="Package any ML model into a scalable, observable FastAPI service",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Shahwar Saleem",
+    author_email="saleemshahwar@email.com",
+    url="https://github.com/shahwarcodes/model-jacket",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "fastapi",
+        "uvicorn[standard]",
+        "pydantic",
+        "prometheus_client",
+        "torch",
+        "onnxruntime",
+        "scikit-learn",
+        "joblib"
+    ],
+    entry_points={
+        "console_scripts": [
+            "jacket=jacket_cli.__main__:main"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
