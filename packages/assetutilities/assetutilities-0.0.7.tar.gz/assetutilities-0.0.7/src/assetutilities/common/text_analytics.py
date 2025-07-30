8de54b981c9c13b8484ab36ae4b0fc3d2785b9f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:37 +0000

    Add package assetutilities/0.0.7/assetutilities-0.0.7.tar.gz

---
+++ packages/assetutilities/assetutilities-0.0.7.tar.gz/assetutilities-0.0.7/src/assetutilities/common/text_analytics.py
@@ -0,0 +1,14 @@
+import glob
+
+
+class TextAnalytics:
+
+    def __init__(self) -> None:
+        pass
+
+    def router(self, cfg):
+
+        self.get_subset_files(cfg)
+
+    def get_subset_files(self, cfg):
+        pass
