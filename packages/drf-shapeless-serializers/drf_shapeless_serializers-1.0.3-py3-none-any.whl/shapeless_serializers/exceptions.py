0ab3961b571e024c352d30c6d807303c86f61e82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:43 +0000

    Add package drf-shapeless-serializers/1.0.3/drf_shapeless_serializers-1.0.3-py3-none-any.whl

---
+++ packages/drf-shapeless-serializers/drf_shapeless_serializers-1.0.3-py3-none-any.whl/shapeless_serializers/exceptions.py
@@ -0,0 +1,8 @@
+
+class DynamicSerializerConfigError(Exception):
+    """Custom exception for dynamic serializer configuration errors."""
+
+
+class ExcessiveNestingError(Exception):
+    """Custom exception for deep length of recursion error"""
+    
