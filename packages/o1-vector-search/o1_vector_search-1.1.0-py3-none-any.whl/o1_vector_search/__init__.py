8932781242fe49bf491e2eda8d7fa66d3fbd4119
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:34 +0000

    Add package o1-vector-search/1.1.0/o1_vector_search-1.1.0-py3-none-any.whl

---
+++ packages/o1-vector-search/o1_vector_search-1.1.0-py3-none-any.whl/o1_vector_search/__init__.py
@@ -0,0 +1,9 @@
+"""
+O(1) Vector Search - Python Implementation
+Instant similarity search using Locality Sensitive Hashing (LSH)
+"""
+
+from .core import O1VectorSearch
+
+__version__ = "1.0.0"
+__all__ = ["O1VectorSearch"]
