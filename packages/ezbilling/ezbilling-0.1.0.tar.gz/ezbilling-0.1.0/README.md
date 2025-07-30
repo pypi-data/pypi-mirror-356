e4e3be28e0fcaa0587adb637ce38db27d77af793
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:46:28 +0000

    Add package ezbilling/0.1.0/ezbilling-0.1.0.tar.gz

---
+++ packages/ezbilling/ezbilling-0.1.0.tar.gz/ezbilling-0.1.0/README.md
@@ -0,0 +1,21 @@
+<!-- 
+python -m build
+
+twine upload dist/*
+
+pypi-AgEIcHlwaS5vcmcCJDI1ZmI2MTUyLWI0MGQtNDExZi1iMzFhLWFhOTIwY2E0MjdkNgACKlszLCI5YjczMWU2MS1mZjg3LTQyNmYtYWZlYy02ZDc1MzNjNThkZmIiXQAABiAEzDCORZ0kqvuZMC-mk0xycE_Ba7V_vQHzlooQ0nJKYA
+
+pypi-AgEIcHlwaS5vcmcCJGE1MjE4MDNiLTVlNTYtNDg4Yy1hN2I5LTM2MjQ3NzQ1ZGFkZQACKlszLCI5YjczMWU2MS1mZjg3LTQyNmYtYWZlYy02ZDc1MzNjNThkZmIiXQAABiCEbEqPgAplNqAYIu49wvkt2NGXS3UzCKQjMcrpX7eUMA
+
+
+ -->
+
+
+# Your Package
+
+A small Python package that says hello.
+
+## Installation
+
+```bash
+pip install ezbillpg
---
+++ packages/ezbilling/ezbilling-0.1.0.tar.gz/ezbilling-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ezbilling",           # Must be unique on PyPI
+    version="0.1.0",
+    author="Nithesh",
+    author_email="nithish@dreaminfinity.com",
+    description="Make payment gatway integration easy",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ezbillpay",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
