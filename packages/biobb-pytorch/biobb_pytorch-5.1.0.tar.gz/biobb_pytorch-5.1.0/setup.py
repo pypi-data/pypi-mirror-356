0546f70f8f77bcd02688b05100cec2bc7bffc284
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:09 +0000

    Add package biobb-pytorch/5.1.0/biobb_pytorch-5.1.0.tar.gz

---
+++ packages/biobb-pytorch/biobb_pytorch-5.1.0.tar.gz/biobb_pytorch-5.1.0/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="biobb_pytorch",
+    version="5.1.0",
+    author="Biobb developers",
+    author_email="pau.andrio@bsc.es",
+    description="biobb_pytorch is the Biobb module collection to create and train ML & DL models.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="Bioinformatics Workflows BioExcel Compatibility",
+    url="https://github.com/bioexcel/biobb_pytorch",
+    project_urls={
+        "Documentation": "http://biobb-pytorch.readthedocs.io/en/latest/",
+        "Bioexcel": "https://bioexcel.eu/",
+    },
+    packages=setuptools.find_packages(exclude=["docs", "test"]),
+    package_data={"biobb_pytorch": ["py.typed"]},
+    install_requires=["biobb_common==5.1.0", "torch"],
+    python_requires=">=3.9",
+    entry_points={
+        "console_scripts": [
+            "train_mdae = biobb_pytorch.mdae.train_mdae:main",
+            "apply_mdae = biobb_pytorch.mdae.apply_mdae:main",
+        ]
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+    ],
+)
