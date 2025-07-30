49e2224e4cc5cea489eb418395b731d61862ac2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:10:34 +0000

    Add package zibanu-django/2.0.4/zibanu_django-2.0.4-py3-none-any.whl

---
+++ packages/zibanu-django/zibanu_django-2.0.4-py3-none-any.whl/zibanu/django/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+#  Developed by CQ Inversiones SAS. Copyright ©. 2019 - 2022. All rights reserved.
+#  Desarrollado por CQ Inversiones SAS. Copyright ©. 2019 - 2022. Todos los derechos reservado
+
+# ****************************************************************
+# IDE:          PyCharm
+# Developed by: macercha
+# Date:         11/12/22 6:06 PM
+# Project:      Zibanu Django Project
+# Module Name:  __init__.py
+# Description:
+# ****************************************************************
+__version__ = "2.0.4"
