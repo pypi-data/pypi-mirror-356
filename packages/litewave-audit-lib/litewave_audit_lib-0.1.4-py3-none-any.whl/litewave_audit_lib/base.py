4c731df3d996e8f1c511e9eb094586823d206024
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:06 +0000

    Add package litewave-audit-lib/0.1.4/litewave_audit_lib-0.1.4-py3-none-any.whl

---
+++ packages/litewave-audit-lib/litewave_audit_lib-0.1.4-py3-none-any.whl/litewave_audit_lib/base.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+from datetime import datetime
+from typing import Optional, Dict, Any
+
+class BaseAuditLogger(ABC):
+    @abstractmethod
+    def log(self, *, who, resource, action, timestamp=None,
+              location=None, request_context=None,
+              context=None, client=None):
+        """Audit an action with full context"""
+        pass
