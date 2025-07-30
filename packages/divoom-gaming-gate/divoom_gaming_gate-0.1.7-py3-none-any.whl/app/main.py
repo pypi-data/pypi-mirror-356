b567e02b08272dd2f2caaf6e2f41cbca5be1120b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:16 +0000

    Add package divoom-gaming-gate/0.1.7/divoom_gaming_gate-0.1.7-py3-none-any.whl

---
+++ packages/divoom-gaming-gate/divoom_gaming_gate-0.1.7-py3-none-any.whl/app/main.py
@@ -0,0 +1,35 @@
+# main.py
+
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTabWidget
+from PyQt5.QtGui import QIcon
+from .screens.screens_tab import ScreensTab
+from .characters.characters_tab import CharactersTab
+from .tools.tools_tab import ToolsTab
+from .designer.designer_tab import DesignerTab
+from .settings.settings_tab import SettingsTab
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Divoom Gaming Gate")
+        tabs = QTabWidget()
+        tabs.addTab(ScreensTab(),    "Screens")
+        tabs.addTab(CharactersTab(), "Characters")
+        tabs.addTab(ToolsTab(), "Tools")
+        settings_tab = SettingsTab()
+        tabs.addTab(DesignerTab(),   "Designer")
+        tabs.addTab(settings_tab, "Settings")  
+        self.setCentralWidget(tabs)
+
+def main():
+    app = QApplication(sys.argv)
+    app.setApplicationName("Gaming Gate")         
+    app.setWindowIcon(QIcon("GamingGate.ico")) 
+    w = MainWindow()
+    w.resize(1000, 700)
+    w.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
