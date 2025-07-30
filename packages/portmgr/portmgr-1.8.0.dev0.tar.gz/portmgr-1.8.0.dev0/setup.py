ea29f6af845e4902de16f6c2f907c647232dba54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:57 +0000

    Add package portmgr/1.8.0.dev0/portmgr-1.8.0.dev0.tar.gz

---
+++ packages/portmgr/portmgr-1.8.0.dev0.tar.gz/portmgr-1.8.0.dev0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='portmgr',
+    version='1.8.0dev0',
+    url="https://github.com/Craeckie/portmgr",
+    description="Simple command interface to manage multiple Docker container",
+    packages=find_packages(), #['portmgr'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    zip_safe=False,
+
+    install_requires=[
+        'jsonschema',
+        'tabulate',
+        'PyYAML',
+        'jsonschema',
+        'humanfriendly'
+    ],
+    entry_points = {
+        'console_scripts': [
+            'portmgr = portmgr:main'
+        ]
+    }
+)
