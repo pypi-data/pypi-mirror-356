25d52c64ec2dd7d2a8d8bbcbf5611bc95be237a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:34 +0000

    Add package odoo-addon-ai-oca-bridge/16.0.2.0.1/odoo_addon_ai_oca_bridge-16.0.2.0.1-py3-none-any.whl

---
+++ packages/odoo-addon-ai-oca-bridge/odoo_addon_ai_oca_bridge-16.0.2.0.1-py3-none-any.whl/odoo/addons/ai_oca_bridge/models/__init__.py
@@ -0,0 +1,3 @@
+from . import ai_bridge
+from . import ai_bridge_execution
+from . import mail_thread
---
+++ packages/odoo-addon-ai-oca-bridge/odoo_addon_ai_oca_bridge-16.0.2.0.1-py3-none-any.whl/odoo/addons/ai_oca_bridge/tests/__init__.py
@@ -0,0 +1,3 @@
+from . import test_bridge
+from . import test_frontend
+from . import test_connection
