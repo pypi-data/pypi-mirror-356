dff5c73542fd31ab66ff24e8768955cd6465f6fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:20 +0000

    Add package print-fn/0.1/print_fn-0.1.tar.gz

---
+++ packages/print-fn/print_fn-0.1.tar.gz/print_fn-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='print_fn',
+    version='0.1',
+    packages=find_packages(),
+    description='Simple print function module',
+    author='savithri',
+    author_email='savithri2005muthuvel@gmail.com',
+    install_requires=[],  # or any packages you use
+)
