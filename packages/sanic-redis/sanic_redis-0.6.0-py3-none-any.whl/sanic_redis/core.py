96c67fbf66657239b408f81630d32d5a674ce25e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:14 +0000

    Add package sanic-redis/0.6.0/sanic_redis-0.6.0-py3-none-any.whl

---
+++ packages/sanic-redis/sanic_redis-0.6.0-py3-none-any.whl/sanic_redis/core.py
@@ -0,0 +1,85 @@
+"""
+Sanic-Redis core file
+"""
+
+from typing import Optional
+
+from redis.asyncio import client, from_url
+from sanic import Sanic
+from sanic.log import logger
+
+
+class SanicRedis:
+    """
+    Redis Class for Sanic
+    """
+
+    conn: client.Redis
+    app: Sanic
+    redis_url: str
+    config_name: str = "REDIS"
+    single_connection_client: bool
+
+    def __init__(
+        self,
+        app: Optional[Sanic] = None,
+        config_name="REDIS",
+        redis_url: str = "",
+        single_connection_client: bool = False,
+    ):
+        """
+        init method of class
+        """
+        self.config_name = config_name
+        self.redis_url = redis_url
+        self.single_connection_client = single_connection_client
+        if app:
+            self.init_app(
+                app=app,
+                redis_url=redis_url,
+                config_name=config_name,
+                single_connection_client=single_connection_client,
+            )
+
+    def init_app(
+        self,
+        app: Sanic,
+        config_name: Optional[str] = None,
+        redis_url: Optional[str] = None,
+        single_connection_client: Optional[bool] = None,
+    ):
+        """
+        init_app for Sanic
+        """
+
+        self.app = app
+        if redis_url:
+            self.redis_url = redis_url
+        if config_name:
+            self.config_name = config_name
+        if single_connection_client:
+            self.single_connection_client = single_connection_client
+
+        @app.listener("before_server_start")
+        async def redis_configure(_app: Sanic, _loop):
+            if self.redis_url:
+                _redis_url = self.redis_url
+            else:
+                _redis_url = _app.config.get(self.config_name)
+            if not _redis_url:
+                raise ValueError(
+                    f"You must specify a redis_url or set the "
+                    f"{config_name} Sanic config variable"
+                )
+            logger.info("[sanic-redis] connecting")
+            _redis = await from_url(
+                _redis_url,
+                single_connection_client=self.single_connection_client,
+            )
+            setattr(_app.ctx, self.config_name.lower(), _redis)
+            self.conn = _redis
+
+        @app.listener("after_server_stop")
+        async def close_redis(_app, _loop):
+            logger.info("[sanic-redis] closing")
+            await self.conn.aclose()
