2624a2f1985ce04c391bcc991289e0ac932e573e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:07:49 +0000

    Add package codewords-client/0.2.4/codewords_client-0.2.4.tar.gz

---
+++ packages/codewords-client/codewords_client-0.2.4.tar.gz/codewords_client-0.2.4/codewords_client/__init__.py
@@ -0,0 +1,4 @@
+from .codewords_client import (AsyncCodewordsClient, AsyncCodewordsResponse,
+                               CodewordsClient, CodewordsResponse)
+
+__all__ = ["AsyncCodewordsClient", "CodewordsClient", "AsyncCodewordsResponse", "CodewordsResponse"]
