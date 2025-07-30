ef78562d86b62742470c87d424aa5f600497359c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:47 +0000

    Add package anemoi-graphs/0.6.2/anemoi_graphs-0.6.2-py3-none-any.whl

---
+++ packages/anemoi-graphs/anemoi_graphs-0.6.2-py3-none-any.whl/anemoi/graphs/edges/__init__.py
@@ -0,0 +1,24 @@
+# (C) Copyright 2024 Anemoi contributors.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
+from .builders.cutoff import CutOffEdges
+from .builders.icon import ICONTopologicalDecoderEdges
+from .builders.icon import ICONTopologicalEncoderEdges
+from .builders.icon import ICONTopologicalProcessorEdges
+from .builders.knn import KNNEdges
+from .builders.multi_scale import MultiScaleEdges
+
+__all__ = [
+    "KNNEdges",
+    "CutOffEdges",
+    "MultiScaleEdges",
+    "ICONTopologicalProcessorEdges",
+    "ICONTopologicalEncoderEdges",
+    "ICONTopologicalDecoderEdges",
+]
