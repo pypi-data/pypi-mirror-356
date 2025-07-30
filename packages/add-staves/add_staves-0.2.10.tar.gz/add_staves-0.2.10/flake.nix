c97292f667f487616d06b55638d008e8bc7c37cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:34:14 +0000

    Add package add-staves/0.2.10/add_staves-0.2.10.tar.gz

---
+++ packages/add-staves/add_staves-0.2.10.tar.gz/add_staves-0.2.10/flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, utils, ... }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+        {
+          devShell = with pkgs; mkShell {
+            packages = [
+              python312
+              uv
+            ];
+
+            shellHook = ''
+              export PATH="$HOME/.local/share/uv/tools/add-staves/bin:$PATH"
+            '';
+          };
+        }
+    );
+}
+
