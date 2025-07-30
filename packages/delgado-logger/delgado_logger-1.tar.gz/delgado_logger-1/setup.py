049c7826f93f4e79d84c2bf4f8189f1a66a8f50c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:15 +0000

    Add package delgado-logger/1/delgado_logger-1.tar.gz

---
+++ packages/delgado-logger/delgado_logger-1.tar.gz/delgado_logger-1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='delgado_logger',
+    version='1',
+    author='Delgado',
+    description='A lightweight and human-readable logging and CSV utility',
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # <-- THIS IS CRITICAL
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires='>=3.7',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    url='https://github.com/pyrometheous/Delgado-Logger',
+    license='MIT'
+)
