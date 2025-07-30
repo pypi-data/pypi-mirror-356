1b8305d94c9e3f28978f957c938d1447825705e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:13 +0000

    Add package leop/0.1.0/leop-0.1.0.tar.gz

---
+++ packages/leop/leop-0.1.0.tar.gz/leop-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='leop',
+    version='0.1.0',
+    author='Leo',
+    author_email='lindb2020@hotmail.com',
+    packages=find_packages(),
+    python_requires='>=3.6',
+    license='MIT',
+    license_file='LICENSE', 
+    description='leop',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
