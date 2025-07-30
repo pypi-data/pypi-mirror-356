8ff63bb1fa10461d52870ae4c147534bf63e0576
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:37 +0000

    Add package mosamatic-cli/1.5.3/mosamatic_cli-1.5.3.tar.gz

---
+++ packages/mosamatic-cli/mosamatic_cli-1.5.3.tar.gz/mosamatic_cli-1.5.3/mosamatic/main.py
@@ -0,0 +1,50 @@
+import click
+
+from mosamatic.commands import (
+    calculatescores,
+    decompressdicomfiles,
+    rescaledicomfiles,
+    segmentmusclefatl3,
+)
+from mosamatic.utils import show_doc_command
+
+
+class CustomHelpGroup(click.Group):
+    def format_commands(self, ctx, formatter):
+        commands = self.list_commands(ctx)
+        with formatter.section('Commands'):
+            for command_name in commands:
+                command = self.get_command(ctx, command_name)
+                if command is None or command.hidden:
+                    continue
+                help_text = command.get_short_help_str()
+                formatter.write_text(f'{command_name:15} {help_text}')
+
+
+@click.group(cls=CustomHelpGroup)
+def cli():
+    pass
+
+
+cli.add_command(calculatescores.calculatescores)
+cli.add_command(decompressdicomfiles.decompressdicomfiles)
+cli.add_command(rescaledicomfiles.rescaledicomfiles)
+cli.add_command(segmentmusclefatl3.segmentmusclefatl3)
+
+cli.add_command(show_doc_command(cli)) # Special command to show long description for command
+
+"""
+mosamatic/
+mosamatic/__init__.py
+mosamatic/main.py
+mosamatic/commands/
+mosamatic/commands/__init__.py
+mosamatic/commands/calculatescores.py
+mosamatic/tasks/
+mosamatic/tasks/__init__.py
+mosamatic/tasks/calculatescores/
+mosamatic/tasks/calculatescores/__init__.py
+mosamatic/tasks/calculatescores/calculatescores.py
+pyproject.toml
+"""
+
