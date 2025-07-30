d17d44262a3ea9d6ae985aa102760172be479b58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:49:01 +0000

    Add package lucid-dl/2.2.0/lucid_dl-2.2.0-py3-none-any.whl

---
+++ packages/lucid-dl/lucid_dl-2.2.0-py3-none-any.whl/lucid/nn/modules/einops.py
@@ -0,0 +1,19 @@
+import lucid
+import lucid.nn as nn
+import lucid.nn.functional as F
+
+from lucid._tensor import Tensor
+from lucid.einops._func import _EinopsPattern
+
+
+__all__ = ["Rearrange"]
+
+
+class Rearrange(nn.Module):
+    def __init__(self, pattern: _EinopsPattern, **shapes: int) -> None:
+        super().__init__()
+        self.pattern = pattern
+        self.shapes = shapes
+
+    def forward(self, input_: Tensor) -> Tensor:
+        return lucid.einops.rearrange(input_, self.pattern, **self.shapes)
