78687498cf0fe930ffa612d3e2a3de1c9a1481e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:21:39 +0000

    Add package mis-utils/1.0.0/mis_utils-1.0.0.tar.gz

---
+++ packages/mis-utils/mis_utils-1.0.0.tar.gz/mis_utils-1.0.0/mis_utils/operaciones.py
@@ -0,0 +1,18 @@
+MI_PI = 3.1415
+
+def suma(n1, n2):
+    return n1 + n2
+
+
+def resta(n1, n2):
+    return n1 - n2
+
+print(__name__)
+
+if __name__ == "__main__":
+    res = suma(2, 2)
+    assert res == 4
+
+    res = resta(2, 2)
+    assert res == 0
+
---
+++ packages/mis-utils/mis_utils-1.0.0.tar.gz/mis_utils-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='mis_utils',
+  version='1.0.0',
+  description='Paquete con utilidades básicas de operaciones matemáticas y traducciones',
+  long_description='## Paquete con utilidades básicas de ...',
+  long_description_content_type='text/markdown',
+  author='Ángel',
+  author_email='angel@gmail.com',
+  packages=find_packages()
+)
