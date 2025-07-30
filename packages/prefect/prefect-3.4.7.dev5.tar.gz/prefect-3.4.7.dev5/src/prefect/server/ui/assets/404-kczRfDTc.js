50b91bc965b65f5c1336d605d25d7a2dccab85e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:32:54 +0000

    Add package prefect/3.4.7.dev5/prefect-3.4.7.dev5.tar.gz

---
+++ packages/prefect/prefect-3.4.7.dev5.tar.gz/prefect-3.4.7.dev5/src/prefect/server/ui/assets/404-kczRfDTc.js
@@ -0,0 +1,2 @@
+import{d as e,c as o,o as a,n as t,dv as n}from"./index-D14GHAAp.js";import{u as r}from"./usePageTitle-BFNAGZxy.js";const m=e({__name:"404",setup(s){return r("Page Not Found"),(c,p)=>(a(),o(t(n)))}});export{m as default};
+//# sourceMappingURL=404-kczRfDTc.js.map
