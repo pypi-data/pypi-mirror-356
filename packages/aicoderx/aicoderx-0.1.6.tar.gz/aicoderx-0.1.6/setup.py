f7dfb570f2aae8da3d17272e9f955f0e1b80d3d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:22 +0000

    Add package aicoderx/0.1.6/aicoderx-0.1.6.tar.gz

---
+++ packages/aicoderx/aicoderx-0.1.6.tar.gz/aicoderx-0.1.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aicoderx",
+    version="0.1.6",
+    packages=find_packages(),
+    install_requires=[
+        "transformers",
+        "torch",
+        "tqdm",  # if used in spinner
+    ],
+    author="Jay Kilaparthi",
+    author_email="jayakeerthk@gmail.com",
+    description="Generate executable Python code from natural language prompts using an LLM.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/jayyvk/ai-coder",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
