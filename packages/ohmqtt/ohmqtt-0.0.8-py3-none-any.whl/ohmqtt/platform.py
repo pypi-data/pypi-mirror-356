5660a3aa517d11e0be614e9c52bc94081c5a5931
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:35 +0000

    Add package ohmqtt/0.0.8/ohmqtt-0.0.8-py3-none-any.whl

---
+++ packages/ohmqtt/ohmqtt-0.0.8-py3-none-any.whl/ohmqtt/platform.py
@@ -0,0 +1,11 @@
+import socket
+import sys
+from typing import cast, Final
+
+
+HAS_AF_UNIX: Final = sys.platform != "win32"
+
+if sys.platform == "win32":
+    AF_UNIX: Final = cast(socket.AddressFamily, 1)
+else:
+    AF_UNIX: Final = socket.AF_UNIX
