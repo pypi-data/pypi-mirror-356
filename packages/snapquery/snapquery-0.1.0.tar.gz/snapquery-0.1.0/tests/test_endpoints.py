8adb644c5d84e7b60014aeba6e2c2ea0e1e36aa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:38 +0000

    Add package snapquery/0.1.0/snapquery-0.1.0.tar.gz

---
+++ packages/snapquery/snapquery-0.1.0.tar.gz/snapquery-0.1.0/tests/test_endpoints.py
@@ -0,0 +1,29 @@
+"""
+Created on 2024-05-03
+
+@author: wf
+"""
+
+from ngwidgets.basetest import Basetest
+
+from snapquery.snapquery_core import NamedQueryManager
+
+
+class TestEndpoints(Basetest):
+    """
+    test endpoint handling according to https://github.com/WolfgangFahl/snapquery/issues/1
+    """
+
+    def setUp(self, debug=False, profile=True):
+        Basetest.setUp(self, debug=debug, profile=profile)
+        self.nqm = NamedQueryManager()
+
+    def testEndpoints(self):
+        """
+        test the endpoint handling
+        """
+        ep_names = self.nqm.endpoints.keys()
+        if self.debug:
+            print(ep_names)
+        self.assertTrue("wikidata" in ep_names)
+        pass
