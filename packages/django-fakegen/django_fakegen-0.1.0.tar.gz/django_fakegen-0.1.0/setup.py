ae8df73927d0dfbb39f746ab527d9c75f6917b79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:42 +0000

    Add package django-fakegen/0.1.0/django_fakegen-0.1.0.tar.gz

---
+++ packages/django-fakegen/django_fakegen-0.1.0.tar.gz/django_fakegen-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-fakegen',
+    version='0.1.0',
+    description='A Django package for generating fake data for your models.',
+    author='Mezo',
+    author_email='motazfawzy73@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Django>=3.2',
+        'Faker',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Framework :: Django',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+) 
