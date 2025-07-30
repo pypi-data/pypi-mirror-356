114b5e4e170ed6e1c5d9996e39ac25b6da5c0efd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:10:13 +0000

    Add package gsheeter/0.1.8/gsheeter-0.1.8.tar.gz

---
+++ packages/gsheeter/gsheeter-0.1.8.tar.gz/gsheeter-0.1.8/gsheeter/spreadsheet/sheet_types.py
@@ -0,0 +1,18 @@
+import pandas as pd, numpy as np
+from typing import Union
+
+
+DIMENSIONS = {
+	'dimensions': {
+		'outer_height': np.nan,
+		'outer_width': np.nan,
+		'inner_height': np.nan,
+		'inner_width': np.nan,
+		'column_height': np.nan,
+		'index_width': np.nan,
+	}
+}
+
+#FRAME_TYPES = (pd.DataFrame, pd.Series)
+#ARRAY_TYPES = (np.ndarray, list, tuple)
+#DATA_TYPES = Union[pd.DataFrame, pd.Series, np.ndarray, list, tuple]
