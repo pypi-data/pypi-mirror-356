4449dd3666a68dd0ccaeed9cc7bd41a4cef6b464
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:08:16 +0000

    Add package PyThermoModels/1.4.8/pythermomodels-1.4.8.tar.gz

---
+++ packages/PyThermoModels/pythermomodels-1.4.8.tar.gz/pythermomodels-1.4.8/pyThermoModels/configs/setting.py
@@ -0,0 +1,13 @@
+# PACKAGE INFO
+# --------------
+
+# package name
+packageName = 'Python Thermodynamic Models'
+# package short name
+packageShortName = 'PyThermoModels'
+# package version
+__version__ = '1.4.8'
+# package description
+__description__ = 'A Python package designed for the calculation of thermodynamic properties using various well-established models.'
+# author
+author = 'Sina Gilassi'
