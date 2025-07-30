8b8e7f276d3e52203aeb7324a55047dd6af4721f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:18:37 +0000

    Add package square-database-structure/2.5.7/square_database_structure-2.5.7.tar.gz

---
+++ packages/square-database-structure/square_database_structure-2.5.7.tar.gz/square_database_structure-2.5.7/square_database_structure/square/public/tables.py
@@ -0,0 +1,24 @@
+from sqlalchemy import MetaData, Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+from square_database_structure.square.public import global_string_schema_name
+
+Base = declarative_base(metadata=MetaData(schema=global_string_schema_name))
+
+
+class Test(Base):
+    __tablename__ = "test"
+
+    test_id = Column(
+        Integer, primary_key=True, nullable=False, unique=True, autoincrement=True
+    )
+    test_text = Column(String, nullable=False, unique=True)
+
+
+class App(Base):
+    __tablename__ = "app"
+
+    app_id = Column(
+        Integer, primary_key=True, nullable=False, unique=True, autoincrement=True
+    )
+    app_name = Column(String, nullable=False, unique=True)
