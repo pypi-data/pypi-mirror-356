c53a4c1c14579d26a49447b25cd86dadec26d7c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:30 +0000

    Add package qibocal/0.2.2/qibocal-0.2.2-py3-none-any.whl

---
+++ packages/qibocal/qibocal-0.2.2-py3-none-any.whl/qibocal/auto/history.py
@@ -0,0 +1,146 @@
+"""Track execution history."""
+
+import json
+import os
+from collections import defaultdict
+from dataclasses import dataclass, field
+from functools import singledispatchmethod
+from pathlib import Path
+from typing import Iterator, Optional
+
+from .task import Completed, Id, TaskId
+
+HISTORY = "history.json"
+"""File where protocols order is dumped."""
+
+
+@dataclass
+class History:
+    """Execution history.
+
+    This is not only used for logging and debugging, but it is an actual
+    part of the execution itself, since later routines can retrieve the
+    output of former ones from here.
+    """
+
+    _tasks: dict[Id, list[Completed]] = field(default_factory=lambda: defaultdict(list))
+    """List of completed tasks.
+
+    .. note::
+
+        Representing the object as a map of sequences makes it smoother to identify the
+        iterations of a given task, since they are already grouped together.
+    """
+    _order: list[TaskId] = field(default_factory=list)
+    """Record of the execution order."""
+
+    @property
+    def _serialized_order(self) -> list[str]:
+        """JSON friendly _order attribute.
+
+        _order, which is a list of TaskId objects, is not JSON serializable,
+        it is converted into a list of strings which match the data folder for each
+        protocol.
+        """
+        return [str(i) for i in self._order]
+
+    @singledispatchmethod
+    def __contains__(self, elem: Id):
+        """Check whether a generic or specific task has been completed."""
+        return elem in self._tasks
+
+    @__contains__.register
+    def _(self, elem: TaskId):
+        return len(self._tasks.get(elem.id, [])) > elem.iteration
+
+    @singledispatchmethod
+    def __getitem__(self, _):
+        """Access a generic or specific task."""
+        raise NotImplementedError
+
+    @__getitem__.register(str)
+    def _(self, key: Id) -> list[Completed]:
+        return self._tasks[key]
+
+    @__getitem__.register
+    def _(self, key: TaskId) -> Completed:
+        return self._tasks[key.id][key.iteration]
+
+    def __iter__(self) -> Iterator[TaskId]:
+        """Iterate individual tasks identifiers.
+
+        It follows the execution order.
+        """
+        return iter(self._order)
+
+    def values(self) -> Iterator[Completed]:
+        """Iterate individual tasks according to the execution order."""
+        return (self[task_id] for task_id in self)
+
+    def items(self) -> Iterator[tuple[TaskId, Completed]]:
+        """Consistent iteration over individual tasks and their ids."""
+        return ((task_id, self[task_id]) for task_id in self)
+
+    @classmethod
+    def load(cls, path: Path) -> "History":
+        """Load history from path.
+
+        Fill history with protocols contained in `data` path. If `history.json` is present,
+        they are sorted based on that, otherwise they are sorted by modification time.
+        """
+        instance = cls()
+        if not (path / HISTORY).exists():
+            protocols = sorted((path / "data").glob("*"), key=os.path.getmtime)
+        else:
+            raw_protocols = json.loads((path / HISTORY).read_text())
+            protocols = [path / "data" / protocol for protocol in raw_protocols]
+
+        for protocol in protocols:
+            instance.push(Completed.load(protocol))
+        return instance
+
+    def push(self, completed: Completed) -> TaskId:
+        """Adding completed task to history."""
+        id = completed.task.id
+        self._tasks[id].append(completed)
+        task_id = TaskId(id=id, iteration=len(self._tasks[id]) - 1)
+        self._order.append(task_id)
+        return task_id
+
+    @staticmethod
+    def route(task_id: TaskId, folder: Path) -> Path:
+        """Determine the path related to a completed task given TaskId.
+
+        `folder` should be usually the general output folder, used by Qibocal to store
+        all the execution results. Cf. :class:`qibocal.auto.output.Output`.
+        """
+        return folder / "data" / f"{task_id}"
+
+    def flush(self, output: Optional[Path] = None):
+        """Flush all content to disk.
+
+        Specifying `output` is possible to select which folder should be considered as
+        the general Qibocal output folder. Cf. :class:`qibocal.auto.output.Output`.
+        """
+        for task_id, completed in self.items():
+            if output is not None:
+                completed.path = self.route(task_id, output)
+            completed.flush()
+        (output / HISTORY).write_text(
+            json.dumps(self._serialized_order, indent=4), encoding="utf-8"
+        )
+
+    # TODO: implement time_travel()
+
+
+class DummyHistory:
+    """Empty History object, used by `qq compare`.
+
+    Used for comparing multiple reports, as their history is not saved.
+    """
+
+    def flush(self, output=None):
+        pass
+
+    def items(self):
+        return tuple()
---
+++ packages/qibocal/qibocal-0.2.2-py3-none-any.whl/qibocal/protocols/signal_experiments/time_of_flight_readout.py
@@ -0,0 +1,162 @@
+from dataclasses import dataclass, field
+from typing import Optional
+
+import numpy as np
+import numpy.typing as npt
+import plotly.graph_objects as go
+from qibolab import AcquisitionType, AveragingMode, PulseSequence
+
+from qibocal.auto.operation import Data, Parameters, QubitId, Results, Routine
+from qibocal.calibration import CalibrationPlatform
+from qibocal.protocols.utils import table_dict, table_html
+from qibocal.result import magnitude
+from qibocal.update import replace
+
+__all__ = ["time_of_flight_readout"]
+
+
+@dataclass
+class TimeOfFlightReadoutParameters(Parameters):
+    """TimeOfFlightReadout runcard inputs."""
+
+    readout_amplitude: Optional[int] = None
+    """Amplitude of the readout pulse."""
+    window_size: Optional[int] = 10
+    """Window size for the moving average."""
+
+
+@dataclass
+class TimeOfFlightReadoutResults(Results):
+    """TimeOfFlightReadout outputs."""
+
+    fitted_parameters: dict[QubitId, dict[str, float]]
+    """Raw fitting output."""
+
+
+TimeOfFlightReadoutType = np.dtype([("samples", np.float64)])
+
+
+@dataclass
+class TimeOfFlightReadoutData(Data):
+    """TimeOfFlightReadout acquisition outputs."""
+
+    windows_size: int
+    sampling_rate: int
+
+    data: dict[QubitId, npt.NDArray] = field(default_factory=dict)
+    """Raw data acquired."""
+
+
+def _acquisition(
+    params: TimeOfFlightReadoutParameters,
+    platform: CalibrationPlatform,
+    targets: list[QubitId],
+) -> TimeOfFlightReadoutData:
+    """Data acquisition for time of flight experiment."""
+
+    sequence = PulseSequence()
+
+    ro_pulses = {}
+    native = platform.natives.single_qubit
+    for qubit in targets:
+        ro_channel, ro_pulse = native[qubit].MZ()[0]
+        if params.readout_amplitude is not None:
+            ro_pulse = replace(ro_pulse, amplitude=params.readout_amplitude)
+        ro_pulses[qubit] = ro_pulse
+        sequence.append((ro_channel, ro_pulse))
+
+    results = platform.execute(
+        [sequence],
+        nshots=params.nshots,
+        relaxation_time=params.relaxation_time,
+        acquisition_type=AcquisitionType.RAW,
+        averaging_mode=AveragingMode.CYCLIC,
+    )
+
+    data = TimeOfFlightReadoutData(
+        windows_size=params.window_size, sampling_rate=platform.sampling_rate
+    )
+
+    # retrieve and store the results for every qubit
+    for qubit in targets:
+        samples = magnitude(results[ro_pulses[qubit].id])
+        # store the results
+        data.register_qubit(TimeOfFlightReadoutType, (qubit), dict(samples=samples))
+    return data
+
+
+def _fit(data: TimeOfFlightReadoutData) -> TimeOfFlightReadoutResults:
+    """Post-processing function for TimeOfFlightReadout."""
+
+    qubits = data.qubits
+    fitted_parameters = {}
+
+    window_size = data.windows_size
+    sampling_rate = data.sampling_rate
+
+    for qubit in qubits:
+        qubit_data = data[qubit]
+        # Calculate moving average change per element
+        moving_average_deltas = np.ediff1d(
+            np.convolve(
+                qubit_data.samples, np.ones(window_size) / window_size, mode="valid"
+            )
+        )
+
+        max_average_change = np.argmax(moving_average_deltas)
+        time_of_flight_readout = max_average_change / sampling_rate
+        fitted_parameters[qubit] = time_of_flight_readout
+
+    return TimeOfFlightReadoutResults(fitted_parameters)
+
+
+def _plot(
+    data: TimeOfFlightReadoutData, target: QubitId, fit: TimeOfFlightReadoutResults
+):
+    """Plotting function for TimeOfFlightReadout."""
+
+    figures = []
+    fitting_report = ""
+    fig = go.Figure()
+    qubit_data = data[target]
+    sampling_rate = data.sampling_rate
+    y = qubit_data.samples
+
+    fig.add_trace(
+        go.Scatter(
+            y=y,
+            textposition="bottom center",
+            name="Expectation value",
+            showlegend=True,
+            legendgroup="group1",
+        ),
+    )
+
+    fig.update_layout(
+        showlegend=True,
+        xaxis_title="Sample",
+        yaxis_title="Signal [a.u.]",
+    )
+    if fit is not None:
+        fig.add_vline(
+            x=fit.fitted_parameters[target] * sampling_rate,
+            line_width=2,
+            line_dash="dash",
+            line_color="grey",
+        )
+        fitting_report = table_html(
+            table_dict(target, "Time of flights [ns]", fit.fitted_parameters[target])
+        )
+    fig.update_layout(
+        showlegend=True,
+        xaxis_title="Sample",
+        yaxis_title="Signal [a.u.]",
+    )
+
+    figures.append(fig)
+
+    return figures, fitting_report
+
+
+time_of_flight_readout = Routine(_acquisition, _fit, _plot)
+"""TimeOfFlightReadout Routine object."""
