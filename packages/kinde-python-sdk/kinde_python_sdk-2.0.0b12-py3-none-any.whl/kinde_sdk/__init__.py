644b60a33e685ecb7ea9def333fafba0bd1db233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:34:13 +0000

    Add package kinde-python-sdk/2.0.0b12/kinde_python_sdk-2.0.0b12-py3-none-any.whl

---
+++ packages/kinde-python-sdk/kinde_python_sdk-2.0.0b12-py3-none-any.whl/kinde_sdk/__init__.py
@@ -0,0 +1,28 @@
+"""
+Kinde Python SDK
+~~~~~~~~~~~~~~~
+
+This is the Kinde Python SDK for interacting with the Kinde API.
+"""
+
+from kinde_sdk.auth.oauth import OAuth
+from kinde_sdk.core.storage.storage_factory import StorageFactory
+from kinde_sdk.core.framework.framework_factory import FrameworkFactory
+from kinde_sdk.core.framework.framework_interface import FrameworkInterface
+from kinde_sdk.core.framework.null_framework import NullFramework
+
+__version__ = "2.1.0"
+
+__all__ = [
+    "OAuth",
+    "TokenManager",
+    "UserSession",
+    "permissions",
+    "claims",
+    "portals",
+    "feature_flags"
+    "StorageFactory",
+    "FrameworkFactory",
+    "FrameworkInterface",
+    "NullFramework",
+]
