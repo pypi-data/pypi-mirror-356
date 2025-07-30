ae48c08e403a604eb1e0006b755d290c3a73883c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:10:19 +0000

    Add package kbputils/0.1.1/kbputils-0.1.1.tar.gz

---
+++ packages/kbputils/kbputils-0.1.1.tar.gz/kbputils-0.1.1/kbputils/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+__version__ = '0.1.1'
+
+__all__ = ['KBPFile', 'AssConverter', 'DoblonTxtConverter', 'LRCConverter']
+
+from .kbp import *
+from .doblontxt import *
+from .lrc import *
+from .converters import *
