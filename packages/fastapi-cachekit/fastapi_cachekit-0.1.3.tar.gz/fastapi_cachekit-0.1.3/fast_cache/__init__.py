52c2bd483bd512b044e809f8221502f910e91a7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:08:26 +0000

    Add package fastapi-cachekit/0.1.3/fastapi_cachekit-0.1.3.tar.gz

---
+++ packages/fastapi-cachekit/fastapi_cachekit-0.1.3.tar.gz/fastapi_cachekit-0.1.3/fast_cache/__init__.py
@@ -0,0 +1,25 @@
+from .integration import FastAPICache
+from .backends.backend import CacheBackend
+
+from .backends.redis import RedisBackend
+from .backends.memory import InMemoryBackend
+from .backends.postgres import PostgresBackend
+from .backends.memcached import MemcachedBackend
+from .backends.mongodb import MongoDBBackend
+from .backends.google_firestore import FirestoreBackend
+
+__all__ = [
+    "FastAPICache",
+    "RedisBackend",
+    "CacheBackend",
+    "InMemoryBackend",
+    "PostgresBackend",
+    "cache",
+    "MemcachedBackend",
+    "MongoDBBackend",
+    "FirestoreBackend"
+]
+
+
+# Create global cache instance
+cache = FastAPICache()
