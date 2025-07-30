c8c3e9d1cf5ab711a8f5e53371ba37f40ad2be06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:14 +0000

    Add package unieai-excel-mcp/0.1.7/unieai_excel_mcp-0.1.7.tar.gz

---
+++ packages/unieai-excel-mcp/unieai_excel_mcp-0.1.7.tar.gz/unieai_excel_mcp-0.1.7/unieai_excel_mcp/tools/write_excel.py
@@ -0,0 +1,22 @@
+from mcp import FastMCP
+from unieai_excel_mcp.services.fill_excel import fill_excel
+
+mcp = FastMCP("unieai-excel-mcp")
+
+@mcp.tool()
+def write_data_to_excel_with_custom(
+    filepath: str,
+    outputpath: str,
+    data: dict,
+) -> str:
+    """
+    將資料寫入 Excel 模板中對應的 {{key}} 位置。
+    """
+    try:
+        return fill_excel({
+            "filepath": filepath,
+            "outputpath": outputpath,
+            "data": data
+        })
+    except Exception as e:
+        return f"Error: {e}"
