6e1781e28c070e29e840d12c795b4588ebc85622
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:10:21 +0000

    Add package thsdk/1.4.10/thsdk-1.4.10.tar.gz

---
+++ packages/thsdk/thsdk-1.4.10.tar.gz/thsdk-1.4.10/thsdk/examples/tick_super_level1.py
@@ -0,0 +1,16 @@
+from thsdk import THS
+import pandas as pd
+import time
+
+with THS() as ths:
+    response = ths.tick_super_level1("USZA300033")
+    print("超级盘口带买卖5档位盘口:")
+    if not response.is_success():
+        print(f"错误信息: {response.err_info}")
+
+    df = pd.DataFrame(response.get_result())
+    df["时间"] = pd.to_datetime(df["时间"], unit="s").dt.tz_localize("UTC").dt.tz_convert("Asia/Shanghai")
+    # pd.set_option('display.max_columns', None)
+    # pd.set_option('display.max_rows', None)
+    print(df)
+    time.sleep(1)
