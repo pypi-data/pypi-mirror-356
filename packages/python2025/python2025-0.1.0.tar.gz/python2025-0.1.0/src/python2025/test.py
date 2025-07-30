2a0fb74eb004e21e87874ddbc269537cc52bedff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:50 +0000

    Add package python2025/0.1.0/python2025-0.1.0.tar.gz

---
+++ packages/python2025/python2025-0.1.0.tar.gz/python2025-0.1.0/src/python2025/test.py
@@ -0,0 +1,7 @@
+SOME_LONG_STRING = "1234567890_1234567890_1234567890_1234567890_1234567890_1234567890_1234567890_1234567890_1234567890_1234567890_1234567890_1234567890_1111111111"
+
+
+d = {
+    "ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss",
+    {"dddddddddddddddddddddddddddddddddddddddddddddd"},
+}
