c191600f86b6806e037261a67615f5c0e751c1d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:07:40 +0000

    Add package operacionesmateo/0.1.0/operacionesmateo-0.1.0.tar.gz

---
+++ packages/operacionesmateo/operacionesmateo-0.1.0.tar.gz/operacionesmateo-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    description = fh.read()
+
+setup(
+    name='operacionesmateo',
+    packages=find_packages(include=['operaciones']),
+    version='0.1.0',
+    description="libreria de operaciones básicas",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    author='Mateo Patiño',
+    license='MIT',
+    install_requires=[],
+    python_requires=">=3.10.12"
+)
+
