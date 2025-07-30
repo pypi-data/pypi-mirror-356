f4e329fa4cfe207b966fe06e83f500fb8177f7f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:37 +0000

    Add package cr-utils/0.1.5/cr_utils-0.1.5-py3-none-any.whl

---
+++ packages/cr-utils/cr_utils-0.1.5-py3-none-any.whl/cr_utils/function/__init__.py
@@ -0,0 +1,8 @@
+from .make import (
+    set_variable_with_default,
+    killall_processes, make_interrupt_handler, make_main,
+    make_async, make_sync,
+)
+from .files import read_jsonl, write_jsonl
+from .text import normalize_text
+from .image import encode_image
