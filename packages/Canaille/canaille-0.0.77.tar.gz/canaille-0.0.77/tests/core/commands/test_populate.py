1aea336e6eacf8b901766f13e4e94b113a65be61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:05:28 +0000

    Add package Canaille/0.0.77/canaille-0.0.77.tar.gz

---
+++ packages/Canaille/canaille-0.0.77.tar.gz/canaille-0.0.77/tests/core/commands/test_populate.py
@@ -0,0 +1,27 @@
+from canaille.app import models
+from canaille.commands import cli
+from canaille.core.populate import fake_users
+
+
+def test_populate_users(cli_runner, backend):
+    assert len(backend.query(models.User)) == 0
+    res = cli_runner.invoke(cli, ["populate", "--nb", "10", "users"])
+    assert res.exit_code == 0, res.stdout
+    assert len(backend.query(models.User)) == 10
+    for user in backend.query(models.User):
+        backend.delete(user)
+
+
+def test_populate_groups(cli_runner, backend):
+    fake_users(10)
+
+    assert len(backend.query(models.Group)) == 0
+    res = cli_runner.invoke(cli, ["populate", "--nb", "10", "groups"])
+    assert res.exit_code == 0, res.stdout
+    assert len(backend.query(models.Group)) == 10
+
+    for group in backend.query(models.Group):
+        backend.delete(group)
+
+    for user in backend.query(models.User):
+        backend.delete(user)
