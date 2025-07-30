cb6905cc04c2e6e567d222fe7be7b7dcf1f00b34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:21 +0000

    Add package vectorvein/0.3.1/vectorvein-0.3.1.tar.gz

---
+++ packages/vectorvein/vectorvein-0.3.1.tar.gz/vectorvein-0.3.1/src/vectorvein/workflow/nodes/triggers.py
@@ -0,0 +1,21 @@
+from typing import Optional
+
+from ..graph.node import Node
+from ..graph.port import PortType, InputPort
+
+
+class ButtonTrigger(Node):
+    def __init__(self, id: Optional[str] = None):
+        super().__init__(
+            node_type="ButtonTrigger",
+            category="triggers",
+            task_name="triggers.button_trigger",
+            node_id=id,
+            ports={
+                "button_text": InputPort(
+                    name="button_text",
+                    port_type=PortType.INPUT,
+                    value="",
+                ),
+            },
+        )
