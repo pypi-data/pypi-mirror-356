be7879a245738dd846d9125fe48b2af487c7f1bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:20 +0000

    Add package seqattn/0.0.1/seqattn-0.0.1-py3-none-any.whl

---
+++ packages/seqattn/seqattn-0.0.1-py3-none-any.whl/seqattn/__init__.py
@@ -0,0 +1,17 @@
+"""
+seqattn - A convenient lib for sequence-level attention abstraction, powered by flashinfer.
+"""
+
+from .cache import NoPageError, FlashInferPackedData, PagedKVCacheManager
+from .descriptor import SequenceData, CacheDescriptor, HeadIDGenerator
+
+__all__ = [
+    "NoPageError",
+    "FlashInferPackedData", 
+    "PagedKVCacheManager",
+    "SequenceData",
+    "CacheDescriptor",
+    "HeadIDGenerator",
+]
+
+__version__ = "0.0.1"
