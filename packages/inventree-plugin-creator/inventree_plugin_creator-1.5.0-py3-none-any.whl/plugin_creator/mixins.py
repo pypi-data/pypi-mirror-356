7796aac68f5c0025cad165cef8a91f79fe8d3212
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:47 +0000

    Add package inventree-plugin-creator/1.5.0/inventree_plugin_creator-1.5.0-py3-none-any.whl

---
+++ packages/inventree-plugin-creator/inventree_plugin_creator-1.5.0-py3-none-any.whl/plugin_creator/mixins.py
@@ -0,0 +1,79 @@
+"""InvenTree plugin mixin selection."""
+
+import os
+
+import questionary
+from questionary.prompts.common import Choice
+
+from .helpers import info
+
+
+def available_mixins() -> list:
+    """Return a list of available plugin mixin classes."""
+
+    # TODO: Support the commented out mixins
+
+    return [
+        # 'ActionMixin',
+        # 'APICallMixin',
+        # 'AppMixin',
+        # 'BarcodeMixin',
+        # 'BulkNotificationMethod',
+        # 'CurrencyExchangeMixin',
+        'EventMixin',
+        # 'DataExportMixin',
+        # 'IconPackMixin',
+        # 'LabelPrintingMixin',
+        'LocateMixin',
+        # 'MailMixin',
+        # 'NavigationMixin',
+        'ReportMixin',
+        'ScheduleMixin',
+        'SettingsMixin',
+        # 'SupplierBarcodeMixin',
+        'UrlsMixin',
+        'UserInterfaceMixin',
+        'ValidationMixin',
+    ]
+
+
+def get_mixins() -> list:
+    """Ask user to select plugin mixins."""
+
+    # Default mixins to select
+    defaults = ['SettingsMixin', 'UserInterfaceMixin']
+
+    choices = [
+        Choice(
+            title=title,
+            checked=title in defaults,
+        ) for title in available_mixins()
+    ]
+
+    return questionary.checkbox(
+        "Select plugin mixins",
+        choices=choices
+    ).ask()
+
+
+def cleanup_mixins(plugin_dir: str, context: dict) -> list:
+    """Post-build step to remove certain files based on selected mixins."""
+
+    mixins = context['plugin_mixins']['mixin_list']
+
+    src_dir = os.path.join(
+        plugin_dir,
+        context['package_name'],
+    )
+
+    if "UrlsMixin" not in mixins:
+        # Remove files associated with the UrlsMixin
+        for filename in [
+            'serializers.py',
+            'views.py',
+        ]:
+            path = os.path.join(src_dir, filename)
+
+            if os.path.exists(path):
+                info("Removing:", filename)
+                os.remove(path)
