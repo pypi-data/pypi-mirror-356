234e8fc0d388541cd13491f6da1005d224573d53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:19 +0000

    Add package attoworld/2025.0.27/attoworld-2025.0.27-cp311-cp311-win32.whl

---
+++ packages/attoworld/attoworld-2025.0.27-cp311-cp311-win32.whl/attoworld/personal/nick/__init__.py
@@ -0,0 +1,7 @@
+"""
+This file contains functions primarily used by Nick Karpowicz.
+"""
+
+from .nick import dark_plot
+from .impulse_response import get_effective_response
+__all__ = ["dark_plot", "get_effective_response"]
