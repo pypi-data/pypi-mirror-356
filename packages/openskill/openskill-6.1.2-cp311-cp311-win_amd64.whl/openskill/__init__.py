6bd038c1e4303989f10e821addaeaf3514ab659e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:09:41 +0000

    Add package openskill/6.1.2/openskill-6.1.2-cp311-cp311-win_amd64.whl

---
+++ packages/openskill/openskill-6.1.2-cp311-cp311-win_amd64.whl/openskill/__init__.py
@@ -0,0 +1,16 @@
+from typing import List
+
+# Public API
+__all__: List[str] = ["models"]
+
+
+# Metadata
+__version__ = "6.1.2"
+__author__ = "Vivek Joshy"
+__email__ = "git@vivekjoshy.com"
+__copyright__ = "Copyright 2023 - 2025, Vivek Joshy"
+__credits__ = ["Philihp Busby"]
+__deprecated__ = False
+__license__ = "MIT"
+__maintainer__ = "Vivek Joshy"
+__status__ = "Production"
