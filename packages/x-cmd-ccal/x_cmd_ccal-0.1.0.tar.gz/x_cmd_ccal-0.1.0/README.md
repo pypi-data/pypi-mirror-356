10abd81410d8d1023be7bc7523d5ce06c86aac54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:52 +0000

    Add package x-cmd-ccal/0.1.0/x_cmd_ccal-0.1.0.tar.gz

---
+++ packages/x-cmd-ccal/x_cmd_ccal-0.1.0.tar.gz/x_cmd_ccal-0.1.0/README.md
@@ -0,0 +1,29 @@
+# rili
+
+一个带农历和节日的终端日历应用，基于 [Textual](https://github.com/Textualize/textual)。
+
+## 方法一: 采用 x-cmd
+
+```
+x rili
+```
+
+## 方法二: 安装
+
+```bash
+pip install x-cmd-rili
+```
+
+## 使用
+
+```bash
+rili
+```
+
+## 依赖
+- textual
+- cnlunar
+- rich
+
+## 许可证
+MIT
---
+++ packages/x-cmd-ccal/x_cmd_ccal-0.1.0.tar.gz/x_cmd_ccal-0.1.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="x-cmd-ccal",
+    version="0.1.0",
+    description="一个带农历和节日的终端日历应用（Textual UI）",
+    author="Li Junhao",
+    author_email="li.junhao@x-cmd.com",
+    license="AGPLv3",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    install_requires=[
+        "textual>=0.40.0",
+        "cnlunar",
+        "rich"
+    ],
+    entry_points={
+        "console_scripts": [
+            "ccal=app:main_entry"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
