690b9f0812b089fb5d10fbf36612f14dd245ea9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:20 +0000

    Add package cheetah-accelerator/0.7.4/cheetah_accelerator-0.7.4.tar.gz

---
+++ packages/cheetah-accelerator/cheetah_accelerator-0.7.4.tar.gz/cheetah_accelerator-0.7.4/cheetah/__init__.py
@@ -0,0 +1,23 @@
+from . import converters  # noqa: F401
+from .accelerator import (  # noqa: F401
+    BPM,
+    Aperture,
+    Cavity,
+    CustomTransferMap,
+    Dipole,
+    Drift,
+    Element,
+    HorizontalCorrector,
+    Marker,
+    Quadrupole,
+    RBend,
+    Screen,
+    Segment,
+    Sextupole,
+    Solenoid,
+    SpaceChargeKick,
+    TransverseDeflectingCavity,
+    Undulator,
+    VerticalCorrector,
+)
+from .particles import Beam, ParameterBeam, ParticleBeam, Species  # noqa: F401
---
+++ packages/cheetah-accelerator/cheetah_accelerator-0.7.4.tar.gz/cheetah_accelerator-0.7.4/setup.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="cheetah-accelerator",
+    version="0.7.4",
+    author="Jan Kaiser & Chenran Xu",
+    author_email="jan.kaiser@desy.de",
+    url="https://github.com/desy-ml/cheetah",
+    description=(
+        "Fast and differentiable particle accelerator optics simulation for"
+        " reinforcement learning and optimisation applications."
+    ),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=[package for package in find_packages() if package.startswith("cheetah")],
+    python_requires=">=3.10",
+    install_requires=[
+        "matplotlib>=3.5",
+        "numpy>=1.21.3",
+        "scipy>=1.7.2",
+        "torch[opt-einsum]>=2.3",
+    ],
+    extras_require={"openpmd": ["openpmd-beamphysics"]},
+)
