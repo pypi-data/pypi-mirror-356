f8e8e4a2a416ad39b2aafc4d8301a87807b5eb52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:10 +0000

    Add package zhmiscellany/5.4.3/zhmiscellany-5.4.3-py3-none-any.whl

---
+++ packages/zhmiscellany/zhmiscellany-5.4.3-py3-none-any.whl/zhmiscellany/netio.py
@@ -0,0 +1,55 @@
+import os, requests
+import zhmiscellany.string
+import urllib.parse
+from zhmiscellany._misc_supportfuncs import patch_rhg
+
+
+def resolve_file(url, destination_folder="."):
+    file_name = urllib.parse.unquote(url.split("/")[-1])
+    file_name = file_name.split('?')[0]
+    destination_path = f"{destination_folder}/{file_name}"
+    if len(destination_path) > 250:
+        destination_path = f'{destination_path[:(250 - len(os.path.splitext(destination_path)[1]))]}{os.path.splitext(destination_path)[1]}'
+    return destination_path
+
+
+def download_file(url, destination_folder=".", just_return_path=False, headers=None, file_path=None):
+    if file_path:
+        destination_path = file_path
+    else:
+        destination_path = resolve_file(url, destination_folder)
+
+    if os.path.exists(destination_path):
+        if just_return_path:
+            return destination_path
+        return False
+    if headers:
+        response = requests.get(url, stream=True, headers=headers)
+    else:
+        response = requests.get(url, stream=True)
+    if response.status_code == 200:
+
+        # Save the file
+        with open(destination_path, 'wb') as file:
+            for chunk in response.iter_content(chunk_size=2**12):
+                file.write(chunk)
+
+        return destination_path
+
+    else:
+        raise Exception(f"Failed to download the file. Status code: {response.status_code}")
+
+
+def generate_headers(url):
+    from random_header_generator import HeaderGenerator
+    generator = HeaderGenerator()
+    headers = {
+    }
+
+    for k, v in generator().items():
+        headers[k] = v
+
+    headers['Referer'] = url
+    headers['Host'] = urllib.parse.urlparse(url).netloc
+
+    return headers
