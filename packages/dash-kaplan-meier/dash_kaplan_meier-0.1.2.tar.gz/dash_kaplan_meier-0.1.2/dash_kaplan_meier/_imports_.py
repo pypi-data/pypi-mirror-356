4380057a98a39811485bc541b11742e170b2dc07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:13 +0000

    Add package dash-kaplan-meier/0.1.2/dash_kaplan_meier-0.1.2.tar.gz

---
+++ packages/dash-kaplan-meier/dash_kaplan_meier-0.1.2.tar.gz/dash_kaplan_meier-0.1.2/dash_kaplan_meier/_imports_.py
@@ -0,0 +1,5 @@
+from .DashKaplanMeier import DashKaplanMeier
+
+__all__ = [
+    "DashKaplanMeier"
+]
---
+++ packages/dash-kaplan-meier/dash_kaplan_meier-0.1.2.tar.gz/dash_kaplan_meier-0.1.2/setup.py
@@ -0,0 +1,38 @@
+import json
+from setuptools import setup
+from pathlib import Path
+
+here = Path(__file__).parent
+with open('package.json') as f:
+    package = json.load(f)
+long_description = (here / 'README.md').read_text()
+
+package_name = package["name"].replace(" ", "_").replace("-", "_")
+
+setup(
+    name=package_name,  
+    version=package["version"],
+    author=package['author'],
+    author_email="llbt.nvs.x@gmail.com",
+    packages=[package_name],
+    include_package_data=True,
+    package_data={
+        package_name: [
+            "*.js",
+            "*.js.map",
+            "*.json"
+        ]
+    },
+    license=package['license'],
+    description=package.get('description', package_name),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "dash",
+        "lifelines",
+    ],
+    classifiers = [
+        'Framework :: Dash',
+    ],    
+    url="https://github.com/XLlobet/dash-kaplan-meier",
+)
