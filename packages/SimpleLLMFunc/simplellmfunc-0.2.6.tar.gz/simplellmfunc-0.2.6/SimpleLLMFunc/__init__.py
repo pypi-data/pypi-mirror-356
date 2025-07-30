3a2c57a69135c4bf12f5ea11b3a6019f549072ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:11 +0000

    Add package SimpleLLMFunc/0.2.6/simplellmfunc-0.2.6.tar.gz

---
+++ packages/SimpleLLMFunc/simplellmfunc-0.2.6.tar.gz/simplellmfunc-0.2.6/SimpleLLMFunc/__init__.py
@@ -0,0 +1,9 @@
+import nest_asyncio
+nest_asyncio.apply()
+
+
+from SimpleLLMFunc.llm_decorator import *
+from SimpleLLMFunc.logger import *
+from SimpleLLMFunc.interface import *
+from SimpleLLMFunc.tool import *
+from SimpleLLMFunc.config import *  
