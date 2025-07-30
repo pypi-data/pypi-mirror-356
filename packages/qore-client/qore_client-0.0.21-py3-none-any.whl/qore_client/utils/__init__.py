7791f0812368e43975ee7c9b434ffdf4ed50f72a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:58 +0000

    Add package qore-client/0.0.21/qore_client-0.0.21-py3-none-any.whl

---
+++ packages/qore-client/qore_client-0.0.21-py3-none-any.whl/qore_client/utils/__init__.py
@@ -0,0 +1,19 @@
+from .drive import DriveOperations
+from .file import FileOperations
+from .folder import FolderOperations
+from .module import ModuleImportManager
+from .organization import OrganizationOperations
+from .webhook import WebhookOperations
+from .workflow.workflow import WorkflowOperations
+from .workspace import WorkspaceOperations
+
+__all__ = [
+    "DriveOperations",
+    "FileOperations",
+    "FolderOperations",
+    "ModuleImportManager",
+    "OrganizationOperations",
+    "WorkflowOperations",
+    "WorkspaceOperations",
+    "WebhookOperations",
+]
