e21da56b34334eb4d5cf33ae450267349ad35b92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:37 +0000

    Add package machine-learning-toolkit/0.1.1/machine_learning_toolkit-0.1.1.tar.gz

---
+++ packages/machine-learning-toolkit/machine_learning_toolkit-0.1.1.tar.gz/machine_learning_toolkit-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='machine-learning-toolkit',
+    version='0.1.1',
+    description='Utility for evaluating ML models with scikit-learn',
+    author='Your Name',
+    author_email='your.email@example.com',
+    packages=["machine_learning_toolkit"],
+    install_requires=[
+        'scikit-learn>=1.0.0',
+        'matplotlib>=3.0.0'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.7',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+)
