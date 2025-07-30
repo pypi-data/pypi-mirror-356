2136c4815ad101e0ecdedeec0c8934b8cb572de2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:47:25 +0000

    Add package cratonml/0.1.1.32/cratonml-0.1.1.32.tar.gz

---
+++ packages/cratonml/cratonml-0.1.1.32.tar.gz/cratonml-0.1.1.32/cratonml/exceptions/WellExceptions.py
@@ -0,0 +1,14 @@
+"""Модуль исключений при работе со скважинными данными.
+"""
+
+
+class WellException(BaseException):
+    """Базовый класс исключений при работе со скважинными данными"""
+
+    pass
+
+
+class WellTrajectoryDepthException(WellException):
+    """"""
+
+    pass
