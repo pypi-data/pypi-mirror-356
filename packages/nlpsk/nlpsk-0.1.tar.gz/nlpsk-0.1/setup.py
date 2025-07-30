e236b32ce6a253ac496482b7e285a7430bb84e90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:48:30 +0000

    Add package nlpsk/0.1/nlpsk-0.1.tar.gz

---
+++ packages/nlpsk/nlpsk-0.1.tar.gz/nlpsk-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nlpsk",
+    version="0.1",
+    packages=find_packages(),  # Автоматически найдет все пакеты
+    description="NLP toolkit based on scikit-learn",
+    author="DEV | SJ | Si Jey",
+    author_email="sijeydev@email.com",
+)
