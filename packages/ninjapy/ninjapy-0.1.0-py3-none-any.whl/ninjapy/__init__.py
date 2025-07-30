f658f1a835a7ff030b18a974a63c4f7a28723ce2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:15 +0000

    Add package ninjapy/0.1.0/ninjapy-0.1.0-py3-none-any.whl

---
+++ packages/ninjapy/ninjapy-0.1.0-py3-none-any.whl/ninjapy/__init__.py
@@ -0,0 +1,5 @@
+from .client import NinjaRMMClient
+from .exceptions import NinjaRMMError, NinjaRMMAuthError
+
+__version__ = "0.1.0"
+__all__ = ["NinjaRMMClient", "NinjaRMMError", "NinjaRMMAuthError"] 
