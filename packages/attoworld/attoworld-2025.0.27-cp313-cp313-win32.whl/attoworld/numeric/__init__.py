4cc9c97d715327e176cb0069b95b611b3d24e8d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:20 +0000

    Add package attoworld/2025.0.27/attoworld-2025.0.27-cp313-cp313-win32.whl

---
+++ packages/attoworld/attoworld-2025.0.27-cp313-cp313-win32.whl/attoworld/numeric/__init__.py
@@ -0,0 +1,18 @@
+"""
+This module will contain numerical tools.
+"""
+
+from .atomic_units import AtomicUnits
+from ..attoworld_rs import fornberg_stencil, interpolate, fwhm, find_first_intercept, find_last_intercept, find_maximum_location, derivative, derivative_periodic
+
+__all__ = [
+    "fornberg_stencil",
+    "find_maximum_location",
+    "fwhm",
+    "find_first_intercept",
+    "find_last_intercept",
+    "derivative",
+    "derivative_periodic",
+    "interpolate",
+    "AtomicUnits"
+]
