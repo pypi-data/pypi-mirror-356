bdbf2422a44c2faae514c6c35ef94dc489b5445b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:00 +0000

    Add package visivo/1.0.61/visivo-1.0.61.tar.gz

---
+++ packages/visivo/visivo-1.0.61.tar.gz/visivo-1.0.61/visivo/commands/aggregate.py
@@ -0,0 +1,15 @@
+import click
+from visivo.query.aggregator import Aggregator
+from .options import output_dir
+
+
+@click.command()
+@output_dir
+@click.option("-j", "--json-file", help="The file with the raw json results from the query")
+def aggregate(output_dir, json_file):
+    from visivo.logging.logger import Logger
+
+    Logger.instance().info("Aggregating")
+
+    Aggregator.aggregate(trace_dir=output_dir, json_file=json_file)
+    Logger.instance().success("Done")
