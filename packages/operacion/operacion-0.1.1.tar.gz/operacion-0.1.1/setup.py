40931bb0e00d1527f6ae88b848f061cab0bddc65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:07:56 +0000

    Add package operacion/0.1.1/operacion-0.1.1.tar.gz

---
+++ packages/operacion/operacion-0.1.1.tar.gz/operacion-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    description = fh.read()
+
+#configuracion de la libreria
+
+setup(
+    name='operacion', #nombre de la carpeta
+    packages=find_packages(include=['operaciones']),
+    version='0.1.1',
+    description="libreria de operaciones básicas",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    author='Alexis Ruales',
+    license='MIT',
+    install_requires=[],
+    python_requires=">=3.11.13"
+)
