447447efd2bf02a796d3d14171df9f739339855f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:17:25 +0000

    Add package asyauth-bAD/0.0.28/asyauth_bad-0.0.28.tar.gz

---
+++ packages/asyauth-bAD/asyauth_bad-0.0.28.tar.gz/asyauth_bad-0.0.28/asyauth/_version.py
@@ -0,0 +1,8 @@
+
+__version__ = "0.0.28"
+__banner__ = \
+"""
+# asyauth %s 
+# Author: Tamas Jos @skelsec (info@skelsecprojects.com)
+# Current Maintainer: Baptiste Crépin (baptiste@cravaterouge.com)
+""" % __version__
