82de60a04815f8066ebdb6187ef9de51bd6e8a07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:58 +0000

    Add package stock-pandas/2.1.2/stock_pandas-2.1.2.tar.gz

---
+++ packages/stock-pandas/stock_pandas-2.1.2.tar.gz/stock_pandas-2.1.2/stock_pandas/meta/__init__.py
@@ -0,0 +1,13 @@
+from .cumulator import (
+    MetaDataFrame,
+    Cumulator,
+    cumulators
+)
+
+from .utils import ColumnInfo
+
+from .time_frame import (
+    TimeFrame,
+    TimeFrameArg,
+    ensure_time_frame
+)
