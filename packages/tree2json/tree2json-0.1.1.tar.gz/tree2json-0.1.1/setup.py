978905075c359de7170432d1463eeed03c627c9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:22 +0000

    Add package tree2json/0.1.1/tree2json-0.1.1.tar.gz

---
+++ packages/tree2json/tree2json-0.1.1.tar.gz/tree2json-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tree2json",
+    version="0.1.0",
+    packages=find_packages(),
+    author="knighthood2001",
+    description="将项目目录树字符串转换为JSON结构",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    python_requires=">=3.6",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
