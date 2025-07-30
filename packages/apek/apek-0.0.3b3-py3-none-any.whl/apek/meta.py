5ba83640e0b0c63a513c8640669860f718f173fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:10:28 +0000

    Add package apek/0.0.3b3/apek-0.0.3b3-py3-none-any.whl

---
+++ packages/apek/apek-0.0.3b3-py3-none-any.whl/apek/meta.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+This module defines the package's metadata.
+
+Constants:
+    cst_version (str): The package version number.
+"""
+
+
+
+from ._text import text
+
+const_version = "0.0.3b3"
