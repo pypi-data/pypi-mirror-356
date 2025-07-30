638979fee9438872821ede680b3ed47a3ab0a1e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:37 +0000

    Add package assetutilities/0.0.7/assetutilities-0.0.7-py2.py3-none-any.whl

---
+++ packages/assetutilities/assetutilities-0.0.7-py2.py3-none-any.whl/assetutilities/modules/zip_utilities/zip_files_to_dataframe.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import zipfile
+import os
+from typing import Dict, List, Optional, Union
+from loguru import logger
+
+import io
+from io import BytesIO
+from assetutilities.modules.csv_utilities.csv_utilities import CSVUtilities
+
+csv_utilities = CSVUtilities()
+
+class ZipFilestoDf:
+
+    def __init__(self):
+        pass
+
+    def router(self,cfg):
+        pass
+    
+    def zip_file_to_dataframe(self, cfg) -> Dict[str, pd.DataFrame]:
+
+        """ 
+        Extracts files from a ZIP file/archive and returns Pandas DataFrame(s) as dictionary.
+        """
+
+        column_names = cfg['zip_utilities']['column_names']
+        zip_file_name_with_path = cfg['zip_utilities']['file_name']
+        rows = 50000 # to limit file size 
+
+        with open(zip_file_name_with_path, "rb") as f:
+            zip_bytes = f.read()
+
+        zip_file = zip_bytes
+
+        if isinstance(zip_file, bytes):
+            zip_file = BytesIO(zip_file)
+
+        with zipfile.ZipFile(zip_file, 'r') as zf:
+            files_in_zip = zf.namelist()
+            file_list = [name for name in files_in_zip if not name.endswith('/')]
+
+            # check if file_list is empty
+            if not file_list:
+                raise ValueError(f"The ZIP file '{zip_file_name_with_path}' is empty.")
+
+            dataframe_dict = {}
+            delimiter = ','
+            dataframe_dict = self.parse_zip_files(column_names, rows, zf, file_list, dataframe_dict, delimiter)
+
+        return dataframe_dict
+
+    def parse_zip_files(self, column_names, nrows, zf, file_list, dataframe_dict, delimiter):
+
+        for file_to_read in file_list:
+            try:
+                with zf.open(file_to_read) as file:
+                    df = csv_utilities.read_zip_file_with_latin1(file, delimiter=delimiter, nrows=nrows)
+                    
+                # If column names are provided and the df has no header set the column names
+                if column_names:
+                    df.columns = column_names
+
+                file_basename = os.path.basename(file_to_read)
+                file_name_without_extension, extension = os.path.splitext(file_basename)
+                dataframe_dict[file_name_without_extension] = df
+
+            except Exception as e:
+                logger.error(f"Error opening/processing file '{file_to_read}': {e}")
+                continue
+
+        return dataframe_dict
+
