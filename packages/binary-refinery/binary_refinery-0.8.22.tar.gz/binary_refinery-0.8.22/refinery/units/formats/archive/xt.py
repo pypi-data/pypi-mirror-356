145260f8febd49ecdb8a34ad5c9cdca59bbdd0b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:51 +0000

    Add package binary-refinery/0.8.22/binary_refinery-0.8.22.tar.gz

---
+++ packages/binary-refinery/binary_refinery-0.8.22.tar.gz/binary_refinery-0.8.22/refinery/units/formats/archive/xt.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+from typing import List, Optional, Type
+
+from refinery.units.formats.archive import ArchiveUnit, MultipleArchives
+from refinery.units import RefineryException
+
+
+class xt(ArchiveUnit, docs='{0}{p}{PathExtractorUnit}'):
+    """
+    This unit generically extracts files from archives. It attempts to identify the archive format
+    and use the corresponding specific extractor from among the ones implemented in refinery.
+    """
+    @classmethod
+    def handles(cls, data: bytearray) -> Optional[bool]:
+        out = False
+        for engine in cls.handlers():
+            engine_verdict = engine.handles(data)
+            if engine_verdict is True:
+                return True
+            if engine_verdict is None:
+                out = None
+        return out
+
+    @staticmethod
+    def handlers():
+        """
+        Returns all archive handlers supported by the unit.
+        """
+        from refinery.units.formats.archive.xtsim import xtsim
+        yield xtsim
+        from refinery.units.formats.archive.xtinno import xtinno
+        yield xtinno
+        from refinery.units.formats.winreg import winreg
+        yield winreg
+        from refinery.units.formats.office.xtone import xtone
+        yield xtone
+        from refinery.units.formats.archive.xtgz import xtgz
+        yield xtgz
+        from refinery.units.formats.email import xtmail
+        yield xtmail
+        from refinery.units.formats.pdf import xtpdf
+        yield xtpdf
+        from refinery.units.formats.archive.xtasar import xtasar
+        yield xtasar
+        from refinery.units.formats.office.xtrtf import xtrtf
+        yield xtrtf
+        from refinery.units.formats.archive.xtzpaq import xtzpaq
+        yield xtzpaq
+        from refinery.units.formats.pe.dotnet.dnsfx import dnsfx
+        yield dnsfx
+        from refinery.units.formats.archive.xtiso import xtiso
+        yield xtiso
+        from refinery.units.formats.archive.xtnsis import xtnsis
+        yield xtnsis
+        from refinery.units.formats.archive.xtnode import xtnode
+        yield xtnode
+        from refinery.units.formats.archive.xtace import xtace
+        yield xtace
+        from refinery.units.formats.archive.xtcab import xtcab
+        yield xtcab
+        from refinery.units.formats.archive.xtcpio import xtcpio
+        yield xtcpio
+        from refinery.units.formats.archive.xtpyi import xtpyi
+        yield xtpyi
+        from refinery.units.formats.archive.xttar import xttar
+        yield xttar
+        from refinery.units.formats.archive.xtiss import xtiss
+        yield xtiss
+        from refinery.units.formats.office.xtdoc import xtdoc
+        yield xtdoc
+        from refinery.units.formats.archive.xtzip import xtzip
+        yield xtzip
+        from refinery.units.formats.archive.xt7z import xt7z
+        yield xt7z
+        from refinery.units.formats.msi import xtmsi
+        yield xtmsi
+        from refinery.units.formats.archive.xtmacho import xtmacho
+        yield xtmacho
+        from refinery.units.formats.archive.xtnuitka import xtnuitka
+        yield xtnuitka
+        from refinery.units.formats.json import xtjson
+        yield xtjson
+        from refinery.units.formats.exe.vsect import vsect
+        yield vsect
+
+    def unpack(self, data):
+        fallback: List[Type[ArchiveUnit]] = []
+        errors = {}
+        pos_args = self.args.paths
+        key_args = dict(
+            list=self.args.list,
+            path=self.args.path,
+            date=self.args.date,
+            join_path=self.args.join,
+            drop_path=self.args.drop,
+        )
+        if self.args.pwd:
+            key_args.update(pwd=self.args.pwd)
+        if self.args.regex:
+            key_args.update(regex=self.args.regex)
+
+        class unpacker:
+            unit = self
+
+            def __init__(self, handler: Type[ArchiveUnit], fallback: bool):
+                self.success = False
+                self.handler = handler
+                self.fallback = fallback
+
+            def __iter__(self):
+                handler = self.handler
+                if self.fallback:
+                    verdict = True
+                else:
+                    verdict = handler.handles(data)
+                if verdict is False:
+                    self.unit.log_info(F'rejected: {handler.name}')
+                elif verdict is True:
+                    if not self.fallback:
+                        self.unit.log_info(F'accepted: {handler.name}')
+                    try:
+                        unit = handler(*pos_args, **key_args)
+                        unit.args.lenient = self.unit.args.lenient
+                        unit.args.quiet = self.unit.args.quiet
+                    except TypeError as error:
+                        self.unit.log_debug('handler construction failed:', error)
+                        return
+                    try:
+                        test_unpack = not self.unit.args.list
+                        for item in unit.unpack(data):
+                            if test_unpack:
+                                item.get_data()
+                                test_unpack = False
+                            yield item
+                    except Exception as error:
+                        if not self.fallback:
+                            errors[handler.name] = error
+                        if isinstance(error, MultipleArchives):
+                            self.unit.log_warn(error)
+                        else:
+                            self.unit.log_debug('handler unpacking failed:', error)
+                    else:
+                        self.success = True
+                elif verdict is None:
+                    fallback.append(handler)
+
+        for handler in self.handlers():
+            self.CustomPathSeparator = handler.CustomPathSeparator
+            it = unpacker(handler, fallback=False)
+            yield from it
+            if it.success:
+                return
+
+        self.log_debug('fallback order:', lambda: ', '.join(h.name for h in fallback))
+
+        for handler in fallback:
+            it = unpacker(handler, fallback=True)
+            yield from it
+            if it.success:
+                return
+
+        if not errors:
+            raise ValueError('input data did not match any known archive format')
+        for name, error in errors.items():
+            self.log_info(F'error when trying to unpack with {name}:', error)
+        raise RefineryException('none of the available unpackers could handle this data')
---
+++ packages/binary-refinery/binary_refinery-0.8.22.tar.gz/binary_refinery-0.8.22/refinery/units/misc/autoxor.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import re
+
+from refinery.units.misc.xkey import xkey
+from refinery.lib.mime import FileMagicInfo as magic
+from refinery.lib.loader import get_entry_point
+
+
+class autoxor(xkey, docs='{0}{p}{1}'):
+    """
+    Assumes input that was encrypted with a polyalphabetic block cipher, like XOR-ing each byte
+    with successive bytes from a key or by subtracting the respective key byte value from each
+    input byte. It uses the `refinery.xkey` unit to attack the cipher and attempts to recover the
+    plaintext automatically.
+    """
+    def process(self, data: bytearray):
+        for result in self._attack(data):
+            key = result.key
+            names = []
+            if result.xor is not False:
+                names.append('xor')
+            if result.xor is not True:
+                names.append('sub')
+            break
+        else:
+            self.log_warn('No key was found; returning original data.')
+            return data
+
+        fallback = None
+
+        for name in names:
+
+            unit = get_entry_point(name)
+            bin, = data | unit(key)
+            space = B'\0' | unit(0x20) | bytes
+
+            if not (m := magic(bin)).blob:
+                self.log_info(F'method {name} resulted in {m.mime} data; returning buffer')
+                return self.labelled(bin, key=key, method=name)
+            if fallback is None:
+                fallback = name, key, bin, m.blob
+            if not any(bin):
+                continue
+
+            as_text = bin | unit(space) | bytearray
+
+            try:
+                decoded = as_text.decode('utf8')
+            except UnicodeDecodeError:
+                is_text = False
+            else:
+                is_text = bool(re.fullmatch(r'[\s\w!-~]+', decoded))
+
+            if is_text:
+                self.log_info('detected likely text input; automatically shifting towards space character')
+                key = (b'\x20' * len(key)) | unit(key) | bytes
+                return self.labelled(as_text, key=key, method=name)
+
+        if fallback:
+            name, key, bin, is_blob = fallback
+            if is_blob:
+                self.log_warn('unrecognized format and no confirmed crib; the output is likely junk')
+            return self.labelled(bin, key=key)
