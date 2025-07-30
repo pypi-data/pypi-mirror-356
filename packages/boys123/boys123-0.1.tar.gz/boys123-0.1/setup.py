b924df74573ef51d83116d40b56ba383e25aba85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:57 +0000

    Add package boys123/0.1/boys123-0.1.tar.gz

---
+++ packages/boys123/boys123-0.1.tar.gz/boys123-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="boys123",    
+    version="0.1",
+    packages=["boys123"],   # this must match your folder name
+    description="A simple demo package",
+    author="krishna",
+    author_email="krish@email.com",
+    python_requires=">=3.6",
+)
