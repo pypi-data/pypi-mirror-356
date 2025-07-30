a55bee64861d8ff727031f65b40c05543f5c8dbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:13 +0000

    Add package bvwx/0.12.2/bvwx-0.12.2-py3-none-any.whl

---
+++ packages/bvwx/bvwx-0.12.2-py3-none-any.whl/bvwx/_util.py
@@ -0,0 +1,36 @@
+"""Utility functions."""
+
+from math import ceil, log2
+
+
+def clog2(x: int) -> int:
+    """Return the ceiling log base two of an integer ≥ 1.
+
+    Tells you the minimum dimension of a Boolean space with at least N points.
+
+    For example, here are the values of ``clog2(N)`` for :math:`1 ≤ N < 18`:
+
+    >>> [clog2(n) for n in range(1, 18)]
+    [0, 1, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 5]
+
+    Defined only for positive integers:
+
+    >>> clog2(0)
+    Traceback (most recent call last):
+        ...
+    ValueError: math domain error
+    """
+    return ceil(log2(x))
+
+
+def mask(n: int) -> int:
+    """Return n bit mask."""
+    return (1 << n) - 1
+
+
+class classproperty:
+    def __init__(self, func):
+        self._f = func
+
+    def __get__(self, unused_obj, cls):
+        return self._f(cls)
