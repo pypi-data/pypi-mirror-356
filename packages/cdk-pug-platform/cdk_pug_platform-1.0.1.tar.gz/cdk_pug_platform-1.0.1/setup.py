e06bec52ff5cc13e0122135c0cb02006f062286b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:20 +0000

    Add package cdk-pug-platform/1.0.1/cdk_pug_platform-1.0.1.tar.gz

---
+++ packages/cdk-pug-platform/cdk_pug_platform-1.0.1.tar.gz/cdk_pug_platform-1.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cdk_pug_platform",
+    version="1.0.1",
+    author="CESAR MORALES",
+    author_email="python-moralescorp@cesarmoralesonya.es",
+    description="AWS CDK application for the PUG Platform",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/morales-corp/cdk-pug-platform",
+    packages=find_packages(),
+    install_requires=[
+        "aws-cdk-lib",
+        "constructs"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
