5e7cfad35be9a0ec5be871f9ebdc9b76709f31b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:13:37 +0000

    Add package plonemeeting.portal.core/2.1.10/plonemeeting_portal_core-2.1.10.tar.gz

---
+++ packages/plonemeeting.portal.core/plonemeeting_portal_core-2.1.10.tar.gz/plonemeeting_portal_core-2.1.10/src/plonemeeting/portal/core/adapters.py
@@ -0,0 +1,46 @@
+from collective.timestamp.adapters import ITimeStamper
+from collective.timestamp.adapters import TimeStamper
+from imio.helpers.content import object_values
+from io import BytesIO
+from plone.namedfile.file import NamedBlobFile
+from zope.interface import implementer
+
+import zipfile
+
+
+@implementer(ITimeStamper)
+class PublicationTimeStamper(TimeStamper):
+    """Handle timestamping operations on publications"""
+
+    def get_data(self):
+        zip_buffer = BytesIO()
+        with zipfile.ZipFile(zip_buffer, "w", zipfile.ZIP_DEFLATED) as zip_file:
+            # add main file
+            if self.context.file:
+                zip_file.writestr(self.context.file.filename, self.context.file.data)
+            # add annexes
+            for item in object_values(self.context, "File"):
+                zip_file.writestr(item.file.filename, item.file.data)
+            # add text
+            if self.context.text:
+                zip_file.writestr("text.txt", self.context.text.raw)
+        zip_buffer.seek(0)
+        return zip_buffer.getvalue()
+
+    def file_has_changed(self, obj, event):
+        return obj.timestamped_file.data != self.get_data()
+
+    def _effective_related_indexes(self):
+        idxs = super(PublicationTimeStamper, self)._effective_related_indexes()
+        # "effective", "effectiveRange", "is_timestamped"
+        # already managed by timestamper.timestamp
+        idxs.append("year")
+        return idxs
+
+    def timestamp(self):
+        data, timestamp_date = super(PublicationTimeStamper, self).timestamp()
+        formatted_date = (
+            timestamp_date.astimezone().strftime("%Y%m%d-%H%M%S")
+        )
+        filename = f"archive_{formatted_date}.zip"
+        self.context.timestamped_file = NamedBlobFile(data=data, filename=filename)
---
+++ packages/plonemeeting.portal.core/plonemeeting_portal_core-2.1.10.tar.gz/plonemeeting_portal_core-2.1.10/src/plonemeeting/portal/core/browser/publication.py
@@ -0,0 +1,104 @@
+from Products.CMFCore.permissions import ModifyPortalContent
+from Products.CMFCore.utils import _checkPermission, getToolByName
+from Products.statusmessages.interfaces import IStatusMessage
+from imio.helpers.workflow import get_state_infos
+from imio.pyutils.utils import sort_by_indexes
+from plone import api
+from plone.app.content.browser.content_status_modify import ContentStatusModifyView
+from plone.dexterity.browser.add import DefaultAddForm
+from plone.dexterity.browser.add import DefaultAddView
+from plone.dexterity.browser.edit import DefaultEditForm
+from plone.dexterity.browser.view import DefaultView
+from plone.dexterity.events import EditFinishedEvent
+from plonemeeting.portal.core import _
+from z3c.form import button
+from zope.event import notify
+
+FIELDSETS_ORDER = ["authority", "dates", "timestamp", "categorization", "settings"]
+ADMIN_FIELDSETS = ["settings"]
+
+
+class AddForm(DefaultAddForm):
+    """Override to reorder and filter out fieldsets."""
+
+    def updateFields(self):
+        super(AddForm, self).updateFields()
+        indexes = [FIELDSETS_ORDER.index(group.__name__) for group in self.groups]
+        groups = sort_by_indexes(self.groups, indexes)
+        pm = getToolByName(self.context, "portal_membership")
+        if not pm.checkPermission("Manage portal", self.context):
+            groups = filter(lambda g: g.__name__ not in ADMIN_FIELDSETS, groups)
+        self.groups = groups
+
+
+class PublicationAdd(DefaultAddView):
+
+    form = AddForm
+
+
+class EditForm(DefaultEditForm):
+    """Override to reorder and filter out fieldsets."""
+
+    @button.buttonAndHandler(_("Save"), name="save")
+    def handleApply(self, action):
+        if api.content.get_state(self.context) == "planned" and not data.get("effective_date"):
+            IStatusMessage(self.request).addStatusMessage(_("msg_missing_effective_date"), "error")
+            return
+        super(EditForm, self).handleApply(action)
+
+    def updateFields(self):
+        super(EditForm, self).updateFields()
+        indexes = [FIELDSETS_ORDER.index(group.__name__) for group in self.groups]
+        groups = sort_by_indexes(self.groups, indexes)
+        pm = getToolByName(self.context, "portal_membership")
+        if not pm.checkPermission("Manage portal", self.context):
+            groups = filter(lambda g: g.__name__ not in ADMIN_FIELDSETS, groups)
+        self.groups = groups
+
+
+class PublicationView(DefaultView):
+    """ """
+
+    def __call__(self):
+        if api.content.get_state(self.context) == "private" and _checkPermission(ModifyPortalContent, self.context):
+            if self.context.enable_timestamping is False:
+                api.portal.show_message(
+                    _("Timestamping is disabled for this element!"), request=self.request, type="warning"
+                )
+            if (
+                self.context.effective_date
+                and self.context.effective_date.isPast()
+                and self.context.enable_timestamping
+            ):
+                api.portal.show_message(_("effective_date_in_past_msg"), request=self.request, type="warning")
+        return super(PublicationView, self).__call__()
+
+    def get_effective_date(self):
+        return self.context.effective_date.strftime("%d/%m/%Y à %H:%M") if self.context.effective_date else "-"
+
+    def get_expiration_date(self):
+        return self.context.expiration_date.strftime("%d/%m/%Y à %H:%M") if self.context.expiration_date else "-"
+
+    def get_decision_date(self):
+        return self.context.decision_date.strftime("%d/%m/%Y") if self.context.decision_date else "-"
+
+    def get_authority_date(self):
+        return self.context.authority_date.strftime("%d/%m/%Y") if self.context.authority_date else "-"
+
+    def get_expired_authority_date(self):
+        return self.context.expired_authority_date.strftime("%d/%m/%Y") if self.context.expired_authority_date else "-"
+
+    def get_entry_date(self):
+        return self.context.entry_date.strftime("%d/%m/%Y") if self.context.entry_date else "-"
+
+    def get_state_title(self):
+        return get_state_infos(self.context)["state_title"]
+
+
+class PublicationContentStatusModifyView(ContentStatusModifyView):
+    """Override to not set a publication date automatically."""
+
+    def editContent(self, obj, effective, expiry):
+        """Override to bypass setting effective_date and
+        expiration_date upon any workflow transition."""
+        return
