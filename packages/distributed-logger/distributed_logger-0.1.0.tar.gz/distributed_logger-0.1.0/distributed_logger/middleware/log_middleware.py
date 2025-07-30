f1a11b9377bf1587d5910bdefabf2683b35d62bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:54 +0000

    Add package distributed-logger/0.1.0/distributed_logger-0.1.0.tar.gz

---
+++ packages/distributed-logger/distributed_logger-0.1.0.tar.gz/distributed_logger-0.1.0/distributed_logger/middleware/log_middleware.py
@@ -0,0 +1,53 @@
+import time
+from distributed_logger.models.log import LogInfo
+import os
+from distributed_logger.loggers.factory import LoggerFactory, EnvType
+from distributed_logger.models.config import ConfigFactory
+
+
+class AuditLogMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def _get_logger(self):
+        broker_type = os.environ.get("BROKER_TYPE", "SIMPLE").upper()
+        if broker_type == "KAFKA":
+            env_type = EnvType.KAFKA
+        else:
+            env_type = EnvType.SIMPLE
+
+        config = ConfigFactory.create_config(
+            config_type=broker_type,
+
+            # Kafka settings
+            bootstrap_servers=os.getenv("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092"),
+            topic=os.getenv("KAFKA_TOPIC", "audit_logs"),
+            client_id=os.getenv("KAFKA_CLIENT_ID", "audit_logger"),
+            host=os.getenv("RABBITMQ_HOST", "localhost"),
+
+        )
+        return LoggerFactory(env_type, config).get_logger()
+
+    def __call__(self, request):
+        start_time = time.time()
+
+        # Process the request
+        response = self.get_response(request)
+
+        try:
+
+            # Get the logger instance
+            self.logger = self._get_logger()
+            
+            log_info = LogInfo(
+                ip_address=request.META.get('REMOTE_ADDR',''),
+                user_id=request.user.id if request.user.is_authenticated else None,
+                request_time=time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(start_time)),
+                action=request.path,
+                request_data=request.POST.dict() if request.method == 'POST' else request.GET.dict()
+            )
+            self.logger.publish(log_info)
+
+        except Exception as e:
+            print(f"Error logging request: {e}")
+        return response
---
+++ packages/distributed-logger/distributed_logger-0.1.0.tar.gz/distributed_logger-0.1.0/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="distributed-logger",
+    version="0.1.0",
+    author="Arjun Navya",
+    author_email="arjun.dahal@navyaadvisors.com",
+    description="A lightweight logging system with Kafka support for auditing and debugging",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/arjun-navya/distributed-logger",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: System :: Logging",
+    ],
+    python_requires=">=3.9",
+    install_requires=[
+        "kafka-python>=2.0.2",
+        "python-json-logger>=2.0.0",
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=6.0",
+            "pytest-cov>=2.0",
+            "black>=21.0",
+            "flake8>=3.8",
+            "mypy>=0.800",
+        ],
+    },
+) 
