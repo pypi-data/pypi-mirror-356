685f632a7090aa29a001abf6f5cef5653309cdc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:08:42 +0000

    Add package sidpy-lite/0.12.9/sidpy_lite-0.12.9-py2.py3-none-any.whl

---
+++ packages/sidpy-lite/sidpy_lite-0.12.9-py2.py3-none-any.whl/sidpy/__init__.py
@@ -0,0 +1,27 @@
+"""
+The sidpy package
+"""
+from .__version__ import version as __version__
+from . import base, hdf, io, sid, viz
+from .base import *
+from .hdf import *
+from .io import *
+from .sid import *
+from .viz import plot_utils
+from .viz import jupyter_utils
+
+__all__ = ['__version__']
+# Traditional hierarchical approach - importing submodules
+__all__ += base.__all__
+__all__ += hdf.__all__
+__all__ += io.__all__
+__all__ += sid.__all__
+__all__ += viz.__all__
+
+# Making things easier by surfacing all low-level modules directly:
+__all__ += ['dict_utils', 'num_utils', 'string_utils']
+__all__ += ['hdf_utils', 'reg_ref', 'dtype_utils', 'prov_utils']
+__all__ += ['interface_utils']
+__all__ += ['FileWidget', 'ChooseDataset']
+__all__ += ['Dimension', 'Translator', 'Dataset', 'Reader']
+__all__ += ['plot_utils', 'jupyter_utils']
