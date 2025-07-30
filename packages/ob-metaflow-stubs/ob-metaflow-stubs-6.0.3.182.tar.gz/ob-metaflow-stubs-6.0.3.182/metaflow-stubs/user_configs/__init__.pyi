6c63be59affca2198863ed3d88c981df6b5e2996
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:36 +0000

    Add package ob-metaflow-stubs/6.0.3.182/ob-metaflow-stubs-6.0.3.182.tar.gz

---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-6.0.3.182.tar.gz/ob-metaflow-stubs-6.0.3.182/metaflow-stubs/user_configs/__init__.pyi
@@ -0,0 +1,13 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.18.1+obcheckpoint(0.2.1);ob(v1)                                                   #
+# Generated on 2025-06-19T23:04:39.535594                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from . import config_parameters as config_parameters
+from . import config_decorators as config_decorators
+from . import config_options as config_options
+
