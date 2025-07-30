3637c21855cf88888f42094382301ead22e4c029
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:11:09 +0000

    Add package mypackage-demo-tutorial-2024/0.1.0/mypackage_demo_tutorial_2024-0.1.0-py3-none-any.whl

---
+++ packages/mypackage-demo-tutorial-2024/mypackage_demo_tutorial_2024-0.1.0-py3-none-any.whl/mypackage/utils.py
@@ -0,0 +1,17 @@
+def add_one(number):
+    """
+    接收一个数字并返回加一后的结果
+    
+    Args:
+        number: 输入的数字 (int 或 float)
+        
+    Returns:
+        输入数字加1后的结果
+        
+    Examples:
+        >>> add_one(5)
+        6
+        >>> add_one(2.5)
+        3.5
+    """
+    return number + 1 
