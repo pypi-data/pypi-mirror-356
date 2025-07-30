fe16ab620d6d1730a93bef4197571a8750db248c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:42 +0000

    Add package jwtninja/0.1.5/jwtninja-0.1.5-py3-none-any.whl

---
+++ packages/jwtninja/jwtninja-0.1.5-py3-none-any.whl/jwt_ninja/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+
+from .models import Session
+
+
+@admin.register(Session)
+class SessionAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "user",
+        "created_at",
+        "updated_at",
+        "expired_at",
+        "ip_address",
+    ]
+    readonly_fields = [
+        "id",
+        "user",
+        "created_at",
+        "updated_at",
+        "expired_at",
+        "ip_address",
+        "data",
+    ]
+    list_filter = ["created_at", "expired_at"]
+    ordering = ["-id"]
