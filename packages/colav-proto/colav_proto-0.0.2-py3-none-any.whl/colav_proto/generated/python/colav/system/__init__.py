7a6849594e219b094c07698586c501934561f6ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:21:24 +0000

    Add package colav-proto/0.0.2/colav_proto-0.0.2-py3-none-any.whl

---
+++ packages/colav-proto/colav_proto-0.0.2-py3-none-any.whl/colav_proto/generated/python/colav/system/__init__.py
@@ -0,0 +1,9 @@
+from .map_metadata_pb2 import MapMetaData
+from .mission_request_pb2 import MissionRequest
+from .mission_response_pb2 import MissionResponse
+
+__all__ = [
+    "MapMetaData",
+    "MissionRequest",
+    "MissionResponse"
+]
