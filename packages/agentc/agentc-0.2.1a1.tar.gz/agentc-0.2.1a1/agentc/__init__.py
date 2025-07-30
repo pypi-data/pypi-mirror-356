e0d760de3e38cccbe373b99f87682cf1fdbf15d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:37:43 +0000

    Add package agentc/0.2.1a1/agentc-0.2.1a1.tar.gz

---
+++ packages/agentc/agentc-0.2.1a1.tar.gz/agentc-0.2.1a1/agentc/__init__.py
@@ -0,0 +1,9 @@
+from . import catalog
+from . import span
+from agentc_core.activity.span import Span
+from agentc_core.catalog.catalog import Catalog
+
+__all__ = ["catalog", "Catalog", "span", "Span"]
+
+# DO NOT edit this value, the plugin "poetry-dynamic-versioning" will automatically set this.
+__version__ = "0.0.0"
---
+++ packages/agentc/agentc-0.2.1a1.tar.gz/agentc-0.2.1a1/agentc/catalog/__init__.py
@@ -0,0 +1,6 @@
+from agentc_core.catalog.catalog import Catalog
+from agentc_core.catalog.catalog import Prompt
+from agentc_core.catalog.catalog import Tool
+from agentc_core.tool.decorator import tool
+
+__all__ = ["Catalog", "Prompt", "Tool", "tool"]
