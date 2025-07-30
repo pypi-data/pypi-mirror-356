186258ff67c4c672cd322f4fae2e1268781530e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:17:32 +0000

    Add package WeiboBot/1.2.3/weibobot-1.2.3-py3-none-any.whl

---
+++ packages/WeiboBot/weibobot-1.2.3-py3-none-any.whl/WeiboBot/exception/__init__.py
@@ -0,0 +1,34 @@
+class WeiboNotExist(Exception):
+    pass
+
+
+class PostWeiboError(Exception):
+    pass
+
+
+class CommentError(Exception):
+    pass
+
+
+class DeleteCommentError(Exception):
+    pass
+
+
+class UploadPicError(Exception):
+    pass
+
+
+class DeleteWeiboError(Exception):
+    pass
+
+
+class SendMessageError(Exception):
+    pass
+
+
+class LikeWeiboError(Exception):
+    pass
+
+
+class RepostWeiboError(Exception):
+    pass
