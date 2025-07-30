919a8555821f46c494fed5c40eccaeb2a73eb7d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:42 +0000

    Add package bioio/2.0.0/bioio-2.0.0.tar.gz

---
+++ packages/bioio/bioio-2.0.0.tar.gz/bioio-2.0.0/bioio/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Top-level package for bioio."""
+
+from importlib.metadata import PackageNotFoundError, version
+
+from .bio_image import BioImage
+from .plugins import plugin_feasibility_report
+
+try:
+    __version__ = version("bioio")
+except PackageNotFoundError:
+    __version__ = "uninstalled"
+
+__author__ = "Eva Maxfield Brown, Dan Toloudis, BioIO Contributors"
+__email__ = "evamaxfieldbrown@gmail.com, danielt@alleninstitute.org"
+
+__all__ = [
+    "BioImage",
+    "plugin_feasibility_report",
+]
