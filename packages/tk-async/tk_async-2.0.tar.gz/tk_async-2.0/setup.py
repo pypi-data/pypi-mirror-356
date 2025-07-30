fb7e30c328bf013128b816067c2774654d351ce4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:45 +0000

    Add package tk-async/2.0/tk_async-2.0.tar.gz

---
+++ packages/tk-async/tk_async-2.0.tar.gz/tk_async-2.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tk-async",
+    version="2.0",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+)
