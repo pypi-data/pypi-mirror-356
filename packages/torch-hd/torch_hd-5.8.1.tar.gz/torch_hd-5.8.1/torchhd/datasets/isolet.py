871599ffa8bdb297c7a19e7b421cd57f74664cfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:09:05 +0000

    Add package torch-hd/5.8.1/torch_hd-5.8.1.tar.gz

---
+++ packages/torch-hd/torch_hd-5.8.1.tar.gz/torch_hd-5.8.1/torchhd/datasets/isolet.py
@@ -0,0 +1,179 @@
+#
+# MIT License
+#
+# Copyright (c) 2023 Mike Heddes, Igor Nunes, Pere Vergés, Denis Kleyko, and Danny Abraham
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+import os
+import os.path
+from typing import Callable, Optional, Tuple, List
+import torch
+from torch.utils import data
+import pandas as pd
+
+from .utils import download_file_from_google_drive, unzip_file
+
+
+class ISOLET(data.Dataset):
+    """`ISOLET <https://archive.ics.uci.edu/ml/datasets/isolet>`_ dataset.
+
+    .. list-table::
+       :widths: 10 10 10 10
+       :align: center
+       :header-rows: 1
+
+       * - Instances
+         - Attributes
+         - Task
+         - Area
+       * - 7797
+         - 617
+         - Classification
+         - Computer
+
+
+    Args:
+        root (string): Root directory of dataset where ``isolet1+2+3+4.data``
+            and  ``isolet5.data`` exist.
+        train (bool, optional): If True, creates dataset from ``isolet1+2+3+4.data``,
+            otherwise from ``isolet5.data``.
+        download (bool, optional): If True, downloads the dataset from the internet and
+            puts it in root directory. If dataset is already downloaded, it is not
+            downloaded again.
+        transform (callable, optional): A function/transform that takes in an torch.FloatTensor
+            and returns a transformed version.
+        target_transform (callable, optional): A function/transform that takes in the
+            target and transforms it.
+
+    """
+
+    classes: List[str] = [
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "I",
+        "J",
+        "K",
+        "L",
+        "M",
+        "N",
+        "O",
+        "P",
+        "Q",
+        "R",
+        "S",
+        "T",
+        "U",
+        "V",
+        "W",
+        "X",
+        "Y",
+        "Z",
+    ]
+
+    def __init__(
+        self,
+        root: str,
+        train: bool = True,
+        transform: Optional[Callable] = None,
+        target_transform: Optional[Callable] = None,
+        download: bool = False,
+    ):
+        root = os.path.join(root, "isolet")
+        root = os.path.expanduser(root)
+        self.root = root
+        os.makedirs(self.root, exist_ok=True)
+
+        self.train = train
+        self.transform = transform
+        self.target_transform = target_transform
+
+        if download:
+            self.download()
+
+        if not self._check_integrity():
+            raise RuntimeError(
+                "Dataset not found or corrupted. You can use download=True to download it"
+            )
+
+        self._load_data()
+
+    def __len__(self) -> int:
+        return self.data.size(0)
+
+    def __getitem__(self, index: int) -> Tuple[torch.FloatTensor, torch.LongTensor]:
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            Tuple[torch.FloatTensor, torch.LongTensor]: (sample, target) where target is the index of the target class
+        """
+        sample = self.data[index]
+        label = self.targets[index]
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        if self.target_transform:
+            label = self.target_transform(label)
+
+        return sample, label
+
+    def _check_integrity(self) -> bool:
+        if not os.path.isdir(self.root):
+            return False
+
+        # Check if the root directory contains the required files
+        has_train_file = os.path.isfile(os.path.join(self.root, "isolet1+2+3+4.data"))
+        has_test_file = os.path.isfile(os.path.join(self.root, "isolet5.data"))
+        if has_train_file and has_test_file:
+            return True
+
+        # TODO: Add more specific checks like an MD5 checksum
+
+        return False
+
+    def _load_data(self):
+        data_file = "isolet1+2+3+4.data" if self.train else "isolet5.data"
+        data = pd.read_csv(os.path.join(self.root, data_file), header=None)
+        self.data = torch.tensor(data.values[:, :-1], dtype=torch.float)
+        self.targets = torch.tensor(data.values[:, -1], dtype=torch.long) - 1
+
+    def download(self):
+        """Download the data if it doesn't exist already."""
+
+        if self._check_integrity():
+            print("Files already downloaded and verified")
+            return
+
+        zip_file_path = os.path.join(self.root, "data.zip")
+        download_file_from_google_drive(
+            "1IMC6xzs2kBnf5_kaiBUzSWiTMR_dFbIX",  # Google Drive shared file ID
+            zip_file_path,
+        )
+
+        unzip_file(zip_file_path, self.root)
+        os.remove(zip_file_path)
---
+++ packages/torch-hd/torch_hd-5.8.1.tar.gz/torch_hd-5.8.1/torchhd/tests/structures/test_multiset.py
@@ -0,0 +1,239 @@
+#
+# MIT License
+#
+# Copyright (c) 2023 Mike Heddes, Igor Nunes, Pere Vergés, Denis Kleyko, and Danny Abraham
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+import pytest
+import torch
+import string
+
+from torchhd import structures, functional
+from torchhd import MAPTensor
+
+seed = 2147483644
+letters = list(string.ascii_lowercase)
+
+
+class TestMultiset:
+    def test_creation_dim(self):
+        M = structures.Multiset(10000)
+        assert torch.allclose(M.value, torch.zeros(10000))
+
+    def test_creation_tensor(self):
+        generator = torch.Generator()
+        generator.manual_seed(seed)
+        keys_hv = functional.random(len(letters), 10000, generator=generator)
+        multiset = functional.multiset(keys_hv)
+
+        M = structures.Multiset(multiset)
+        assert torch.allclose(M.value, multiset)
+
+    def test_generator(self):
+        generator = torch.Generator()
+        generator.manual_seed(seed)
+        hv1 = functional.random(60, 10000, generator=generator)
+
+        generator = torch.Generator()
+        generator.manual_seed(seed)
+        hv2 = functional.random(60, 10000, generator=generator)
+
+        assert (hv1 == hv2).min().item()
+
+    def test_add(self):
+        keys_hv = MAPTensor(
+            [
+                [1.0, -1.0, 1.0, 1.0, 1.0],
+                [-1.0, -1.0, 1.0, 1.0, 1.0],
+                [1.0, 1.0, 1.0, -1.0, 1.0],
+                [1.0, 1.0, 1.0, -1.0, -1.0],
+                [-1.0, 1.0, -1.0, -1.0, 1.0],
+                [-1.0, -1.0, -1.0, -1.0, 1.0],
+                [-1.0, -1.0, -1.0, -1.0, 1.0],
+                [-1.0, -1.0, -1.0, 1.0, -1.0],
+                [1.0, -1.0, -1.0, 1.0, -1.0],
+                [1.0, 1.0, 1.0, -1.0, -1.0],
+                [-1.0, 1.0, -1.0, 1.0, -1.0],
+                [-1.0, -1.0, -1.0, -1.0, -1.0],
+                [-1.0, -1.0, 1.0, 1.0, 1.0],
+                [1.0, 1.0, -1.0, -1.0, 1.0],
+                [1.0, 1.0, 1.0, 1.0, -1.0],
+                [1.0, 1.0, -1.0, 1.0, 1.0],
+                [-1.0, -1.0, 1.0, 1.0, -1.0],
+                [-1.0, -1.0, 1.0, 1.0, 1.0],
+                [-1.0, -1.0, -1.0, -1.0, -1.0],
+                [-1.0, 1.0, -1.0, 1.0, -1.0],
+                [-1.0, 1.0, -1.0, 1.0, 1.0],
+                [1.0, 1.0, 1.0, 1.0, 1.0],
+                [-1.0, -1.0, 1.0, -1.0, 1.0],
+                [1.0, 1.0, -1.0, 1.0, 1.0],
+                [-1.0, -1.0, -1.0, -1.0, 1.0],
+                [-1.0, 1.0, -1.0, -1.0, 1.0],
+            ]
+        )
+        M = structures.Multiset(5)
+
+        M.add(keys_hv[0])
+        assert torch.allclose(M.value, MAPTensor([1.0, -1.0, 1.0, 1.0, 1.0]))
+
+        M.add(keys_hv[1])
+        assert torch.allclose(M.value, MAPTensor([0.0, -2.0, 2.0, 2.0, 2.0]))
+
+        M.add(keys_hv[2])
+        assert torch.allclose(M.value, MAPTensor([1.0, -1.0, 3.0, 1.0, 3.0]))
+
+    def test_remove(self):
+        generator = torch.Generator()
+        generator.manual_seed(seed)
+        keys_hv = functional.random(len(letters), 1000, generator=generator)
+        M = structures.Multiset(1000)
+
+        M.add(keys_hv[0])
+        M.add(keys_hv[1])
+
+        assert M.contains(keys_hv[0]) > torch.tensor([0.5])
+
+        M.remove(keys_hv[0])
+        assert M.contains(keys_hv[0]) < torch.tensor([0.1])
+        assert M.contains(keys_hv[1]) > torch.tensor([0.5])
+        assert M.remove(keys_hv[0]) is None
+
+    def test_contains(self):
+        generator = torch.Generator()
+        generator.manual_seed(seed)
+        keys_hv = functional.random(len(letters), 1000, generator=generator)
+        M = structures.Multiset(1000)
+
+        M.add(keys_hv[0])
+        M.add(keys_hv[0])
+        M.add(keys_hv[0])
+        M.add(keys_hv[1])
+        assert M.contains(keys_hv[0]) > torch.tensor([0.8])
+        M.remove(keys_hv[0])
+        assert M.contains(keys_hv[0]) > torch.tensor([0.8])
+        M.remove(keys_hv[0])
+        assert M.contains(keys_hv[0]) > torch.tensor([0.7])
+        M.remove(keys_hv[0])
+        assert M.contains(keys_hv[0]) < torch.tensor([0.1])
+        M.remove(keys_hv[1])
+        assert M.contains(keys_hv[1]) < torch.tensor([0.1])
+
+    def test_length(self):
+        generator = torch.Generator()
+        generator.manual_seed(seed)
+        keys_hv = functional.random(len(letters), 4, generator=generator)
+        M = structures.Multiset(4)
+
+        M.add(keys_hv[0])
+        M.add(keys_hv[0])
+        M.add(keys_hv[1])
+
+        assert len(M) == 3
+        M.remove(keys_hv[0])
+
+        assert len(M) == 2
+
+    def test_clear(self):
+        generator = torch.Generator()
+        generator.manual_seed(seed)
+        keys_hv = functional.random(len(letters), 4, generator=generator)
+        M = structures.Multiset(4)
+
+        M.add(keys_hv[0])
+        M.add(keys_hv[0])
+        M.add(keys_hv[1])
+
+        M.clear()
+
+        assert M.contains(keys_hv[0]) < torch.tensor([0.1])
+        assert M.contains(keys_hv[1]) < torch.tensor([0.1])
+
+        M.add(keys_hv[0])
+        assert M.contains(keys_hv[0]) > torch.tensor([0.8])
+
+    def test_from_ngrams(self):
+        keys_hv = MAPTensor(
+            [
+                [1.0, -1.0, 1.0, 1.0, 1.0],
+                [-1.0, -1.0, 1.0, 1.0, 1.0],
+                [1.0, 1.0, 1.0, -1.0, 1.0],
+                [1.0, 1.0, 1.0, -1.0, -1.0],
+                [-1.0, 1.0, -1.0, -1.0, 1.0],
+                [-1.0, -1.0, -1.0, -1.0, 1.0],
+                [-1.0, -1.0, -1.0, -1.0, 1.0],
+                [-1.0, -1.0, -1.0, 1.0, -1.0],
+                [1.0, -1.0, -1.0, 1.0, -1.0],
+                [1.0, 1.0, 1.0, -1.0, -1.0],
+                [-1.0, 1.0, -1.0, 1.0, -1.0],
+                [-1.0, -1.0, -1.0, -1.0, -1.0],
+                [-1.0, -1.0, 1.0, 1.0, 1.0],
+                [1.0, 1.0, -1.0, -1.0, 1.0],
+                [1.0, 1.0, 1.0, 1.0, -1.0],
+                [1.0, 1.0, -1.0, 1.0, 1.0],
+                [-1.0, -1.0, 1.0, 1.0, -1.0],
+                [-1.0, -1.0, 1.0, 1.0, 1.0],
+                [-1.0, -1.0, -1.0, -1.0, -1.0],
+                [-1.0, 1.0, -1.0, 1.0, -1.0],
+                [-1.0, 1.0, -1.0, 1.0, 1.0],
+                [1.0, 1.0, 1.0, 1.0, 1.0],
+                [-1.0, -1.0, 1.0, -1.0, 1.0],
+                [1.0, 1.0, -1.0, 1.0, 1.0],
+                [-1.0, -1.0, -1.0, -1.0, 1.0],
+                [-1.0, 1.0, -1.0, -1.0, 1.0],
+            ]
+        )
+
+        M = structures.Multiset.from_ngrams(keys_hv)
+        assert torch.allclose(M.value, MAPTensor([6.0, 0.0, -10.0, 8.0, -4.0]))
+
+    def test_from_tensor(self):
+        keys_hv = MAPTensor(
+            [
+                [1.0, -1.0, 1.0, 1.0, 1.0],
+                [-1.0, -1.0, 1.0, 1.0, 1.0],
+                [1.0, 1.0, 1.0, -1.0, 1.0],
+                [1.0, 1.0, 1.0, -1.0, -1.0],
+                [-1.0, 1.0, -1.0, -1.0, 1.0],
+                [-1.0, -1.0, -1.0, -1.0, 1.0],
+                [-1.0, -1.0, -1.0, -1.0, 1.0],
+                [-1.0, -1.0, -1.0, 1.0, -1.0],
+                [1.0, -1.0, -1.0, 1.0, -1.0],
+                [1.0, 1.0, 1.0, -1.0, -1.0],
+                [-1.0, 1.0, -1.0, 1.0, -1.0],
+                [-1.0, -1.0, -1.0, -1.0, -1.0],
+                [-1.0, -1.0, 1.0, 1.0, 1.0],
+                [1.0, 1.0, -1.0, -1.0, 1.0],
+                [1.0, 1.0, 1.0, 1.0, -1.0],
+                [1.0, 1.0, -1.0, 1.0, 1.0],
+                [-1.0, -1.0, 1.0, 1.0, -1.0],
+                [-1.0, -1.0, 1.0, 1.0, 1.0],
+                [-1.0, -1.0, -1.0, -1.0, -1.0],
+                [-1.0, 1.0, -1.0, 1.0, -1.0],
+                [-1.0, 1.0, -1.0, 1.0, 1.0],
+                [1.0, 1.0, 1.0, 1.0, 1.0],
+                [-1.0, -1.0, 1.0, -1.0, 1.0],
+                [1.0, 1.0, -1.0, 1.0, 1.0],
+                [-1.0, -1.0, -1.0, -1.0, 1.0],
+                [-1.0, 1.0, -1.0, -1.0, 1.0],
+            ]
+        )
+
+        M = structures.Multiset.from_tensor(keys_hv)
+        assert torch.allclose(M.value, MAPTensor([-6.0, 0.0, -4.0, 2.0, 6.0]))
