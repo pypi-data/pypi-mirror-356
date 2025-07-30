42f2889d09cb6085bd01b52e8d48c714940b8353
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:34 +0000

    Add package sharq-models/0.1.0/sharq_models-0.1.0-py3-none-any.whl

---
+++ packages/sharq-models/sharq_models-0.1.0-py3-none-any.whl/sharq_models/__init__.py
@@ -0,0 +1,22 @@
+__all__ = (
+    "Faculty",
+    "Direction",
+    "Specialization",
+    "StudyForm",
+    "Application",
+    "User",
+    "AdditionalInfo",
+    "EducationInfo",
+    "PassportData"
+)
+
+from src.models import Application
+from src.models import Faculty
+from src.models import Direction
+from src.models import Specialization
+from src.models import StudyForm
+
+from src.models import User
+from  src.models import EducationInfo
+from src.models import AdditionalInfo
+from src.models import PassportData
