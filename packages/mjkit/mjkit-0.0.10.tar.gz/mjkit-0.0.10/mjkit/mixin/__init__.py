c3d1ee36f5d531fe07431c94f5a90fea6bbe660f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:31 +0000

    Add package mjkit/0.0.10/mjkit-0.0.10.tar.gz

---
+++ packages/mjkit/mjkit-0.0.10.tar.gz/mjkit-0.0.10/mjkit/mixin/__init__.py
@@ -0,0 +1,12 @@
+"""
+자동 생성 파일입니다. 직접 수정하지 마세요.
+생성일자: 2025-06-20
+생성 위치: mixin/__init__.py
+"""
+from .logging_mixin import LoggingMixin
+from .attribute_printer_mixin import AttributePrinterMixin
+
+__all__ = [
+    "LoggingMixin",
+    "AttributePrinterMixin"
+]
