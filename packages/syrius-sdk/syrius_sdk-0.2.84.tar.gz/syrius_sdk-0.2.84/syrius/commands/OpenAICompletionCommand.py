2b6df3432d95518823d2da0b624c31f5752457f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:02 +0000

    Add package syrius-sdk/0.2.84/syrius_sdk-0.2.84.tar.gz

---
+++ packages/syrius-sdk/syrius_sdk-0.2.84.tar.gz/syrius_sdk-0.2.84/syrius/commands/OpenAICompletionCommand.py
@@ -0,0 +1,15 @@
+from typing import Any
+
+from syrius.commands.LoopInputCommand import loopType
+from syrius.commands.abstract import Command, AbstractCommand
+
+
+class OpenAICompletionCommand(Command):
+    """ """
+    id: int = 18
+    messages: list[dict[str, Any]] | AbstractCommand | loopType
+    api_key: str | AbstractCommand | loopType
+    model: str | AbstractCommand | loopType
+    temperature: float | AbstractCommand | loopType
+    tools: dict[str, Any] | AbstractCommand | loopType
+    extract: str | AbstractCommand | loopType
