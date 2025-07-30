ec7f6a303cfdee3f2cfbddf1d34eb387f93153f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:13:56 +0000

    Add package tai-jaix/0.1.1.17/tai_jaix-0.1.1.17-py3-none-any.whl

---
+++ packages/tai-jaix/tai_jaix-0.1.1.17-py3-none-any.whl/jaix/env/singular/__init__.py
@@ -0,0 +1,9 @@
+from jaix.env.singular.singular_environment import (
+    SingularEnvironment,
+)
+from jaix.env.singular.ec_env import ECEnvironmentConfig, ECEnvironment
+from jaix.env.singular.hpo_env import HPOEnvironmentConfig, HPOEnvironment
+from jaix.env.singular.mastermind_env import (
+    MastermindEnvironmentConfig,
+    MastermindEnvironment,
+)
