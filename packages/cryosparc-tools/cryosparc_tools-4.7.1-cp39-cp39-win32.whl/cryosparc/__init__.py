8afdaeae8a46b4b2e33ee4f2a138c45b6da5b2ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:14:36 +0000

    Add package cryosparc-tools/4.7.1/cryosparc_tools-4.7.1-cp39-cp39-win32.whl

---
+++ packages/cryosparc-tools/cryosparc_tools-4.7.1-cp39-cp39-win32.whl/cryosparc/__init__.py
@@ -0,0 +1,14 @@
+__version__ = "4.7.1"
+
+
+def get_include():
+    """
+    Get the include directory for the ``<cryosparc-tools/dataset.h>`` header
+    file to access CryoSPARC dataset handles from other languages.
+
+    Returns:
+        str: Include path for C compilers.
+    """
+    from pathlib import Path
+
+    return str(Path(__file__).parent / "include")
