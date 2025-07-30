a83b3704837067452765ca9fb05ae3127498f84d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:52 +0000

    Add package x4c/2025.6.14/x4c-2025.6.14.tar.gz

---
+++ packages/x4c/x4c-2025.6.14.tar.gz/x4c-2025.6.14/README.md
@@ -0,0 +1,17 @@
+# x4c: Xarray for CESM
+x4c (xarray4cesm) is an Xarray extension that aims to support efficient and intuitive CESM output analysis and visualization:
+- Analysis features: regrid, various of mean calculation, annualization/seasonalization, etc.
+- Visualization features: timeseries, horizontal and vertical spatial plots, etc.
+
+> **_Disclaimer:_**  This package is still in its early stage and under active development, and its API could be changed frequently.
+
+For more details, please refer to the documentation linked below.
+
+## Documentation
+
++ Homepage: https://NCAR.github.io/x4c
++ Installation: https://NCAR.github.io/x4c/ug-installation.html
+
+
+## License
+GPL-2.0
---
+++ packages/x4c/x4c-2025.6.14.tar.gz/x4c-2025.6.14/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='x4c',  # required
+    version='2025.6.14',
+    description='x4c: Xarray for CESM',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Feng Zhu',
+    author_email='fengzhu@ucar.edu',
+    url='https://github.com/NCAR/x4c',
+    packages=find_packages(),
+    include_package_data=True,
+    license='GPL-2.0',
+    zip_safe=False,
+    keywords='Xarray, CESM, Climate Data Analysis and Visualization',
+    classifiers=[
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+    install_requires=[
+        'netCDF4',
+        'xarray',
+        'dask',
+        'nc-time-axis',
+        'colorama',
+        'tqdm',
+        'xhistogram',
+    ],
+)
