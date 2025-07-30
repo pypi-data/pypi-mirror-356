34e7f2dcbf7bd4f645ba5f32088531e7c4b02d9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:33 +0000

    Add package tapestrysdk/0.1.14/tapestrysdk-0.1.14.tar.gz

---
+++ packages/tapestrysdk/tapestrysdk-0.1.14.tar.gz/tapestrysdk-0.1.14/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name= "tapestrysdk",
+    version="0.1.14",
+    packages=find_packages(),
+    install_requires=[
+        "openai",
+        "pymysql",
+        "python-dotenv",
+        "pycryptodome",
+    ],
+)
