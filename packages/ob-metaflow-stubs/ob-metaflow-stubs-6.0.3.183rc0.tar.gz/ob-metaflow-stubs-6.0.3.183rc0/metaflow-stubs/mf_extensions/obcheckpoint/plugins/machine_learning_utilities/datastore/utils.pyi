1137948a150c955b163b8c28a18b45ff4fd299ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:07:39 +0000

    Add package ob-metaflow-stubs/6.0.3.183rc0/ob-metaflow-stubs-6.0.3.183rc0.tar.gz

---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-6.0.3.183rc0.tar.gz/ob-metaflow-stubs-6.0.3.183rc0/metaflow-stubs/mf_extensions/obcheckpoint/plugins/machine_learning_utilities/datastore/utils.pyi
@@ -0,0 +1,19 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.18.1+obcheckpoint(0.2.1);ob(v1)                                                   #
+# Generated on 2025-06-20T20:53:54.353038                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+
+def is_json_serializable(value):
+    """
+    Check if value is JSON serializable.
+    """
+    ...
+
+def safe_serialize(data):
+    ...
+
---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-6.0.3.183rc0.tar.gz/ob-metaflow-stubs-6.0.3.183rc0/metaflow-stubs/plugins/gcp/gs_exceptions.pyi
@@ -0,0 +1,18 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.18.1+obcheckpoint(0.2.1);ob(v1)                                                   #
+# Generated on 2025-06-20T20:53:54.333144                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+import metaflow
+import typing
+if typing.TYPE_CHECKING:
+    import metaflow.exception
+
+from ...exception import MetaflowException as MetaflowException
+
+class MetaflowGSPackageError(metaflow.exception.MetaflowException, metaclass=type):
+    ...
+
