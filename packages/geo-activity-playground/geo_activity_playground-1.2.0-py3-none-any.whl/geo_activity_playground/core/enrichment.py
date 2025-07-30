c1fad02626baefc76cabb0a4760bcd2bc9c48a1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:31 +0000

    Add package geo-activity-playground/1.2.0/geo_activity_playground-1.2.0-py3-none-any.whl

---
+++ packages/geo-activity-playground/geo_activity_playground-1.2.0-py3-none-any.whl/geo_activity_playground/core/enrichment.py
@@ -0,0 +1,212 @@
+import datetime
+import logging
+import pickle
+from typing import Optional
+
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from tqdm import tqdm
+
+from .config import Config
+from .coordinates import get_distance
+from .datamodel import Activity
+from .datamodel import ActivityMeta
+from .datamodel import DB
+from .datamodel import get_or_make_equipment
+from .datamodel import get_or_make_kind
+from .missing_values import some
+from .paths import activity_extracted_meta_dir
+from .paths import activity_extracted_time_series_dir
+from .paths import TIME_SERIES_DIR
+from .tiles import compute_tile_float
+from .time_conversion import convert_to_datetime_ns
+
+logger = logging.getLogger(__name__)
+
+
+def populate_database_from_extracted(config: Config) -> None:
+    available_ids = {
+        int(path.stem) for path in activity_extracted_meta_dir().glob("*.pickle")
+    }
+    present_ids = {
+        int(elem)
+        for elem in DB.session.scalars(sqlalchemy.select(Activity.upstream_id)).all()
+        if elem
+    }
+    new_ids = available_ids - present_ids
+
+    for upstream_id in tqdm(new_ids, desc="Importing new activities into database"):
+        extracted_metadata_path = (
+            activity_extracted_meta_dir() / f"{upstream_id}.pickle"
+        )
+        with open(extracted_metadata_path, "rb") as f:
+            extracted_metadata: ActivityMeta = pickle.load(f)
+
+        extracted_time_series_path = (
+            activity_extracted_time_series_dir() / f"{upstream_id}.parquet"
+        )
+        time_series = pd.read_parquet(extracted_time_series_path)
+
+        # Skip activities that don't have geo information attached to them. This shouldn't happen, though.
+        if "latitude" not in time_series.columns:
+            logger.warning(
+                f"Activity {upstream_id} doesn't have latitude/longitude information. Ignoring this one."
+            )
+            continue
+
+        time_series = _embellish_single_time_series(
+            time_series,
+            extracted_metadata.get("start", None),
+            config.time_diff_threshold_seconds,
+        )
+
+        kind_name = extracted_metadata.get("kind", None)
+        if kind_name:
+            # Rename kinds if needed.
+            if kind_name in config.kind_renames:
+                kind_name = config.kind_renames[kind_name]
+            kind = get_or_make_kind(kind_name, config)
+        else:
+            kind = None
+
+        equipment_name = extracted_metadata.get("equipment", None)
+        if equipment_name:
+            equipment = get_or_make_equipment(equipment_name, config)
+        elif kind:
+            equipment = kind.default_equipment
+        else:
+            equipment = None
+
+        activity = Activity(
+            name=extracted_metadata.get("name", "Name Placeholder"),
+            distance_km=0,
+            equipment=equipment,
+            kind=kind,
+            calories=some(extracted_metadata.get("calories", None)),
+            elevation_gain=some(extracted_metadata.get("elevation_gain", None)),
+            steps=some(extracted_metadata.get("steps", None)),
+            path=extracted_metadata.get("path", None),
+            upstream_id=upstream_id,
+        )
+
+        update_via_time_series(activity, time_series)
+
+        DB.session.add(activity)
+        try:
+            DB.session.commit()
+        except sqlalchemy.exc.StatementError:
+            logger.error(
+                f"Could not insert the following activity into the database: {vars(activity)=}"
+            )
+            raise
+
+        enriched_time_series_path = TIME_SERIES_DIR() / f"{activity.id}.parquet"
+        time_series.to_parquet(enriched_time_series_path)
+
+
+def update_via_time_series(
+    activity: Activity, time_series: pd.DataFrame
+) -> ActivityMeta:
+    activity.start = some(time_series["time"].iloc[0])
+    activity.elapsed_time = some(
+        time_series["time"].iloc[-1] - time_series["time"].iloc[0]
+    )
+    activity.distance_km = (
+        time_series["distance_km"].iloc[-1] - time_series["distance_km"].iloc[0]
+    )
+    if "calories" in time_series.columns:
+        activity.calories = (
+            time_series["calories"].iloc[-1] - time_series["calories"].iloc[0]
+        )
+    activity.moving_time = _compute_moving_time(time_series)
+
+    activity.start_latitude = time_series["latitude"].iloc[0]
+    activity.end_latitude = time_series["latitude"].iloc[-1]
+    activity.start_longitude = time_series["longitude"].iloc[0]
+    activity.end_longitude = time_series["longitude"].iloc[-1]
+    if "elevation_gain_cum" in time_series.columns:
+        elevation_gain_cum = time_series["elevation_gain_cum"].fillna(0)
+        activity.elevation_gain = (
+            elevation_gain_cum.iloc[-1] - elevation_gain_cum.iloc[0]
+        )
+
+
+def _compute_moving_time(time_series: pd.DataFrame) -> datetime.timedelta:
+    def moving_time(group) -> datetime.timedelta:
+        selection = group["speed"] > 1.0
+        time_diff = group["time"].diff().loc[selection]
+        return time_diff.sum()
+
+    return (
+        time_series.groupby("segment_id").apply(moving_time, include_groups=False).sum()
+    )
+
+
+def _embellish_single_time_series(
+    timeseries: pd.DataFrame,
+    start: Optional[datetime.datetime],
+    time_diff_threshold_seconds: int,
+) -> pd.DataFrame:
+    if start is not None and pd.api.types.is_dtype_equal(
+        timeseries["time"].dtype, "int64"
+    ):
+        time = timeseries["time"]
+        del timeseries["time"]
+        timeseries["time"] = [
+            convert_to_datetime_ns(start + datetime.timedelta(seconds=t)) for t in time
+        ]
+    timeseries["time"] = convert_to_datetime_ns(timeseries["time"])
+    assert pd.api.types.is_dtype_equal(timeseries["time"].dtype, "datetime64[ns]"), (
+        timeseries["time"].dtype,
+        timeseries["time"].iloc[0],
+    )
+
+    distances = get_distance(
+        timeseries["latitude"].shift(1),
+        timeseries["longitude"].shift(1),
+        timeseries["latitude"],
+        timeseries["longitude"],
+    ).fillna(0.0)
+    if time_diff_threshold_seconds:
+        time_diff = (
+            timeseries["time"] - timeseries["time"].shift(1)
+        ).dt.total_seconds()
+        jump_indices = time_diff >= time_diff_threshold_seconds
+        distances.loc[jump_indices] = 0.0
+
+    if "distance_km" not in timeseries.columns:
+        timeseries["distance_km"] = pd.Series(np.cumsum(distances)) / 1000
+
+    if "speed" not in timeseries.columns:
+        timeseries["speed"] = (
+            timeseries["distance_km"].diff()
+            / (timeseries["time"].diff().dt.total_seconds() + 1e-3)
+            * 3600
+        )
+
+    potential_jumps = (timeseries["speed"] > 40) & (timeseries["speed"].diff() > 10)
+    if np.any(potential_jumps):
+        timeseries = timeseries.loc[~potential_jumps].copy()
+
+    if "segment_id" not in timeseries.columns:
+        if time_diff_threshold_seconds:
+            timeseries["segment_id"] = np.cumsum(jump_indices)
+        else:
+            timeseries["segment_id"] = 0
+
+    if "x" not in timeseries.columns:
+        x, y = compute_tile_float(timeseries["latitude"], timeseries["longitude"], 0)
+        timeseries["x"] = x
+        timeseries["y"] = y
+
+    if "altitude" in timeseries.columns:
+        timeseries.rename(columns={"altitude": "elevation"}, inplace=True)
+    if "elevation" in timeseries.columns:
+        elevation_diff = timeseries["elevation"].diff()
+        elevation_diff = elevation_diff.ewm(span=5, min_periods=5).mean()
+        elevation_diff.loc[elevation_diff.abs() > 30] = 0
+        elevation_diff.loc[elevation_diff < 0] = 0
+        timeseries["elevation_gain_cum"] = elevation_diff.cumsum()
+
+    return timeseries
---
+++ packages/geo-activity-playground/geo_activity_playground-1.2.0-py3-none-any.whl/geo_activity_playground/importers/directory.py
@@ -0,0 +1,146 @@
+import hashlib
+import logging
+import pathlib
+import pickle
+import re
+import traceback
+from typing import Optional
+
+from tqdm import tqdm
+
+from ..core.config import Config
+from ..core.datamodel import ActivityMeta
+from ..core.datamodel import DEFAULT_UNKNOWN_NAME
+from ..core.paths import activity_extracted_dir
+from ..core.paths import activity_extracted_meta_dir
+from ..core.paths import activity_extracted_time_series_dir
+from ..core.tasks import stored_object
+from ..core.tasks import WorkTracker
+from .activity_parsers import ActivityParseError
+from .activity_parsers import read_activity
+
+logger = logging.getLogger(__name__)
+
+ACTIVITY_DIR = pathlib.Path("Activities")
+
+
+def import_from_directory(
+    metadata_extraction_regexes: list[str], config: Config
+) -> None:
+
+    activity_paths = [
+        path
+        for path in ACTIVITY_DIR.rglob("*.*")
+        if path.is_file()
+        and path.suffixes
+        and not path.stem.startswith(".")
+        and not path.suffix in config.ignore_suffixes
+    ]
+    work_tracker = WorkTracker(activity_extracted_dir() / "work-tracker-extract.pickle")
+    new_activity_paths = work_tracker.filter(activity_paths)
+
+    with stored_object(
+        activity_extracted_dir() / "file-hashes.pickle", {}
+    ) as file_hashes:
+        for path in tqdm(new_activity_paths, desc="Detect deleted activities"):
+            file_hashes[path] = get_file_hash(path)
+
+        deleted_files = set(file_hashes.keys()) - set(activity_paths)
+        deleted_hashes = [file_hashes[path] for path in deleted_files]
+        for deleted_hash in deleted_hashes:
+            activity_extracted_meta_path = (
+                activity_extracted_meta_dir() / f"{deleted_hash}.pickle"
+            )
+            activity_extracted_time_series_path = (
+                activity_extracted_time_series_dir() / f"{deleted_hash}.parquet"
+            )
+            logger.warning(f"Deleting {activity_extracted_meta_path}")
+            logger.warning(f"Deleting {activity_extracted_time_series_path}")
+            activity_extracted_meta_path.unlink(missing_ok=True)
+            activity_extracted_time_series_path.unlink(missing_ok=True)
+        for deleted_file in deleted_files:
+            logger.warning(f"Deleting {deleted_file}")
+            del file_hashes[deleted_file]
+            work_tracker.discard(deleted_file)
+
+    paths_with_errors = []
+    for path in tqdm(new_activity_paths, desc="Parse activity metadata (serially)"):
+        errors = _cache_single_file(path)
+        if errors:
+            paths_with_errors.append(errors)
+
+    for path in tqdm(new_activity_paths, desc="Collate activity metadata"):
+        activity_id = get_file_hash(path)
+        file_metadata_path = activity_extracted_meta_dir() / f"{activity_id}.pickle"
+        work_tracker.mark_done(path)
+
+        if not file_metadata_path.exists():
+            continue
+
+        with open(file_metadata_path, "rb") as f:
+            activity_meta_from_file = pickle.load(f)
+
+        activity_meta = ActivityMeta(
+            id=activity_id,
+            # https://stackoverflow.com/a/74718395/653152
+            name=path.name.removesuffix("".join(path.suffixes)),
+            path=str(path),
+            kind=DEFAULT_UNKNOWN_NAME,
+            equipment=DEFAULT_UNKNOWN_NAME,
+            consider_for_achievements=True,
+        )
+        activity_meta.update(activity_meta_from_file)
+        activity_meta.update(_get_metadata_from_path(path, metadata_extraction_regexes))
+        with open(file_metadata_path, "wb") as f:
+            pickle.dump(activity_meta, f)
+
+    if paths_with_errors:
+        logger.warning(
+            "There were errors while parsing some of the files. These were skipped and tried again next time."
+        )
+        for path, error in paths_with_errors:
+            logger.error(f"{path}: {error}")
+
+    work_tracker.close()
+
+
+def _cache_single_file(path: pathlib.Path) -> Optional[tuple[pathlib.Path, str]]:
+    activity_id = get_file_hash(path)
+    timeseries_path = activity_extracted_time_series_dir() / f"{activity_id}.parquet"
+    file_metadata_path = activity_extracted_meta_dir() / f"{activity_id}.pickle"
+
+    if not timeseries_path.exists():
+        try:
+            activity_meta_from_file, timeseries = read_activity(path)
+        except ActivityParseError as e:
+            logger.error(f"Error while parsing file {path}:")
+            traceback.print_exc()
+            return path, str(e)
+        except:
+            logger.error(f"Encountered a problem with {path=}, see details below.")
+            raise
+
+        if len(timeseries) == 0:
+            return None
+
+        timeseries.to_parquet(timeseries_path)
+        with open(file_metadata_path, "wb") as f:
+            pickle.dump(activity_meta_from_file, f)
+    return None
+
+
+def get_file_hash(path: pathlib.Path) -> int:
+    file_hash = hashlib.blake2s()
+    with open(path, "rb") as f:
+        while chunk := f.read(8192):
+            file_hash.update(chunk)
+    return int(file_hash.hexdigest(), 16) % 2**62
+
+
+def _get_metadata_from_path(
+    path: pathlib.Path, metadata_extraction_regexes: list[str]
+) -> dict[str, str]:
+    for regex in metadata_extraction_regexes:
+        if m := re.search(regex, str(path.relative_to(ACTIVITY_DIR))):
+            return m.groupdict()
+    return {}
