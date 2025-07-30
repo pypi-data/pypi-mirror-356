973a2c2329355a10e52619bf8abd1486b89470ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:22 +0000

    Add package mx82/0.2.4/mx82-0.2.4-py3-none-any.whl

---
+++ packages/mx82/mx82-0.2.4-py3-none-any.whl/mx82/funcs.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+
+def sf(x, sig=3):
+    """Round a number or np.ndarray to the specified number of significant figures."""
+    if isinstance(x, np.ndarray):
+        return np.array([sf(xi, sig) for xi in x])
+    if x == 0:
+        return 0
+    return round(x, sig - int(np.floor(np.log10(abs(x)))) - 1)
+
+
+def sf3(x):
+    """Round a number or np.ndarray to 3 significant figures."""
+    if isinstance(x, np.ndarray):
+        return np.array([sf(xi, 3) for xi in x])
+    if x == 0:
+        return 0
+    return round(x, 3 - int(np.floor(np.log10(abs(x)))) - 1)
+
+
+def sf4(x):
+    """Round a number or np.ndarray to 4 significant figures."""
+    if isinstance(x, np.ndarray):
+        return np.array([sf(xi, 4) for xi in x])
+    if x == 0:
+        return 0
+    return round(x, 4 - int(np.floor(np.log10(abs(x)))) - 1)
+
+
+def sf5(x):
+    """Round a number or np.ndarray to 5 significant figures."""
+    if isinstance(x, np.ndarray):
+        return np.array([sf(xi, 5) for xi in x])
+    if x == 0:
+        return 0
+    return round(x, 5 - int(np.floor(np.log10(abs(x)))) - 1)
