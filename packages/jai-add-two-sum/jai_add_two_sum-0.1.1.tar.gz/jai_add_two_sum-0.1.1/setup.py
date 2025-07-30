896f62a104987511bb4672847bac3838fb5e8938
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:48 +0000

    Add package jai-add-two-sum/0.1.1/jai_add_two_sum-0.1.1.tar.gz

---
+++ packages/jai-add-two-sum/jai_add_two_sum-0.1.1.tar.gz/jai_add_two_sum-0.1.1/setup.py
@@ -0,0 +1,19 @@
+__author__ = "narmadha-raghu"
+from setuptools import setup, find_packages
+
+setup(
+    name="jai_add_two_sum",
+    version="0.1.1",
+    author="Jai",
+    author_email="jai@gmail.com",
+    description="",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+)
