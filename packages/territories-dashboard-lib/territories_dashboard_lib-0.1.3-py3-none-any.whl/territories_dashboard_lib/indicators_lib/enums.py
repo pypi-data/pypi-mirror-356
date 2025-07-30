a9e540301e9f587653353708a308317d3fa9875d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:34:16 +0000

    Add package territories-dashboard-lib/0.1.3/territories_dashboard_lib-0.1.3-py3-none-any.whl

---
+++ packages/territories-dashboard-lib/territories_dashboard_lib-0.1.3-py3-none-any.whl/territories_dashboard_lib/indicators_lib/enums.py
@@ -0,0 +1,59 @@
+from django.db import models
+
+
+class AggregationFunctions(models.TextChoices):
+    DISCRETE_COMPONENT_2 = "discrete"
+    REPEATED_COMPONENT_2 = "repeated"
+
+
+class MeshLevel(models.TextChoices):
+    National = "fr"
+    Region = "reg"
+    Department = "dep"
+    Epci = "epci"
+    Town = "com"
+
+
+class GeoLevel(models.TextChoices):
+    France = "fr"
+    Region = "reg"
+    Department = "dep"
+    Epci = "epci"
+    Town = "com"
+
+
+class FranceGeoLevel(models.TextChoices):
+    All = "FR0,FR1,FR2"
+    METRO = "FR0,FR1"
+    METRO_HORS_IDF = "FR0"
+
+
+FRANCE_GEOLEVEL_TITLES = {
+    FranceGeoLevel.All: "France entière",
+    FranceGeoLevel.METRO: "France métropolitaine",
+    FranceGeoLevel.METRO_HORS_IDF: "France métropolitaine hors IDF",
+}
+
+FRANCE_DB_VALUES = {
+    FranceGeoLevel.All: "FR_TOT",
+    FranceGeoLevel.METRO: "FR_METRO",
+    FranceGeoLevel.METRO_HORS_IDF: "FR_METRO_HORS_IDF",
+}
+
+
+DEFAULT_MESH = MeshLevel.Region
+
+MESH_TITLES = {
+    MeshLevel.National: "France entière",
+    MeshLevel.Region: "Région",
+    MeshLevel.Department: "Département",
+    MeshLevel.Epci: "Intercommunalité",
+    MeshLevel.Town: "Commune",
+}
+
+MESH_DB = {
+    MeshLevel.Region: "REG",
+    MeshLevel.Department: "DEP",
+    MeshLevel.Epci: "EPCI",
+    MeshLevel.Town: "DEPCOM",
+}
