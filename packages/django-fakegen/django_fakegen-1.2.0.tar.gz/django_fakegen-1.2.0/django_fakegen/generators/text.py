77166d165c29f3bfdfa91fa40f363ca8034d70d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:40 +0000

    Add package django-fakegen/1.2.0/django_fakegen-1.2.0.tar.gz

---
+++ packages/django-fakegen/django_fakegen-1.2.0.tar.gz/django_fakegen-1.2.0/django_fakegen/generators/text.py
@@ -0,0 +1,14 @@
+# Placeholder for char field generator 
+from .base import BaseFieldGenerator
+
+from django.db import models
+
+class TextFieldGenerator(BaseFieldGenerator):
+
+    def can_handle(self, field) -> bool:
+        return field.get_internal_type() == "TextField"
+    
+
+    def generate(self, field, faker, registry) -> None:
+        max_length = getattr(field, "max_length", 200)
+        return faker.text(max_nb_chars=max_length)
