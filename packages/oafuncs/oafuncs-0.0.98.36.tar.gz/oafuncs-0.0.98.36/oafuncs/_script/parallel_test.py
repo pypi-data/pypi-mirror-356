4809fcfc4eed94192e15a9664036d6d4e36ae57d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:35 +0000

    Add package oafuncs/0.0.98.36/oafuncs-0.0.98.36.tar.gz

---
+++ packages/oafuncs/oafuncs-0.0.98.36.tar.gz/oafuncs-0.0.98.36/oafuncs/_script/parallel_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding=utf-8
+"""
+Author: Liu Kun && 16031215@qq.com
+Date: 2025-04-08 16:18:49
+LastEditors: Liu Kun && 16031215@qq.com
+LastEditTime: 2025-04-08 16:18:50
+FilePath: \\Python\\My_Funcs\\OAFuncs\\oafuncs\\_script\\parallel_test.py
+Description:
+EditPlatform: vscode
+ComputerInfo: XPS 15 9510
+SystemInfo: Windows 11
+Python Version: 3.12
+"""
