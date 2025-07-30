4bfc178ccca4da7c7b12d91591e5d10b5818c840
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:19 +0000

    Add package nonebot-plugin-qqessentials/0.1.1/nonebot_plugin_qqessentials-0.1.1-py3-none-any.whl

---
+++ packages/nonebot-plugin-qqessentials/nonebot_plugin_qqessentials-0.1.1-py3-none-any.whl/nonebot_plugin_qqessentials/__init__.py
@@ -0,0 +1,22 @@
+from nonebot import get_plugin_config
+from nonebot.plugin import PluginMetadata
+
+from . import __main__ as __main__
+from . import friend as friend
+from . import group as group
+
+
+from .config import Config
+
+__version__ = "0.1.1"
+__plugin_meta__ = PluginMetadata(
+    name="QQEssentials",
+    description="一个能够满足你很多需求的基础插件！",
+    usage="目前支持:点赞，撤回，设精，禁言，发送消息等更多操作，后续会持续更新！",
+    type="application",
+    homepage="https://github.com/Murasame-Dev/nonebot-plugin-qqessentials",
+    supported_adapters= {"~onebot.v11"},
+    config=Config,
+)
+
+config = get_plugin_config(Config)
