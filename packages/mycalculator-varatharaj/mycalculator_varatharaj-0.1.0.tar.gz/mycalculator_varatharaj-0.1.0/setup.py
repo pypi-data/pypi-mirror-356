392259270f4a6a8d0c263ab413d55cd148e3c6fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:43 +0000

    Add package mycalculator-varatharaj/0.1.0/mycalculator_varatharaj-0.1.0.tar.gz

---
+++ packages/mycalculator-varatharaj/mycalculator_varatharaj-0.1.0.tar.gz/mycalculator_varatharaj-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mycalculator_varatharaj',                
+    version='0.1.0',
+    author='Varatharaj',
+    author_email='varatharajskacas@gmail.com',
+    description='A simple calculator package',
+    packages=find_packages(),           
+    install_requires=[],          
+  
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.6',
+)
