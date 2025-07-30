a4aa4604cd61df6f24088819f5976e05bdb3f222
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:26 +0000

    Add package maite-datasets/0.0.2/maite_datasets-0.0.2.tar.gz

---
+++ packages/maite-datasets/maite_datasets-0.0.2.tar.gz/maite_datasets-0.0.2/src/maite_datasets/__init__.py
@@ -0,0 +1,5 @@
+"""Module for MAITE compliant Computer Vision datasets."""
+
+from maite_datasets._builder import to_image_classification_dataset, to_object_detection_dataset
+
+__all__ = ["to_image_classification_dataset", "to_object_detection_dataset"]
