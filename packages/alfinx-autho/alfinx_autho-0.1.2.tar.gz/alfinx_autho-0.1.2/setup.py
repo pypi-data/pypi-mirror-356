c11d812b98a43062d136c68c1ae44803ec112112
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:07 +0000

    Add package alfinx-autho/0.1.2/alfinx_autho-0.1.2.tar.gz

---
+++ packages/alfinx-autho/alfinx_autho-0.1.2.tar.gz/alfinx_autho-0.1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="alfinx_autho",
+    version="0.1.2",
+    description="Headers auto-detection library",
+    author="Maharram Mansimli",
+    author_email="mnismlim@gmail.com",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
