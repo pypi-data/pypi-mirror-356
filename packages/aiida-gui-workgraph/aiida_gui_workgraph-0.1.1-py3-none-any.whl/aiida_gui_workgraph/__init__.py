3f1b9616ff7a413b1720cb2615fcc60f46f5c26c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:54 +0000

    Add package aiida-gui-workgraph/0.1.1/aiida_gui_workgraph-0.1.1-py3-none-any.whl

---
+++ packages/aiida-gui-workgraph/aiida_gui_workgraph-0.1.1-py3-none-any.whl/aiida_gui_workgraph/__init__.py
@@ -0,0 +1,18 @@
+import pathlib
+from .workgraph import router as workgraph_router
+from .scheduler import router as scheduler_router
+
+__version__ = "0.1.1"
+
+# static_dir points to plugin1/static
+THIS_DIR = pathlib.Path(__file__).parent
+static_dir = str(THIS_DIR / "static")
+
+plugin = {
+    "routers": {
+        "workgraph": workgraph_router,
+        "scheduler": scheduler_router,
+    },
+    "name": "WorkGraph",
+    "static_dirs": {"workgraph": static_dir},
+}
