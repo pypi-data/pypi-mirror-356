6973e4b5b70be1e3a817c6904c8e5c614cd99ebf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:21 +0000

    Add package shfuncdecfmt/1.0.1/shfuncdecfmt-1.0.1.tar.gz

---
+++ packages/shfuncdecfmt/shfuncdecfmt-1.0.1.tar.gz/shfuncdecfmt-1.0.1/src/shfuncdecfmt/__init__.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+
+import argparse
+import re
+from argparse import ArgumentParser
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+
+
+class Args(argparse.Namespace):
+    files: Sequence[Path]
+
+
+def parse_args() -> Args:
+    parser = ArgumentParser("shfuncdecfmt")
+
+    parser.add_argument(
+        "files",
+        nargs="+",
+        type=Path,
+    )
+
+    return parser.parse_args(namespace=Args())
+
+
+def main() -> int:
+    args = parse_args()
+
+    retval = 0
+    for file in args.files:
+        content = file.read_text()
+
+        new_content = re.sub(
+            r"^(\s*)(function\s*)?([a-zA-Z_][a-zA-Z0-9_]*)\s*(?:\(\s*\))?\s*\{",
+            r"\1\3() {",
+            content,
+            flags=re.MULTILINE,
+        )
+
+        if new_content != content:
+            file.write_text(new_content)
+            retval = 1
+
+    return retval
