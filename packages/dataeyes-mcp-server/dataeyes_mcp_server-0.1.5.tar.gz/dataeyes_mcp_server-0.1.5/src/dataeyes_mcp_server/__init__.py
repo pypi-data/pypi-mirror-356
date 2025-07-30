64af78f5a003d6d40fd90b8fbefc667f16ccf51c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:10:29 +0000

    Add package dataeyes-mcp-server/0.1.5/dataeyes_mcp_server-0.1.5.tar.gz

---
+++ packages/dataeyes-mcp-server/dataeyes_mcp_server-0.1.5.tar.gz/dataeyes_mcp_server-0.1.5/src/dataeyes_mcp_server/__init__.py
@@ -0,0 +1,8 @@
+#from .dataeyes import mcp
+from .dataeyes import main
+#def main():
+#    """MCP Dataeyes Server - HTTP call Dataeyes API for MCP"""
+#    dataeyes(transport="stdio")
+
+if __name__ == "__main__":
+    main()
