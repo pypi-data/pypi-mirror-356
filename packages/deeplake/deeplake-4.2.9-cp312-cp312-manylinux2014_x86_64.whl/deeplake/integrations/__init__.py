3201953600f4d0cfbbf3f3fd897061345c8b7a8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:08:46 +0000

    Add package deeplake/4.2.9/deeplake-4.2.9-cp312-cp312-manylinux2014_x86_64.whl

---
+++ packages/deeplake/deeplake-4.2.9-cp312-cp312-manylinux2014_x86_64.whl/deeplake/integrations/__init__.py
@@ -0,0 +1,6 @@
+from .labelbox import (
+    create_labelbox_annotation_project,
+    create_dataset_from_video_annotation_project,
+    converter_for_video_project_with_id,
+    load_blob_file_paths_from_azure,
+)
