c38bfab903f331b94a6f3fb962dd21786554487c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:35 +0000

    Add package cehuibaibaoxiang2/0.1.0/cehuibaibaoxiang2-0.1.0.tar.gz

---
+++ packages/cehuibaibaoxiang2/cehuibaibaoxiang2-0.1.0.tar.gz/cehuibaibaoxiang2-0.1.0/setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 18 11:15:25 2025
+
+@author: 2
+"""
+
+from setuptools import setup
+
+setup(
+    name="cehuibaibaoxiang2",  
+    version="0.1.0",         
+    author="徐若朴",       
+    author_email="2253959@tongji.edu.cn",  
+    description="（自用）本人写的测绘专业课作业代码合集",  
+    long_description=open("README.md",encoding="utf-8").read(),  
+    long_description_content_type="text/markdown",  
+    url="https://github.com/piaopiaoxuruop/cehuibaibaoxiang/tree/main?tab=readme-ov-file",  
+    license="MIT",  # 许可证类型
+    py_modules=["cehuibaibaoxiang"],  
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: GIS",
+    ],
+    install_requires=[
+        "numpy",  # 用于数学运算和数组操作
+        "scipy",  # 用于科学计算，包括Delaunay三角剖分和griddata插值
+        "matplotlib",  # 用于绘图
+        "shapely",  # 用于几何操作
+        "pyproj",  # 用于地理坐标转换
+        "rasterio",  # 用于栅格数据操作
+        "imageio",  # 用于图像读写
+        "opencv-python",  # OpenCV的Python接口
+        "pandas",  # 用于数据分析
+    ],
+    python_requires=">=3.8",  # Python 版本要求
+)
