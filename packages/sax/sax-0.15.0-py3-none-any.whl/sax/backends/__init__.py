1da14b94db911758302357b3db7f7b6f17de5fc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:09 +0000

    Add package sax/0.15.0/sax-0.15.0-py3-none-any.whl

---
+++ packages/sax/sax-0.15.0-py3-none-any.whl/sax/backends/__init__.py
@@ -0,0 +1,141 @@
+"""SAX Backends."""
+
+from __future__ import annotations
+
+import warnings
+from typing import Any, cast
+
+from ..saxtypes import Backend, Model, SType
+from ..saxtypes.netlist import Instance
+from .additive import (
+    analyze_circuit_additive,
+    analyze_instances_additive,
+    evaluate_circuit_additive,
+)
+from .filipsson_gunnar import (
+    analyze_circuit_fg,
+    analyze_instances_fg,
+    evaluate_circuit_fg,
+)
+from .forward_only import (
+    analyze_circuit_forward,
+    analyze_instances_forward,
+    evaluate_circuit_forward,
+)
+
+__all__ = [
+    "analyze_circuit",
+    "analyze_circuit_additive",
+    "analyze_circuit_fg",
+    "analyze_circuit_forward",
+    "analyze_instances",
+    "analyze_instances_additive",
+    "analyze_instances_fg",
+    "analyze_instances_forward",
+    "backend_map",
+    "circuit_backends",
+    "evaluate_circuit",
+    "evaluate_circuit_additive",
+    "evaluate_circuit_fg",
+    "evaluate_circuit_forward",
+    "validate_circuit_backend",
+]
+
+circuit_backends = {
+    "fg": (
+        analyze_instances_fg,
+        analyze_circuit_fg,
+        evaluate_circuit_fg,
+    ),
+    "filipsson_gunnar": (
+        analyze_instances_fg,
+        analyze_circuit_fg,
+        evaluate_circuit_fg,
+    ),
+    "additive": (
+        analyze_instances_additive,
+        analyze_circuit_additive,
+        evaluate_circuit_additive,
+    ),
+    "forward": (
+        analyze_instances_forward,
+        analyze_circuit_forward,
+        evaluate_circuit_forward,
+    ),
+}
+
+backend_map = {
+    "fg": "filipsson_gunnar",
+    "filipsson_gunnar": "filipsson_gunnar",
+    "additive": "additive",
+    "forward": "forward",
+}
+
+try:
+    from .klu import analyze_circuit_klu, analyze_instances_klu, evaluate_circuit_klu
+
+    circuit_backends["klu"] = (
+        analyze_instances_klu,
+        analyze_circuit_klu,
+        evaluate_circuit_klu,
+    )
+    circuit_backends["default"] = (
+        analyze_instances_klu,
+        analyze_circuit_klu,
+        evaluate_circuit_klu,
+    )
+    backend_map["klu"] = "klu"
+    backend_map["default"] = "klu"
+except ImportError:
+    circuit_backends["default"] = (
+        analyze_instances_fg,
+        analyze_circuit_fg,
+        evaluate_circuit_fg,
+    )
+    backend_map["default"] = "filipsson_gunnar"
+    warnings.warn(
+        "klujax not found. Please install klujax for "
+        "better performance during circuit evaluation!",
+        stacklevel=2,
+    )
+
+
+def analyze_instances(
+    instances: dict[str, Instance],
+    models: dict[str, Model],
+) -> Any:  # noqa: ANN401
+    """Analyze circuit instances."""
+    return circuit_backends["default"][0](instances, models)
+
+
+def analyze_circuit(
+    analyzed_instances: Any,  # noqa: ANN401
+    connections: dict[str, str],
+    ports: dict[str, str],
+) -> Any:  # noqa: ANN401
+    """Analyze the circuit based on analyzed instances."""
+    return circuit_backends["default"][1](analyzed_instances, connections, ports)
+
+
+def evaluate_circuit(
+    analyzed: Any,  # noqa: ANN401
+    instances: dict[str, SType],
+) -> SType:
+    """Evaluate the circuit based on analyzed instances."""
+    return circuit_backends["default"][2](analyzed, instances)
+
+
+def validate_circuit_backend(backend: str) -> Backend:
+    """Validate the circuit backend."""
+    backend = backend.lower()
+    backend = backend_map.get(backend, backend)
+    # assert valid circuit_backend
+    if backend not in circuit_backends:
+        msg = (
+            f"circuit backend {backend} not found. Allowed circuit backends: "
+            f"{', '.join(circuit_backends.keys())}."
+        )
+        raise KeyError(
+            msg,
+        )
+    return cast(Backend, backend)
---
+++ packages/sax/sax-0.15.0-py3-none-any.whl/sax/multimode.py
@@ -0,0 +1,240 @@
+"""SAX Multimode support."""
+
+from __future__ import annotations
+
+from collections.abc import Iterable
+from functools import wraps
+from typing import cast, overload
+
+import jax.numpy as jnp
+from natsort import natsorted
+
+import sax
+
+from .constants import DEFAULT_MODE, DEFAULT_MODES
+
+__all__ = [
+    "multimode",
+    "singlemode",
+]
+
+
+@overload
+def multimode(
+    S: sax.SDictModel, modes: tuple[str, ...] = DEFAULT_MODES
+) -> sax.SDictModel: ...
+
+
+@overload
+def multimode(
+    S: sax.SCooModel, modes: tuple[str, ...] = DEFAULT_MODES
+) -> sax.SCooModel: ...
+
+
+@overload
+def multimode(
+    S: sax.SDenseModel, modes: tuple[str, ...] = DEFAULT_MODES
+) -> sax.SDenseModel: ...
+
+
+@overload
+def multimode(S: sax.SDict, modes: tuple[str, ...] = DEFAULT_MODES) -> sax.SDictMM: ...
+
+
+@overload
+def multimode(S: sax.SCoo, modes: tuple[str, ...] = DEFAULT_MODES) -> sax.SCooMM: ...
+
+
+@overload
+def multimode(
+    S: sax.SDense, modes: tuple[str, ...] = DEFAULT_MODES
+) -> sax.SDenseMM: ...
+
+
+def multimode(
+    S: sax.SType | sax.Model,
+    modes: tuple[str, ...] = DEFAULT_MODES,
+) -> sax.STypeMM | sax.ModelMM:
+    """Convert a single mode model to a multimode model."""
+    if (model := sax.try_into[sax.Model](S)) is not None:
+
+        @wraps(model)
+        def new_model(**params: sax.SettingsValue) -> sax.STypeMM:
+            return multimode(model(**params), modes=modes)
+
+        return cast(sax.ModelMM, new_model)
+
+    s: sax.SType = sax.into[sax.SType](S)
+
+    if (sdict_sm := sax.try_into[sax.SDictSM](s)) is not None:
+        return _multimode_sdict(sdict_sm, modes=modes)
+
+    if (scoo_sm := sax.try_into[sax.SCooSM](s)) is not None:
+        return _multimode_scoo(scoo_sm, modes=modes)
+
+    if (sdense_sm := sax.try_into[sax.SDenseSM](s)) is not None:
+        return _multimode_sdense(sdense_sm)
+
+    if (s_mm := sax.try_into[sax.STypeMM](s)) is not None:
+        return s_mm
+
+    msg = f"Cannot convert {S!r} to multimode. Unknown SType."
+    raise ValueError(msg)
+
+
+def _multimode_sdict(
+    sdict: sax.SDict, modes: tuple[str, ...] = DEFAULT_MODES
+) -> sax.SDict:
+    multimode_sdict = {}
+    mode_combinations = _mode_combinations(modes)
+    for (p1, p2), value in sdict.items():
+        for m1, m2 in mode_combinations:
+            multimode_sdict[f"{p1}@{m1}", f"{p2}@{m2}"] = value
+    return multimode_sdict
+
+
+def _multimode_scoo(
+    scoo: sax.SCoo, modes: tuple[sax.Mode, ...] = DEFAULT_MODES
+) -> sax.SCoo:
+    Si, Sj, Sx, port_map = scoo
+    num_ports = len(port_map)
+    mode_map = {mode: i for i, mode in enumerate(modes)}
+
+    mode_combinations = _mode_combinations(modes)
+
+    Si_m = jnp.concatenate(
+        [Si + mode_map[m] * num_ports for m, _ in mode_combinations],
+        -1,
+    )
+    Sj_m = jnp.concatenate(
+        [Sj + mode_map[m] * num_ports for _, m in mode_combinations],
+        -1,
+    )
+    Sx_m = jnp.concatenate([Sx for _ in mode_combinations], -1)
+    port_map_m = {
+        f"{port}@{mode}": idx + mode_map[mode] * num_ports
+        for mode in modes
+        for port, idx in port_map.items()
+    }
+
+    return Si_m, Sj_m, Sx_m, port_map_m
+
+
+def _multimode_sdense(
+    sdense: sax.SDenseSM, modes: tuple[sax.Mode, ...] = DEFAULT_MODES
+) -> sax.SDenseMM:
+    Sx, port_map = sdense
+    num_ports = len(port_map)
+    mode_map: dict[sax.Mode, int] = {mode: i for i, mode in enumerate(modes)}
+
+    Sx_m = sax.block_diag(*(Sx for _ in modes))
+
+    port_map_m = {
+        f"{port}@{mode}": idx + mode_map[mode] * num_ports
+        for mode in modes
+        for port, idx in port_map.items()
+    }
+
+    return Sx_m, port_map_m
+
+
+@overload
+def singlemode(S: sax.SDictModel, mode: str = DEFAULT_MODE) -> sax.SDictModelSM: ...
+
+
+@overload
+def singlemode(S: sax.SCooModel, mode: str = DEFAULT_MODE) -> sax.SCooModelSM: ...
+
+
+@overload
+def singlemode(S: sax.SDenseModel, mode: str = DEFAULT_MODE) -> sax.SDenseModelSM: ...
+
+
+@overload
+def singlemode(S: sax.SDict, mode: str = DEFAULT_MODE) -> sax.SDictSM: ...
+
+
+@overload
+def singlemode(S: sax.SCoo, mode: str = DEFAULT_MODE) -> sax.SCooSM: ...
+
+
+@overload
+def singlemode(S: sax.SDense, mode: str = DEFAULT_MODE) -> sax.SDenseSM: ...
+
+
+def singlemode(
+    S: sax.SType | sax.Model, mode: sax.Mode = DEFAULT_MODE
+) -> sax.STypeSM | sax.ModelSM:
+    """Convert multimode model to a singlemode model."""
+    if (model := sax.try_into[sax.Model](S)) is not None:
+
+        @wraps(model)
+        def new_model(**params: sax.SettingsValue) -> sax.STypeSM:
+            return singlemode(model(**params), mode=mode)
+
+        return cast(sax.ModelSM, new_model)
+
+    if (sdict_mm := sax.try_into[sax.SDictMM](S)) is not None:
+        return _singlemode_sdict(sdict_mm, mode=mode)
+
+    if (scoo_mm := sax.try_into[sax.SCooMM](S)) is not None:
+        return _singlemode_scoo(scoo_mm, mode=mode)
+
+    if (sdense_mm := sax.try_into[sax.SDenseMM](S)) is not None:
+        return _singlemode_sdense(sdense_mm, mode=mode)
+
+    if (s_sm := sax.try_into[sax.STypeSM](S)) is not None:
+        return s_sm
+
+    msg = f"Cannot convert {S!r} to singlemode. Unknown SType."
+    raise ValueError(msg)
+
+
+def _singlemode_sdict(sdict: sax.SDictMM, mode: str = DEFAULT_MODE) -> sax.SDictSM:
+    sdict_sm = {}
+    for (p1, p2), value in sdict.items():
+        if p1.endswith(f"@{mode}") and p2.endswith(f"@{mode}"):
+            p1, _ = p1.split("@")
+            p2, _ = p2.split("@")
+            sdict_sm[p1, p2] = value
+    return sdict_sm
+
+
+def _singlemode_scoo(scoo: sax.SCooMM, mode: str = DEFAULT_MODE) -> sax.SCooSM:
+    Si, Sj, Sx, port_map = scoo
+    # no need to touch the data...
+    # just removing some ports from the port map should be enough
+    port_map = {
+        port.split("@")[0]: idx
+        for port, idx in port_map.items()
+        if port.endswith(f"@{mode}")
+    }
+    return Si, Sj, Sx, port_map
+
+
+def _singlemode_sdense(sdense: sax.SDenseMM, mode: str = DEFAULT_MODE) -> sax.SDenseSM:
+    Sx, port_map = sdense
+    port_map = {
+        port.split("@")[0]: idx
+        for port, idx in port_map.items()
+        if port.endswith(f"@{mode}")
+    }
+    return _consolidate_sdense((Sx, port_map))
+
+
+def _mode_combinations(
+    modes: Iterable[str], *, cross: bool = False
+) -> tuple[tuple[str, str], ...]:
+    if cross:
+        combinations = natsorted((m1, m2) for m1 in modes for m2 in modes)
+    else:
+        combinations = natsorted((m, m) for m in modes)
+    return tuple(combinations)
+
+
+def _consolidate_sdense(S: sax.SDense) -> sax.SDense:
+    s, pm = S
+    idxs = list(pm.values())
+    s = s[..., idxs, :][..., :, idxs]
+    pm = {p: i for i, p in enumerate(pm)}
+    return s, pm
