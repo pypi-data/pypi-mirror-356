b8eded4933a75cead04e1e8ef002317fcf24bd89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:58 +0000

    Add package krutrim-client/0.0.1/krutrim_client-0.0.1.tar.gz

---
+++ packages/krutrim-client/krutrim_client-0.0.1.tar.gz/krutrim_client-0.0.1/examples/securityGroup/DetachSecurityGroupRules.py
@@ -0,0 +1,23 @@
+from krutrim_client import KrutrimClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv("api_key")
+
+client = KrutrimClient(api_key = api_key)
+
+
+try:
+    detach_security_group_rules_resp = client.securityGroup.detach_rule(
+        ruleid ="Enter the Security Group Rule ID",
+        securityid = "Enter the Security Group ID",
+        vpcid = "Enter the VPC ID",
+        x_region = "Enter the region",
+        # x_region possible values "In-Bangalore-1","In-Hyderabad-1"
+    )
+
+    print(f"Detached Successfully")
+except Exception as e:
+    print(f"Exception has occurred:  {e}")
