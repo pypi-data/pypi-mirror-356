140260d3df7acc93c533fdf5e3d1f5be66e05742
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:10:11 +0000

    Add package devnetgen/0.2.9/devnetgen-0.2.9.tar.gz

---
+++ packages/devnetgen/devnetgen-0.2.9.tar.gz/devnetgen-0.2.9/devnetgen/test.py
@@ -0,0 +1,7 @@
+from devnetgen.main import create_crud
+from devnetgen.main import create_tests
+
+path = "C:\\Users\\Aleksandr\\Documents\\Jetbrains Projects\\Rider\\NaturalAreasSales\\server\\Domain\\Entities\\References\\Country.cs"
+
+create_crud(path)
+create_tests(path)
