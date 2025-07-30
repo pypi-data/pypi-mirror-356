d329e5f48e5361c32a2c25d85a0c481c4a23b65a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:27 +0000

    Add package sphero-unsw/0.1.2/sphero_unsw-0.1.2.tar.gz

---
+++ packages/sphero-unsw/sphero_unsw-0.1.2.tar.gz/sphero_unsw-0.1.2/sphero_unsw/__init__.py
@@ -0,0 +1,9 @@
+# Start by importing everything from spherov2
+from spherov2 import *
+
+# Then override or attach your own modules
+from sphero_unsw import scanner, sphero_edu, utils, types, toys_scanner
+
+# Import the boltplus module 
+from sphero_unsw.toy.boltplus import BOLTPLUS
+from sphero_unsw.commands.sensor import Sensor
