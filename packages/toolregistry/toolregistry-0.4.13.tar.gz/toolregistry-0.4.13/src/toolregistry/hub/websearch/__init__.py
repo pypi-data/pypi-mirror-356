eda65231e35434f94e40bb42823980c14504b7ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:24 +0000

    Add package toolregistry/0.4.13/toolregistry-0.4.13.tar.gz

---
+++ packages/toolregistry/toolregistry-0.4.13.tar.gz/toolregistry-0.4.13/src/toolregistry/hub/websearch/__init__.py
@@ -0,0 +1,13 @@
+from .fetch import Fetch
+from .websearch import WebSearchGeneral
+from .websearch_bing import WebSearchBing
+from .websearch_google import WebSearchGoogle
+from .websearch_searxng import WebSearchSearXNG
+
+__all__ = [
+    "Fetch",
+    "WebSearchGeneral",
+    "WebSearchBing",
+    "WebSearchGoogle",
+    "WebSearchSearXNG",
+]
